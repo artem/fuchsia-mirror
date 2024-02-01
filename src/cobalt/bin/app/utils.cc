afdcdd122e39b9f17a36e77e66329b40b6cda57d
Brian Bosak <bbosak@google.com>
2024-02-01 23:56:09 +0000

    [cobalt] Remove verbosity
    Verbosity has been unsupported for many years on fuchsia,
    no viewer supports it and we don't intend to support it.
    This change cleans up to use severities instead.
    
    Bug: b/322552971
    Bug: b/300157652
    Change-Id: I5964a4d60f90b5a9651adaa239baca42cc21cc39
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/984695
    Commit-Queue: Brian Bosak <bbosak@google.com>
    Reviewed-by: Thomas Gales <tgales@google.com>

--- src/cobalt/bin/app/utils.cc
+++ src/cobalt/bin/app/utils.cc
@@ -41,7 +41,7 @@ fpromise::result<void, fuchsia::metrics::Error> ToMetricsResult(const Status &s)
 }
 
 std::string ReadPublicKeyPem(const std::string &pem_file_path) {
-  FX_VLOGS(2) << "Reading PEM file at " << pem_file_path;
+  FX_LOGS(DEBUG) << "Reading PEM file at " << pem_file_path;
   std::string pem_out;
   FX_CHECK(util::PemUtil::ReadTextFile(pem_file_path, &pem_out))
       << "Unable to read file public key PEM file from path " << pem_file_path << ".";
