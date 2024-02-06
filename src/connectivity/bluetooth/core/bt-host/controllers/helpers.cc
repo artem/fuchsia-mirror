1c23aed6b15af5aee6b373034bc585e9fd3bf3d1
Lulu Wang <luluwang@google.com>
2024-02-06 16:03:30 +0000

    [bt][core] Add more statuses for Zx to Pw status helper
    Add more statuses to ZxStatusToPwStatus
    
    Change-Id: Icb2ac91d1d028e02e3e88541ea49e7c2dc0251c4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/986417
    Commit-Queue: Lulu Wang <luluwang@google.com>
    Reviewed-by: Ben Lawson <benlawson@google.com>

--- src/connectivity/bluetooth/core/bt-host/controllers/helpers.cc
+++ src/connectivity/bluetooth/core/bt-host/controllers/helpers.cc
@@ -10,14 +10,30 @@ pw::Status ZxStatusToPwStatus(zx_status_t status) {
   switch (status) {
     case ZX_OK:
       return PW_STATUS_OK;
-    case ZX_ERR_INVALID_ARGS:
-      return pw::Status::InvalidArgument();
-    case ZX_ERR_ALREADY_EXISTS:
-      return pw::Status::AlreadyExists();
-    case ZX_ERR_PEER_CLOSED:
-      return pw::Status::Unavailable();
+    case ZX_ERR_INTERNAL:
+      return pw::Status::Internal();
     case ZX_ERR_NOT_SUPPORTED:
       return pw::Status::Unimplemented();
+    case ZX_ERR_NO_RESOURCES:
+      return pw::Status::ResourceExhausted();
+    case ZX_ERR_INVALID_ARGS:
+      return pw::Status::InvalidArgument();
+    case ZX_ERR_OUT_OF_RANGE:
+      return pw::Status::OutOfRange();
+    case ZX_ERR_CANCELED:
+      return pw::Status::Cancelled();
+    case ZX_ERR_PEER_CLOSED:
+      return pw::Status::Unavailable();
+    case ZX_ERR_NOT_FOUND:
+      return pw::Status::NotFound();
+    case ZX_ERR_ALREADY_EXISTS:
+      return pw::Status::AlreadyExists();
+    case ZX_ERR_UNAVAILABLE:
+      return pw::Status::Unavailable();
+    case ZX_ERR_ACCESS_DENIED:
+      return pw::Status::PermissionDenied();
+    case ZX_ERR_IO_DATA_LOSS:
+      return pw::Status::DataLoss();
     default:
       return pw::Status::Unknown();
   }
