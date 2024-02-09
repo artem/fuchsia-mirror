eb0c2bf44b8f37abd2c1fc9007db8c06b1b47b01
Adam Perry <adamperry@google.com>
2024-02-09 03:51:56 +0000

    [starnix] Enable kill_job_on_panic tests.
    Register for the event stream and await its successful
    connection before starting the panicking child.
    
    Passed 1000 runs locally.
    
    Multiply: kill_job_on_panic
    Fixed: b/297302221
    Change-Id: I3c89780195a6a35c14c43bb19a550e26d83bdcd6
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/987241
    Reviewed-by: Vickie Cheng <vickiecheng@google.com>
    Fuchsia-Auto-Submit: Adam Perry <adamperry@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/starnix/lib/kill_job_on_panic/tests/integration.rs
+++ src/starnix/lib/kill_job_on_panic/tests/integration.rs
@@ -2,35 +2,41 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+use component_events::{
+    events::{EventStream, ExitStatus, Stopped},
+    matcher::EventMatcher,
+};
 use diagnostics_reader::{ArchiveReader, Logs, Severity};
-use fidl_fuchsia_component::{BinderMarker, EventStreamMarker, EventType};
+use fidl_fuchsia_component::BinderMarker;
 use fuchsia_component::client::connect_to_protocol;
 use futures::StreamExt;
+use tracing::info;
 
 #[fuchsia::test]
 async fn panicking_process_kills_job_with_other_processes() {
-    println!("starting panicking_child");
+    let mut events = EventStream::open().await.unwrap();
+
+    info!("starting panicking_child");
     let _start_panicking_child = connect_to_protocol::<BinderMarker>().unwrap();
 
     // Wait for component stop event to arrive. The component will only be stopped by ELF runner
     // when the "root" process terminates, which in this test will only happen when its job is
     // killed by the kill_job_on_panic library called by the child process it spawns. The test
     // should time out if the hook does not work.
-    println!("waiting for panicking_child to stop");
-    let realm_events = connect_to_protocol::<EventStreamMarker>().unwrap();
-    'wait_for_stop: loop {
-        for event in realm_events.get_next().await.unwrap() {
-            let header = event.header.as_ref().expect("all events have a header");
-            match (header.event_type, header.moniker.as_ref().map(|m| m.as_str())) {
-                (Some(EventType::Stopped), Some("./panicking_child")) => {
-                    break 'wait_for_stop;
-                }
-                _ => continue,
-            }
-        }
-    }
+    info!("waiting for panicking_child to stop");
+    assert_eq!(
+        EventMatcher::ok()
+            .moniker("panicking_child")
+            .wait::<Stopped>(&mut events)
+            .await
+            .unwrap()
+            .result()
+            .unwrap()
+            .status,
+        ExitStatus::Crash(11),
+    );
 
-    println!("panicking_child stopped, checking for expected error message");
+    info!("panicking_child stopped, checking for expected error message");
     let mut logs = ArchiveReader::new().snapshot_then_subscribe::<Logs>().unwrap();
     loop {
         let next_log = logs
@@ -43,6 +49,4 @@ async fn panicking_process_kills_job_with_other_processes() {
             break;
         }
     }
-
-    println!("got both component stop and expected log message, exiting");
 }
