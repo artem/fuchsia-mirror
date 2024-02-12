1bf6863a88ab53792039e642b0f464669417ba12
Sarah Chan <spqchan@google.com>
2024-02-12 18:45:08 +0000

    [fdf] Migrate goldfish control composite
    Migrate the goldfish control controller to composite
    node specs
    
    Tests: Manual
    Bug: 42072838
    Change-Id: Ie23e569c204db3fd0ce27be267ca4baba833e33e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/989595
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Sarah Chan <spqchan@google.com>
    Reviewed-by: Jocelyn Dang <jocelyndang@google.com>

--- src/devices/board/drivers/x86/goldfish-control.cc
+++ src/devices/board/drivers/x86/goldfish-control.cc
@@ -9,7 +9,11 @@
 
 #include <array>
 
-#include "src/devices/board/drivers/x86/goldfish_control_2_bind.h"
+#include <bind/fuchsia/cpp/bind.h>
+#include <bind/fuchsia/goldfish/platform/cpp/bind.h>
+#include <bind/fuchsia/google/platform/cpp/bind.h>
+#include <bind/fuchsia/sysmem/cpp/bind.h>
+
 #include "src/devices/board/drivers/x86/x86.h"
 
 #define PCI_VID_GOLDFISH_ADDRESS_SPACE 0x607D
@@ -17,25 +21,70 @@
 
 namespace x86 {
 
-constexpr zx_device_prop_t props[] = {
-    {BIND_PLATFORM_DEV_VID, 0, PDEV_VID_GOOGLE},
-    {BIND_PLATFORM_DEV_PID, 0, PDEV_PID_GOLDFISH},
-    {BIND_PLATFORM_DEV_DID, 0, PDEV_DID_GOLDFISH_CONTROL},
+const ddk::BindRule kGoldfishPipeRules[] = {
+    ddk::MakeAcceptBindRule(bind_fuchsia::FIDL_PROTOCOL,
+                            bind_fuchsia_goldfish_platform::BIND_FIDL_PROTOCOL_PIPE),
+    ddk::MakeAcceptBindRule(bind_fuchsia::PLATFORM_DEV_VID,
+                            bind_fuchsia_google_platform::BIND_PLATFORM_DEV_VID_GOOGLE),
+    ddk::MakeAcceptBindRule(bind_fuchsia::PLATFORM_DEV_PID,
+                            bind_fuchsia_google_platform::BIND_PLATFORM_DEV_PID_GOLDFISH),
+    ddk::MakeAcceptBindRule(
+        bind_fuchsia::PLATFORM_DEV_DID,
+        bind_fuchsia_google_platform::BIND_PLATFORM_DEV_DID_GOLDFISH_PIPE_CONTROL),
+
 };
 
-static const composite_device_desc_t comp_desc = {
-    .props = props,
-    .props_count = std::size(props),
-    .fragments = goldfish_control_2_fragments,
-    .fragments_count = std::size(goldfish_control_2_fragments),
-    .primary_fragment = "goldfish-pipe",
-    .spawn_colocated = false,
+const device_bind_prop_t kGoldfishPipeProperties[] = {
+    ddk::MakeProperty(bind_fuchsia::FIDL_PROTOCOL,
+                      bind_fuchsia_goldfish_platform::BIND_FIDL_PROTOCOL_PIPE),
+    ddk::MakeProperty(bind_fuchsia::PLATFORM_DEV_VID,
+                      bind_fuchsia_google_platform::BIND_PLATFORM_DEV_VID_GOOGLE),
+    ddk::MakeProperty(bind_fuchsia::PLATFORM_DEV_PID,
+                      bind_fuchsia_google_platform::BIND_PLATFORM_DEV_PID_GOLDFISH),
+    ddk::MakeProperty(bind_fuchsia::PLATFORM_DEV_DID,
+                      bind_fuchsia_google_platform::BIND_PLATFORM_DEV_DID_GOLDFISH_PIPE_CONTROL),
+};
+
+const ddk::BindRule kGoldfishAddressSpaceRules[] = {
+    ddk::MakeAcceptBindRule(bind_fuchsia::FIDL_PROTOCOL,
+                            bind_fuchsia_goldfish_platform::BIND_FIDL_PROTOCOL_ADDRESS_SPACE),
+};
+
+const device_bind_prop_t kGoldfishAddressSpaceProperties[] = {
+    ddk::MakeProperty(bind_fuchsia::FIDL_PROTOCOL,
+                      bind_fuchsia_goldfish_platform::BIND_FIDL_PROTOCOL_ADDRESS_SPACE),
+};
+
+const ddk::BindRule kGoldfishSyncRules[] = {
+    ddk::MakeAcceptBindRule(bind_fuchsia::FIDL_PROTOCOL,
+                            bind_fuchsia_goldfish_platform::BIND_FIDL_PROTOCOL_SYNC),
+};
+
+const device_bind_prop_t kGoldfishSyncProperties[] = {
+    ddk::MakeProperty(bind_fuchsia::FIDL_PROTOCOL,
+                      bind_fuchsia_goldfish_platform::BIND_FIDL_PROTOCOL_SYNC),
+};
+
+const ddk::BindRule kSysmemRules[] = {
+    ddk::MakeAcceptBindRule(bind_fuchsia::FIDL_PROTOCOL,
+                            bind_fuchsia_sysmem::BIND_FIDL_PROTOCOL_DEVICE),
+};
+
+const device_bind_prop_t kSysmemProperties[] = {
+    ddk::MakeProperty(bind_fuchsia::FIDL_PROTOCOL, bind_fuchsia_sysmem::BIND_FIDL_PROTOCOL_DEVICE),
 };
 
 zx_status_t X86::GoldfishControlInit() {
-  zx_status_t status = DdkAddComposite("goldfish-control-2", &comp_desc);
+  zx_status_t status = DdkAddCompositeNodeSpec(
+      "goldfish-control-2",
+      ddk::CompositeNodeSpec(kGoldfishPipeRules, kGoldfishPipeProperties)
+          .AddParentSpec(kGoldfishAddressSpaceRules, kGoldfishAddressSpaceProperties)
+          .AddParentSpec(kGoldfishSyncRules, kGoldfishSyncProperties)
+          .AddParentSpec(kSysmemRules, kSysmemProperties));
+
   if (status != ZX_OK) {
-    zxlogf(ERROR, "%s(goldfish-control-2): DdkAddComposite failed: %d", __func__, status);
+    zxlogf(ERROR, "goldfish-control-2: DdkAddCompositeNodeSpec failed: %s",
+           zx_status_get_string(status));
     return status;
   }
   return status;
