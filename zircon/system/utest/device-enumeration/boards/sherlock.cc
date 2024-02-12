6a72520a4112e9e07b9ede55d6e9593c52ac110b
Sarah Chan <spqchan@google.com>
2024-02-12 17:20:39 +0000

    [fdf] Reland migrate Tas5720 to composite node specs
    Migrate the tas5720 driver to composite node specs.
    This affects sherlock.
    
    The reland removes the changes to add the composites
    to the device enumeration test since while the nodes
    show up, the device-enumeration-test flakes when trying
    to detect them. This issue exists before this change.
    
    Bug: 42072838
    Tests: Manual
    Change-Id: I6b2f9c0804ffed881a78a66c7fb6e4744aefb1be
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/989214
    Reviewed-by: Andres Oportus <andresoportus@google.com>
    Commit-Queue: Sarah Chan <spqchan@google.com>

--- zircon/system/utest/device-enumeration/boards/sherlock.cc
+++ zircon/system/utest/device-enumeration/boards/sherlock.cc
@@ -83,6 +83,14 @@ TEST_F(DeviceEnumerationTest, SherlockTest) {
       "class/temperature/001",
       "class/temperature/002",
 
+      // Audio
+      // TODO(b/324598947): Re-enable once we figure out how to get them to stop flaking
+      // in device-enumeration-test. The device nodes show up, but the device-enumeration-test
+      // sometimes fail to wait for them.
+      // "sys/platform/05_00_2/i2c-0/aml-i2c/i2c/i2c-0-111/audio-tas5720-woofer",
+      // "sys/platform/05_00_2/i2c-0/aml-i2c/i2c/i2c-0-108/audio-tas5720-left-tweeter",
+      // "sys/platform/05_00_2/i2c-0/aml-i2c/i2c/i2c-0-109/audio-tas5720-right-tweeter",
+
       // LCD Bias
       "sys/platform/05:00:2:2/i2c-2/aml-i2c/i2c/i2c-2-62",
 
