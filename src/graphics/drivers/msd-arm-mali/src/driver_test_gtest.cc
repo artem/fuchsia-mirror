163ee0b3b2f70dba1626e5d82e52c334f5c00000
John Bauman <jbauman@google.com>
2024-02-13 22:00:49 +0000

    [msd-arm-mali] Run gtest on a separate thread
    Instead of running on the dispatcher threadpool, which has uncertain
    lifetime, run unit tests on a dedicated thread.
    
    Change-Id: Ibe7783dc8c2b9ccb9fc9cd0ea7dfe63c58a578bc
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/990992
    Fuchsia-Auto-Submit: John Bauman <jbauman@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Craig Stout <cstout@google.com>

--- src/graphics/drivers/msd-arm-mali/src/driver_test_gtest.cc
+++ src/graphics/drivers/msd-arm-mali/src/driver_test_gtest.cc
@@ -25,8 +25,14 @@ zx_status_t magma_indriver_test(ParentDevice* device) {
   const char* argv[kArgc + 1] = {"magma_indriver_test"};
   testing::InitGoogleTest(const_cast<int*>(&kArgc), const_cast<char**>(argv));
 
-  printf("[DRV START=]\n");
-  zx_status_t status = RUN_ALL_TESTS() == 0 ? ZX_OK : ZX_ERR_INTERNAL;
-  printf("[DRV END===]\n[==========]\n");
+  // Use a thread with a well-known lifetime to run the test to ensure all
+  // gtest-internal TLS state is released by the end of testing.
+  zx_status_t status;
+  std::thread test_thread([&]() {
+    printf("[DRV START=]\n");
+    status = RUN_ALL_TESTS() == 0 ? ZX_OK : ZX_ERR_INTERNAL;
+    printf("[DRV END===]\n[==========]\n");
+  });
+  test_thread.join();
   return status;
 }
