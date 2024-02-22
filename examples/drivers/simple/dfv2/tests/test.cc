d12cb2669d8e3b30d234fcc6decd4307e4be5369
Sarah Chan <spqchan@google.com>
2024-02-22 01:33:22 +0000

    [fdf] Migrate simple driver test to the new test fixture
    Migrate the simple driver unit test to the new
    test fixtures
    
    Change-Id: I92a6ae469063409e3b961f48418b2783f4854a2c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/991634
    Reviewed-by: Novin Changizi <novinc@google.com>
    Commit-Queue: Sarah Chan <spqchan@google.com>

--- examples/drivers/simple/dfv2/tests/test.cc
+++ examples/drivers/simple/dfv2/tests/test.cc
@@ -2,71 +2,39 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <lib/async-loop/loop.h>
-#include <lib/async_patterns/testing/cpp/dispatcher_bound.h>
-#include <lib/driver/component/cpp/driver_base.h>
-#include <lib/driver/testing/cpp/driver_lifecycle.h>
-#include <lib/driver/testing/cpp/driver_runtime.h>
-#include <lib/driver/testing/cpp/test_environment.h>
-#include <lib/driver/testing/cpp/test_node.h>
-#include <lib/fdf/env.h>
-#include <lib/fdf/testing.h>
-
-#include <gtest/gtest.h>
+#include <lib/driver/testing/cpp/fixtures/gtest_fixture.h>
 
 #include "examples/drivers/simple/dfv2/simple_driver.h"
 
 namespace testing {
 
-class TestEnvironmentWrapper {
+class SimpleDriverTestEnvironment {
  public:
-  fdf::DriverStartArgs Init() {
-    zx::result start_args_result = node_.CreateStartArgsAndServe();
-    ZX_ASSERT(start_args_result.is_ok());
-
-    zx::result init_result =
-        test_environment_.Initialize(std::move(start_args_result->incoming_directory_server));
-    ZX_ASSERT(init_result.is_ok());
-
-    return std::move(start_args_result.value().start_args);
+  static std::unique_ptr<SimpleDriverTestEnvironment> CreateAndInitialize(
+      fdf::OutgoingDirectory& to_driver_vfs) {
+    // Perform any additional initialization here, such as setting up compat device servers
+    // and FIDL servers.
+    return std::make_unique<SimpleDriverTestEnvironment>();
   }
-
-  void VerifyChildNode() {
-    EXPECT_EQ(1u, node_.children().size());
-    EXPECT_TRUE(node_.children().count("simple_child"));
-  }
-
- private:
-  fdf_testing::TestNode node_{"root"};
-  fdf_testing::TestEnvironment test_environment_;
 };
 
-class SimpleDriverTest : public testing::Test {
+class FixtureConfig final {
  public:
-  void SetUp() override {
-    fdf::DriverStartArgs start_args = environment_.SyncCall(&TestEnvironmentWrapper::Init);
-    zx::result driver = runtime_.RunToCompletion(driver_.Start(std::move(start_args)));
-    EXPECT_EQ(ZX_OK, driver.status_value());
-  }
+  static constexpr bool kDriverOnForeground = true;
+  static constexpr bool kAutoStartDriver = true;
+  static constexpr bool kAutoStopDriver = true;
 
-  async_dispatcher_t* env_dispatcher() { return test_env_dispatcher_->async_dispatcher(); }
-
-  async_patterns::TestDispatcherBound<TestEnvironmentWrapper>& environment() {
-    return environment_;
-  }
-
- private:
-  fdf_testing::DriverRuntime runtime_;
-  fdf::UnownedSynchronizedDispatcher test_env_dispatcher_ = runtime_.StartBackgroundDispatcher();
-
-  async_patterns::TestDispatcherBound<TestEnvironmentWrapper> environment_{env_dispatcher(),
-                                                                           std::in_place};
-
-  fdf_testing::DriverUnderTest<simple::SimpleDriver> driver_;
+  using DriverType = simple::SimpleDriver;
+  using EnvironmentType = SimpleDriverTestEnvironment;
 };
+
+class SimpleDriverTest : public fdf_testing::DriverTestFixture<FixtureConfig> {};
 
 TEST_F(SimpleDriverTest, VerifyChildNode) {
-  environment().SyncCall(&TestEnvironmentWrapper::VerifyChildNode);
+  RunInNodeContext([](fdf_testing::TestNode& node) {
+    EXPECT_EQ(1u, node.children().size());
+    EXPECT_TRUE(node.children().count("simple_child"));
+  });
 }
 
 }  // namespace testing
