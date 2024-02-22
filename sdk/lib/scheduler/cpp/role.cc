706230c3e79e9e0ccefc9aab384615ceecab02d6
Anirudh Mathukumilli <rudymathu@google.com>
2024-02-22 21:04:43 +0000

    [sdk][scheduler] Support service disconnects
    Previously, the CPP library would fail to correctly handle service
    disconnects from the ProfileProvider. This change adds that support by
    automatically reconnecting to the ProfileProvider after a disconnect
    happens.
    
    This is quite unlikely to happen at runtime, but it's better to handle
    it explicitly to avoid unexpected `ZX_ERR_PEER_CLOSED` errors at
    runtime. It also matches the behavior of the rust library.
    
    Bug: 323262398
    Change-Id: I3288e6f2074388ec14a3116e38dab88186870d7a
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/994930
    Fuchsia-Auto-Submit: Anirudh Mathukumilli <rudymathu@google.com>
    Reviewed-by: Corey Tabaka <eieio@google.com>
    Commit-Queue: Anirudh Mathukumilli <rudymathu@google.com>

--- sdk/lib/scheduler/cpp/role.cc
+++ sdk/lib/scheduler/cpp/role.cc
@@ -5,24 +5,69 @@
 #include <fidl/fuchsia.scheduler/cpp/fidl.h>
 #include <lib/scheduler/role.h>
 
+#include <shared_mutex>
+
 #include <sdk/lib/component/incoming/cpp/protocol.h>
 
 namespace {
 
-zx::result<fidl::WireSyncClient<fuchsia_scheduler::ProfileProvider>> ConnectToProfileProvider() {
+class RoleClient {
+ public:
+  RoleClient() = default;
+  zx::result<std::shared_ptr<fidl::WireSyncClient<fuchsia_scheduler::ProfileProvider>>> Connect();
+  void Disconnect();
+
+ private:
+  // Keeps track of a synchronous connection to the ProfileProvider service.
+  // This shared pointer is set to nullptr if the connection has been terminated.
+  std::shared_ptr<fidl::WireSyncClient<fuchsia_scheduler::ProfileProvider>> profile_provider_;
+  std::shared_mutex mutex_;
+};
+
+zx::result<std::shared_ptr<fidl::WireSyncClient<fuchsia_scheduler::ProfileProvider>>>
+RoleClient::Connect() {
+  // Check if we already have a connection to the profile provider, and return it if we do.
+  // This requires acquiring the read lock.
+  {
+    std::shared_lock lock(mutex_);
+    if (profile_provider_ != nullptr) {
+      return zx::ok(profile_provider_);
+    }
+  }
+
+  // At this point, we don't have an existing connection to the profile provider, so we need to
+  // create one. This requires the following set of operations to happen in order.
+  // 1. Acquire the write lock. This will prevent concurrent connection attempts.
+  // 2. Make sure that another thread didn't already connect the client by the time we got the write
+  //    lock.
+  // 3. Establish the connection.
+  std::unique_lock lock(mutex_);
+  if (profile_provider_ != nullptr) {
+    return zx::ok(profile_provider_);
+  }
   auto client_end_result = component::Connect<fuchsia_scheduler::ProfileProvider>();
   if (!client_end_result.is_ok()) {
     return client_end_result.take_error();
   }
-  return zx::ok(fidl::WireSyncClient(std::move(*client_end_result)));
+  profile_provider_ = std::make_shared<fidl::WireSyncClient<fuchsia_scheduler::ProfileProvider>>(
+      fidl::WireSyncClient(std::move(*client_end_result)));
+  return zx::ok(profile_provider_);
 }
+
+void RoleClient::Disconnect() {
+  std::unique_lock lock(mutex_);
+  profile_provider_ = nullptr;
+}
+
+// Stores a persistent connection to the ProfileProvider that reconnects on disconnect.
+static RoleClient role_client{};
 
 zx_status_t SetRole(const zx_handle_t borrowed_handle, std::string_view role) {
 // TODO(https://fxbug.dev/323262398): Remove this check once the necessary API is in the SDK.
 #if __Fuchsia_API_level__ < FUCHSIA_HEAD
   return ZX_ERR_NOT_SUPPORTED;
 #endif  // #if __Fuchsia_API_level__ < FUCHSIA_HEAD
-  static zx::result client = ConnectToProfileProvider();
+  zx::result client = role_client.Connect();
   if (!client.is_ok()) {
     return client.error_value();
   }
@@ -35,8 +80,13 @@ zx_status_t SetRole(const zx_handle_t borrowed_handle, std::string_view role) {
   }
 
   fidl::WireResult result =
-      (*client)->SetProfileByRole(std::move(handle), fidl::StringView::FromExternal(role));
+      (*(*client))->SetProfileByRole(std::move(handle), fidl::StringView::FromExternal(role));
   if (!result.ok()) {
+    // If the service closed the connection, disconnect the client. This will ensure that future
+    // callers of SetRole reconnect to the ProfileProvider service.
+    if (result.status() == ZX_ERR_PEER_CLOSED) {
+      role_client.Disconnect();
+    }
     return result.status();
   }
   return result->status;
