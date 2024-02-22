c0dd73ce201858d98be212b0f6bbfa6e0a5e13af
Hunter Freyer <hjfreyer@google.com>
2024-02-22 18:48:19 +0000

    [versioning] Refactor rust library.
    This change adds a new version of the rust version_history library such
    that access to the history data itself is more locked down. See attached
    bug for justification.
    
    Switching the tree to use the new version will come over a sequence of
    CLs, to avoid too many owners on one CL. Then the old version of the
    library will be deleted.
    
    Bug: 326104955
    
    Change-Id: I6e360d3fda43e43845e9f3beb41f4c703a06cd81
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/992676
    Reviewed-by: Erick Tryzelaar <etryzelaar@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: Hunter Freyer <hjfreyer@google.com>

--- src/lib/versioning/version-history/rust/src/lib.rs
+++ src/lib/versioning/version-history/rust/src/lib.rs
@@ -2,30 +2,49 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use version_history_shared::Status;
-
 use std::{
     array::TryFromSliceError,
     convert::{TryFrom, TryInto},
     fmt,
 };
 
+use itertools::Itertools;
+
+pub use version_history_shared::Status;
+
 /// VERSION_HISTORY is an array of all the known SDK versions.  It is guaranteed
 /// (at compile-time) by the proc_macro to be non-empty.
+///
+/// Deprecated. Use [HISTORY] instead.
+///
+/// TODO: https://fxbug.dev/326104955 - Delete this.
 pub const VERSION_HISTORY: &[Version] = &version_history_macro::declare_version_history!();
 
 /// LATEST_VERSION is the latest known SDK version.
+///
+/// Deprecated. Use [HISTORY] instead.
+///
+/// TODO: https://fxbug.dev/326104955 - Delete this.
 pub const LATEST_VERSION: &Version = &version_history_macro::latest_sdk_version!();
 
 /// SUPPORTED_API_LEVELS are the supported API levels.
+///
+/// Deprecated. Use [HISTORY] instead.
+///
+/// TODO: https://fxbug.dev/326104955 - Delete this.
 pub const SUPPORTED_API_LEVELS: &[Version] = &version_history_macro::get_supported_versions!();
+
+/// Global [VersionHistory] instance generated at build-time from the contents
+/// of `//sdk/version_history.json`.
+pub const HISTORY: VersionHistory =
+    VersionHistory { versions: &version_history_macro::declare_version_history!() };
 
 /// An `ApiLevel` represents an API level of the Fuchsia platform.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Copy, Clone)]
 pub struct ApiLevel(u64);
 
 impl ApiLevel {
-    pub fn from_u64(value: u64) -> Self {
+    pub const fn from_u64(value: u64) -> Self {
         Self(value)
     }
 
@@ -66,8 +85,8 @@ impl From<ApiLevel> for u64 {
     }
 }
 
-/// An `AbiRevision` represents the ABI revision of a Fuchsia Package.
-/// https://fuchsia.dev/fuchsia-src/contribute/governance/rfcs/0135_package_abi_revision?#design
+/// An `AbiRevision` is the 64-bit stamp representing an ABI revision of the
+/// Fuchsia platform.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Copy, Clone)]
 pub struct AbiRevision(u64);
 
@@ -84,7 +103,7 @@ impl AbiRevision {
         self.0.to_le_bytes()
     }
 
-    pub fn from_u64(u: u64) -> AbiRevision {
+    pub const fn from_u64(u: u64) -> AbiRevision {
         AbiRevision(u)
     }
 
@@ -140,27 +159,28 @@ impl TryFrom<&[u8]> for AbiRevision {
     }
 }
 
-/// Version is a mapping between the supported API level and the ABI revisions.
+/// Version represents an API level of the Fuchsia platform API.
 ///
-/// See https://fuchsia.dev/fuchsia-src/contribute/governance/rfcs/0002_platform_versioning for more
-/// details.
+/// See
+/// https://fuchsia.dev/fuchsia-src/contribute/governance/rfcs/0239_platform_versioning_in_practice
+/// for more details.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
 pub struct Version {
-    /// The API level denotes a set of APIs available when building an application for a given
-    /// release of the FUCHSIA IDK.
+    /// The number associated with this API level.
     pub api_level: ApiLevel,
 
-    /// The ABI revision denotes the semantics of the Fuchsia System Interface that an application
-    /// expects the platform to provide.
+    /// The ABI revision associated with this API level.
     pub abi_revision: AbiRevision,
 
-    /// The Status denotes the current status of the API level, either unsupported, supported, in-development.
+    /// The Status denotes the current status of the API level.
     pub status: Status,
 }
 
 impl Version {
     /// Returns true if this version is supported - that is, whether components
     /// targeting this version will be able to run on this device.
+    ///
+    /// TODO: https://fxbug.dev/326104955 - Make this private.
     pub fn is_supported(&self) -> bool {
         match self.status {
             Status::InDevelopment | Status::Supported => true,
@@ -169,6 +189,7 @@ impl Version {
     }
 }
 
+/// TODO: https://fxbug.dev/326104955 - Delete this.
 pub fn version_from_abi_revision(abi_revision: AbiRevision) -> Option<Version> {
     // TODO(https://fxbug.dev/42068452): Store APIs and ABIs in a map instead of a list.
     VERSION_HISTORY.iter().find(|v| v.abi_revision == abi_revision).cloned()
@@ -176,11 +197,15 @@ pub fn version_from_abi_revision(abi_revision: AbiRevision) -> Option<Version> {
 
 /// Returns true if the given abi_revision is listed in the VERSION_HISTORY of
 /// known SDK versions.
+///
+/// TODO: https://fxbug.dev/326104955 - Delete this.
 pub fn is_valid_abi_revision(abi_revision: AbiRevision) -> bool {
     version_from_abi_revision(abi_revision).is_some()
 }
 
 /// Returns true if the given abi_revision is listed in SUPPORTED_API_LEVELS.
+///
+/// TODO: https://fxbug.dev/326104955 - Delete this.
 pub fn is_supported_abi_revision(abi_revision: AbiRevision) -> bool {
     if let Some(version) = version_from_abi_revision(abi_revision) {
         version.is_supported()
@@ -189,17 +214,9 @@ pub fn is_supported_abi_revision(abi_revision: AbiRevision) -> bool {
     }
 }
 
-/// Returns a vector of the API levels in SUPPORTED_API_LEVELS.
-pub fn get_supported_api_levels() -> Vec<ApiLevel> {
-    let mut api_levels: Vec<ApiLevel> = Vec::new();
-    for version in SUPPORTED_API_LEVELS {
-        api_levels.push(version.api_level);
-    }
-
-    return api_levels;
-}
-
 /// Returns a vector of the ABI revisions in SUPPORTED_API_LEVELS.
+///
+/// TODO: https://fxbug.dev/326104955 - Delete this.
 pub fn get_supported_abi_revisions() -> Vec<AbiRevision> {
     let mut abi_revisions: Vec<AbiRevision> = Vec::new();
     for version in SUPPORTED_API_LEVELS {
@@ -209,10 +226,12 @@ pub fn get_supported_abi_revisions() -> Vec<AbiRevision> {
     return abi_revisions;
 }
 
+/// TODO: https://fxbug.dev/326104955 - Delete this.
 pub fn get_latest_abi_revision() -> AbiRevision {
     return LATEST_VERSION.abi_revision;
 }
 
+/// TODO: https://fxbug.dev/326104955 - Delete this.
 pub fn check_abi_revision(abi_revision: Option<AbiRevision>) -> Result<(), AbiRevisionError> {
     let abi_revision = abi_revision.ok_or(AbiRevisionError::Absent)?;
 
@@ -230,6 +249,130 @@ pub fn check_abi_revision(abi_revision: Option<AbiRevision>) -> Result<(), AbiRe
             abi_revision,
             supported_versions: SUPPORTED_API_LEVELS.to_vec(),
         })
+    }
+}
+
+// VersionHistory stores the history of Fuchsia API levels, and lets callers
+// query the support status of API levels and ABI revisions.
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct VersionHistory {
+    versions: &'static [Version],
+}
+
+impl VersionHistory {
+    /// Outside of tests, callers should use the static [HISTORY]
+    /// instance. However, for testing purposes, you may want to build your own
+    /// hermetic "alternate history" that doesn't change over time.
+    ///
+    /// If you're not testing versioning in particular, and you just want an API
+    /// level/ABI revision that works, see
+    /// [get_example_supported_version_for_tests].
+    pub fn new_for_testing(versions: &'static [Version]) -> Self {
+        VersionHistory { versions }
+    }
+
+    /// Components which are not packaged but are "part of the platform"
+    /// nonetheless (e.g. components loaded from bootfs) should be considered to
+    /// have this ABI revision.
+    pub fn get_abi_revision_for_platform_components(&self) -> AbiRevision {
+        self.supported_versions().last().unwrap().abi_revision
+    }
+
+    /// ffx currently presents information suggesting that the platform supports
+    /// a _single_ API level and ABI revision. This is misleading and should be
+    /// fixed. Until we do, we have to choose a particular Version for ffx to
+    /// present.
+    ///
+    /// TODO: https://fxbug.dev/326096999 - Remove this, or turn it into
+    /// something that makes more sense.
+    pub fn get_misleading_version_for_ffx(&self) -> Version {
+        self.supported_versions().last().unwrap()
+    }
+
+    /// The packaging tools currently stamp packages with a default ABI revision
+    /// if the API level or ABI revision were not included on the command line.
+    /// This is dangerous, because it'll be difficult to notice if the wrong ABI
+    /// stamp is being applied. We should remove this default, but until we do,
+    /// we'll use this ABI revision.
+    ///
+    /// TODO: https://fxbug.dev/326095332 - Remove this.
+    pub fn get_default_abi_revision_for_swd(&self) -> AbiRevision {
+        self.supported_versions().last().unwrap().abi_revision
+    }
+
+    /// API level to be used in tests that create components on the fly and need
+    /// to specify a supported API level or ABI revision, but don't particularly
+    /// care which. The returned [Version] will be consistent within a given
+    /// build, but may change from build to build.
+    pub fn get_example_supported_version_for_tests(&self) -> Version {
+        self.supported_versions().last().unwrap()
+    }
+
+    /// Check whether the platform supports building components that target the
+    /// given API level, and if so, returns the ABI revision associated with
+    /// that API level.
+    ///
+    /// TODO: https://fxbug.dev/326096347 - This doesn't actually check that the
+    /// API level is supported, merely that it exists. At time of writing, this
+    /// is the behavior implemented by ffx package, so I'm keeping it consistent
+    /// in the interest of no-op refactoring.
+    pub fn check_api_level_for_build(
+        &self,
+        api_level: ApiLevel,
+    ) -> Result<AbiRevision, ApiLevelError> {
+        if let Some(version) = self.version_from_api_level(api_level) {
+            Ok(version.abi_revision)
+        } else {
+            Err(ApiLevelError::Unknown {
+                api_level,
+                supported: self.supported_versions().map(|v| v.api_level).collect(),
+            })
+        }
+    }
+
+    /// Check whether the operating system supports running components that
+    /// target the given ABI revision.
+    pub fn check_abi_revision_for_runtime(
+        &self,
+        abi_revision: AbiRevision,
+    ) -> Result<(), AbiRevisionError> {
+        if let Some(version) = self.version_from_abi_revision(abi_revision) {
+            if version.is_supported() {
+                Ok(())
+            } else {
+                Err(AbiRevisionError::Unsupported {
+                    version,
+                    supported_versions: self.supported_versions().collect(),
+                })
+            }
+        } else {
+            Err(AbiRevisionError::Unknown {
+                abi_revision,
+                supported_versions: self.supported_versions().collect(),
+            })
+        }
+    }
+
+    /// The packaging tools currently allow specifying an ABI revision directly
+    /// on the command line, and validate that that ABI revision _exists_, but
+    /// nothing else about it.
+    ///
+    /// TODO: https://fxbug.dev/326095523 - Figure out if this behavior makes
+    /// sense, or if ffx package should even accept --abi-revision.
+    pub fn abi_revision_matches_some_api_level(&self, abi_revision: AbiRevision) -> bool {
+        self.version_from_abi_revision(abi_revision).is_some()
+    }
+
+    fn supported_versions(&self) -> impl Iterator<Item = Version> + '_ {
+        self.versions.iter().filter(|v| v.is_supported()).cloned()
+    }
+
+    fn version_from_abi_revision(&self, abi_revision: AbiRevision) -> Option<Version> {
+        self.versions.iter().find(|v| v.abi_revision == abi_revision).cloned()
+    }
+
+    pub fn version_from_api_level(&self, api_level: ApiLevel) -> Option<Version> {
+        self.versions.iter().find(|v| v.api_level == api_level).cloned()
     }
 }
 
@@ -255,7 +398,7 @@ impl std::fmt::Display for AbiRevisionError {
                 write!(f, "The following API levels are supported: ")?;
 
                 for (idx, version) in supported_versions.iter().enumerate() {
-                    write!(f, "{} ({})", version.api_level, version.abi_revision)?;
+                    write!(f, "\n└── {} ({})", version.api_level, version.abi_revision)?;
                     if idx != supported_versions.len() - 1 {
                         write!(f, ", ")?;
                     }
@@ -276,10 +419,48 @@ impl std::fmt::Display for AbiRevisionError {
             AbiRevisionError::Unsupported { version, supported_versions } => {
                 write!(
                     f,
-                    "This ABI targets API {} ({}), which is no longer supported. ",
+                    "Component targets API {} ({}), which is no longer supported. ",
                     version.api_level, version.abi_revision
                 )?;
                 write_supported_versions(f, supported_versions)
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ApiLevelError {
+    /// The user attempted to build a component targeting an API level that was
+    /// not recognized.
+    Unknown { api_level: ApiLevel, supported: Vec<ApiLevel> },
+
+    /// The user attempted to build a component targeting an API level that was
+    /// recognized, but is not supported for building.
+    Unsupported { version: Version, supported: Vec<ApiLevel> },
+}
+
+impl std::error::Error for ApiLevelError {}
+
+impl std::fmt::Display for ApiLevelError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ApiLevelError::Unknown { api_level, supported } => {
+                write!(
+                    f,
+                    "Unknown target API level: {}. The SDK may be too old to support it?
+The following API levels are supported: {}",
+                    api_level,
+                    supported.iter().join(",")
+                )
+            }
+            ApiLevelError::Unsupported { version, supported } => {
+                write!(
+                    f,
+                    "Building components targeting API level {} is no longer supported.
+The following API levels are supported: {}",
+                    version.api_level,
+                    supported.iter().join(",")
+                )
             }
         }
     }
@@ -341,6 +522,139 @@ mod tests {
             VERSION_HISTORY.iter().all(|v| v.abi_revision != AbiRevision::from_u64(*u))
         )) {
             assert!(!is_valid_abi_revision(AbiRevision::from_u64(u)))
+        }
+    }
+
+    pub const FAKE_VERSION_HISTORY: VersionHistory = VersionHistory {
+        versions: &[
+            Version {
+                api_level: ApiLevel::from_u64(4),
+                abi_revision: AbiRevision::from_u64(0xabcd0004),
+                status: Status::Unsupported,
+            },
+            Version {
+                api_level: ApiLevel::from_u64(5),
+                abi_revision: AbiRevision::from_u64(0xabcd0005),
+                status: Status::Supported,
+            },
+            Version {
+                api_level: ApiLevel::from_u64(6),
+                abi_revision: AbiRevision::from_u64(0xabcd0006),
+                status: Status::Supported,
+            },
+            Version {
+                api_level: ApiLevel::from_u64(7),
+                abi_revision: AbiRevision::from_u64(0xabcd0007),
+                status: Status::InDevelopment,
+            },
+        ],
+    };
+
+    #[test]
+    fn test_proc_macro_worked2() {
+        let expected = version_history_from_shared();
+        assert_eq!(expected, HISTORY.versions);
+    }
+
+    #[test]
+    fn test_example_supported_abi_revision_is_supported() {
+        let example_version = HISTORY.get_example_supported_version_for_tests();
+
+        assert_eq!(
+            HISTORY.check_api_level_for_build(example_version.api_level),
+            Ok(example_version.abi_revision)
+        );
+
+        HISTORY
+            .check_abi_revision_for_runtime(example_version.abi_revision)
+            .expect("you said it was supported!");
+    }
+
+    #[test]
+    fn test_check_abi_revision() {
+        let supported_versions: Vec<Version> =
+            FAKE_VERSION_HISTORY.versions[1..4].iter().cloned().collect();
+
+        assert_eq!(
+            FAKE_VERSION_HISTORY.check_abi_revision_for_runtime(0x1234.into()),
+            Err(AbiRevisionError::Unknown {
+                abi_revision: 0x1234.into(),
+
+                supported_versions: supported_versions.clone()
+            })
+        );
+
+        assert_eq!(
+            FAKE_VERSION_HISTORY.check_abi_revision_for_runtime(0xabcd0004.into()),
+            Err(AbiRevisionError::Unsupported {
+                version: FAKE_VERSION_HISTORY.versions[0].clone(),
+
+                supported_versions: supported_versions.clone()
+            })
+        );
+
+        FAKE_VERSION_HISTORY
+            .check_abi_revision_for_runtime(0xabcd0005.into())
+            .expect("level 5 should be supported");
+        FAKE_VERSION_HISTORY
+            .check_abi_revision_for_runtime(0xabcd0007.into())
+            .expect("level 7 should be supported");
+    }
+
+    #[test]
+    fn test_check_api_level() {
+        let supported: Vec<ApiLevel> = vec![5.into(), 6.into(), 7.into()];
+
+        assert_eq!(
+            FAKE_VERSION_HISTORY.check_api_level_for_build(42.into()),
+            Err(ApiLevelError::Unknown { api_level: 42.into(), supported: supported.clone() })
+        );
+
+        // This currently says API 4 is supported, but it shouldn't.
+        //
+        // TODO: https://fxbug.dev/326096347 - Uncomment this once it passes.
+        //
+        // assert_eq!(
+        //     FAKE_VERSION_HISTORY.check_api_level_for_build(4.into()),
+        //     Err(ApiLevelError::Unsupported {
+        //         version: FAKE_VERSION_HISTORY.versions[0],
+
+        //         supported: supported.clone()
+        //     })
+        // );
+
+        assert_eq!(FAKE_VERSION_HISTORY.check_api_level_for_build(6.into()), Ok(0xabcd0006.into()));
+        assert_eq!(FAKE_VERSION_HISTORY.check_api_level_for_build(7.into()), Ok(0xabcd0007.into()));
+    }
+
+    #[test]
+    fn test_various_getters() {
+        assert_eq!(
+            FAKE_VERSION_HISTORY.get_abi_revision_for_platform_components(),
+            0xabcd0007.into()
+        );
+        assert_eq!(FAKE_VERSION_HISTORY.get_default_abi_revision_for_swd(), 0xabcd0007.into());
+        assert_eq!(
+            FAKE_VERSION_HISTORY.get_example_supported_version_for_tests(),
+            FAKE_VERSION_HISTORY.versions[3].clone()
+        );
+        assert_eq!(
+            FAKE_VERSION_HISTORY.get_misleading_version_for_ffx(),
+            FAKE_VERSION_HISTORY.versions[3].clone()
+        );
+    }
+
+    #[test]
+    fn test_valid_abi_checker() {
+        for i in 0xabcd0004..=0xabcd0007 {
+            assert!(
+                FAKE_VERSION_HISTORY.abi_revision_matches_some_api_level(AbiRevision::from_u64(i))
+            );
+        }
+        for i in 0xabcd0008..=0xabcd000a {
+            assert!(
+                !FAKE_VERSION_HISTORY.abi_revision_matches_some_api_level(AbiRevision::from_u64(i))
+            );
         }
     }
 }
