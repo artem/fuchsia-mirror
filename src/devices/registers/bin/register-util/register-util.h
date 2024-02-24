62f728d8bb7c0808544dc9c278b09cf07c89003b
Suraj Malhotra <surajmalhotra@google.com>
2024-02-24 00:43:09 +0000

    [register-util] Move register-util to registers area
    Change-Id: I2ba6e228058f75899817dbe703cd8bb796261e76
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/995686
    Reviewed-by: Aaron Wood <aaronwood@google.com>
    Fuchsia-Auto-Submit: Suraj Malhotra <surajmalhotra@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Ruby Zhuang <rdzhuang@google.com>

--- src/devices/registers/bin/register-util/register-util.h
+++ src/devices/registers/bin/register-util/register-util.h
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef SRC_DEVICES_BIN_REGISTER_UTIL_REGISTER_UTIL_H_
-#define SRC_DEVICES_BIN_REGISTER_UTIL_REGISTER_UTIL_H_
+#ifndef SRC_DEVICES_REGISTERS_BIN_REGISTER_UTIL_REGISTER_UTIL_H_
+#define SRC_DEVICES_REGISTERS_BIN_REGISTER_UTIL_REGISTER_UTIL_H_
 
 #include <fidl/fuchsia.hardware.registers/cpp/wire.h>
 
 int run(int argc, const char** argv, fidl::ClientEnd<fuchsia_hardware_registers::Device> channel);
 
-#endif  // SRC_DEVICES_BIN_REGISTER_UTIL_REGISTER_UTIL_H_
+#endif  // SRC_DEVICES_REGISTERS_BIN_REGISTER_UTIL_REGISTER_UTIL_H_
