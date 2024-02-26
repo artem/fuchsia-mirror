45d0050201efe97dbbb59bc4ee9206ab2af44926
Chris Drouillard <cdrllrd@google.com>
2024-02-26 14:31:16 +0000

    [storage][trace] Ensure tracing always compiles
    Refactor the storage tracing library to make the macros always expand
    to function calls. This ensures that the tracing code compiles even when
    tracing is disabled.
    
    This was verified to be working by making invalid trace calls in fxfs
    and checking that they didn't compile both with storage_enable_tracing
    enabled and disabled.
    
    Fixed: b/326268022
    Change-Id: Ifd4c7b8e40b9f6e8ff9f7442df46398bbb01e8bb
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/994854
    Commit-Queue: Chris Drouillard <cdrllrd@google.com>
    Reviewed-by: Martin Lindsay <mlindsay@google.com>

--- src/storage/lib/trace/src/lib.rs
+++ src/storage/lib/trace/src/lib.rs
@@ -5,68 +5,159 @@
 #[cfg(feature = "tracing")]
 mod fuchsia;
 #[cfg(feature = "tracing")]
-pub use fuchsia::*;
+pub mod __backend {
+    pub use crate::fuchsia::*;
+}
 
 #[cfg(not(feature = "tracing"))]
 mod noop;
 #[cfg(not(feature = "tracing"))]
-pub use noop::*;
+pub mod __backend {
+    pub use crate::noop::*;
+}
+
+pub use __backend::{Id, TraceFutureExt};
+
+/// Convenience macro for creating a trace duration event from this macro invocation to the end of
+/// the current scope.
+///
+/// See `fuchsia_trace::duration!` for more details.
+#[macro_export]
+macro_rules! duration {
+    ($category:expr, $name:expr $(, $key:expr => $val:expr)*) => {
+        let args;
+        let _scope = if $crate::__backend::TraceCategoryContext::acquire($category).is_some() {
+            args = [$($crate::__backend::ArgValue::of($key, $val)),*];
+            Some($crate::__backend::duration($category, $name, &args))
+        } else {
+            None
+        };
+    }
+}
+
+/// Writes a flow begin event with the specified id.
+///
+/// See `fuchsia_trace::flow_begin!` for more details.
+#[macro_export]
+macro_rules! flow_begin {
+    ($category:expr, $name:expr, $flow_id:expr $(, $key:expr => $val:expr)*) => {
+        if let Some(context) = $crate::__backend::TraceCategoryContext::acquire($category) {
+            $crate::__backend::flow_begin(&context, $name, ($flow_id).into(),
+                                          &[$($crate::__backend::ArgValue::of($key, $val)),*])
+        }
+    }
+}
+
+/// Writes a flow step event with the specified id.
+///
+/// See `fuchsia_trace::flow_step!` for more details.
+#[macro_export]
+macro_rules! flow_step {
+    ($category:expr, $name:expr, $flow_id:expr $(, $key:expr => $val:expr)*) => {
+        if let Some(context) = $crate::__backend::TraceCategoryContext::acquire($category) {
+            $crate::__backend::flow_step(&context, $name, ($flow_id).into(),
+                                         &[$($crate::__backend::ArgValue::of($key, $val)),*])
+        }
+    }
+}
+
+/// Writes a flow end event with the specified id.
+///
+/// See `fuchsia_trace::flow_end!` for more details.
+#[macro_export]
+macro_rules! flow_end {
+    ($category:expr, $name:expr, $flow_id:expr $(, $key:expr => $val:expr)*) => {
+        if let Some(context) = $crate::__backend::TraceCategoryContext::acquire($category) {
+            $crate::__backend::flow_end(&context, $name, ($flow_id).into(),
+                                        &[$($crate::__backend::ArgValue::of($key, $val)),*])
+        }
+    }
+}
+
+/// Constructs a `TraceFutureArgs` object to be passed to `TraceFutureExt::trace`.
+///
+/// See `fuchsia_trace::trace_future_args!` for more details.
+#[macro_export]
+macro_rules! trace_future_args {
+    ($category:expr, $name:expr $(, $key:expr => $val:expr)*) => {{
+        let context = $crate::__backend::TraceCategoryContext::acquire($category);
+        let args = if context.is_some() {
+            vec![$($crate::__backend::ArgValue::of($key, $val)),*]
+        } else {
+            vec![]
+        };
+        $crate::__backend::trace_future_args(context, $category, $name, args)
+    }}
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::TraceFutureExt;
+    use crate::{duration, flow_begin, flow_end, trace_future_args, TraceFutureExt};
 
     #[fuchsia::test]
     fn test_duration() {
-        let tace_only_var = 6;
-        crate::duration!(c"category", c"name");
-        crate::duration!(c"category", c"name", "arg" => 5);
-        crate::duration!(c"category", c"name", "arg" => 5, "arg2" => tace_only_var);
+        let trace_only_var = 6;
+        duration!(c"category", c"name");
+        duration!(c"category", c"name", "arg" => 5);
+        duration!(c"category", c"name", "arg" => 5, "arg2" => trace_only_var);
     }
 
     #[fuchsia::test]
     fn test_flow_begin() {
-        let tace_only_var = 6;
+        let trace_only_var = 6;
         let flow_id = 5u64;
-        crate::flow_begin!(c"category", c"name", flow_id);
-        crate::flow_begin!(c"category", c"name", flow_id, "arg" => 5);
-        crate::flow_begin!(c"category", c"name", flow_id, "arg" => 5, "arg2" => tace_only_var);
+        flow_begin!(c"category", c"name", flow_id);
+        flow_begin!(c"category", c"name", flow_id, "arg" => 5);
+        flow_begin!(c"category", c"name", flow_id, "arg" => 5, "arg2" => trace_only_var);
     }
 
     #[fuchsia::test]
     fn test_flow_step() {
-        let tace_only_var = 6;
+        let trace_only_var = 6;
         let flow_id = 5u64;
-        crate::flow_step!(c"category", c"name", flow_id);
-        crate::flow_step!(c"category", c"name", flow_id, "arg" => 5);
-        crate::flow_step!(c"category", c"name", flow_id, "arg" => 5, "arg2" => tace_only_var);
+        flow_step!(c"category", c"name", flow_id);
+        flow_step!(c"category", c"name", flow_id, "arg" => 5);
+        flow_step!(c"category", c"name", flow_id, "arg" => 5, "arg2" => trace_only_var);
     }
 
     #[fuchsia::test]
     fn test_flow_end() {
-        let tace_only_var = 6;
+        let trace_only_var = 6;
         let flow_id = 5u64;
-        crate::flow_end!(c"category", c"name", flow_id);
-        crate::flow_end!(c"category", c"name", flow_id, "arg" => 5);
-        crate::flow_end!(c"category", c"name", flow_id, "arg" => 5, "arg2" => tace_only_var);
+        flow_end!(c"category", c"name", flow_id);
+        flow_end!(c"category", c"name", flow_id, "arg" => 5);
+        flow_end!(c"category", c"name", flow_id, "arg" => 5, "arg2" => trace_only_var);
     }
 
     #[fuchsia::test]
     async fn test_trace_future() {
-        let value = async move { 5 }.trace(crate::trace_future_args!(c"category", c"name")).await;
+        let value = async move { 5 }.trace(trace_future_args!(c"category", c"name")).await;
         assert_eq!(value, 5);
 
-        let value = async move { 5 }
-            .trace(crate::trace_future_args!(c"category", c"name", "arg1" => 6))
-            .await;
+        let value =
+            async move { 5 }.trace(trace_future_args!(c"category", c"name", "arg1" => 6)).await;
         assert_eq!(value, 5);
 
-        let tace_only_var = 7;
+        let trace_only_var = 7;
         let value = async move { 5 }
-            .trace(
-                crate::trace_future_args!(c"category", c"name", "arg1" => 6, "ar2" => tace_only_var),
-            )
+            .trace(trace_future_args!(c"category", c"name", "arg1" => 6, "ar2" => trace_only_var))
             .await;
         assert_eq!(value, 5);
+    }
+
+    #[fuchsia::test]
+    fn test_arg_types() {
+        duration!(c"category", c"name", "bool" => true);
+        duration!(c"category", c"name", "i32" => 5i32, "u32" => 5u32);
+        duration!(c"category", c"name", "i64" => 5i64, "u64" => 5u64);
+        duration!(c"category", c"name", "isize" => 5isize, "usize" => 5usize);
+        duration!(c"category", c"name", "f64" => 5f64);
+
+        let owned_str = "test-str".to_owned();
+        duration!(c"category", c"name", "str" => owned_str.as_str());
+
+        let mut value = 5u64;
+        duration!(c"category", c"name", "const-ptr" => &value as *const u64);
+        duration!(c"category", c"name", "mut-ptr" => &mut value as *mut u64);
     }
 }
--- src/storage/lib/trace/src/noop.rs
+++ src/storage/lib/trace/src/noop.rs
@@ -2,7 +2,68 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use std::future::Future;
+//! Noop implementations of the functions in `fuchsia_trace` that are called from the tracing
+//! macros. These functions are used instead of omitting the calls within the macro to ensure that
+//! the macro invocations still compile even when tracing is disabled. The tests in this library are
+//! built with tracing enabled and with tracing disabled which will ensure that these functions
+//! can't diverge from the real ones.
+
+use std::{ffi::CStr, future::Future, marker::PhantomData};
+
+pub struct TraceCategoryContext(());
+
+impl TraceCategoryContext {
+    pub fn acquire(_category: &'static CStr) -> Option<Self> {
+        None
+    }
+}
+
+pub struct DurationScope<'a>(PhantomData<&'a ()>);
+
+#[inline]
+pub const fn duration<'a>(
+    _category: &'static CStr,
+    _name: &'static CStr,
+    _args: &'a [Arg<'_>],
+) -> DurationScope<'a> {
+    DurationScope(PhantomData)
+}
+
+pub struct Id(());
+
+impl From<u64> for Id {
+    #[inline]
+    fn from(_id: u64) -> Self {
+        Self(())
+    }
+}
+
+#[inline]
+pub const fn flow_begin(
+    _context: &TraceCategoryContext,
+    _name: &'static CStr,
+    _flow_id: Id,
+    _args: &[Arg<'_>],
+) {
+}
+
+#[inline]
+pub const fn flow_step(
+    _context: &TraceCategoryContext,
+    _name: &'static CStr,
+    _flow_id: Id,
+    _args: &[Arg<'_>],
+) {
+}
+
+#[inline]
+pub const fn flow_end(
+    _context: &TraceCategoryContext,
+    _name: &'static CStr,
+    _flow_id: Id,
+    _args: &[Arg<'_>],
+) {
+}
 
 pub struct TraceFutureArgs {
     pub _use_trace_future_args: (),
@@ -16,55 +77,56 @@ pub trait TraceFutureExt: Future + Sized {
 
 impl<T: Future + Sized> TraceFutureExt for T {}
 
-#[macro_export]
-macro_rules! duration {
-    ($category:expr, $name:expr $(, $key:expr => $val:expr)*) => {
-        $crate::__ignore_unused_variables!($($val),*);
-    }
+#[inline]
+pub fn trace_future_args<'a>(
+    _context: Option<TraceCategoryContext>,
+    _category: &'static CStr,
+    _name: &'static CStr,
+    _args: Vec<Arg<'a>>,
+) -> TraceFutureArgs {
+    TraceFutureArgs { _use_trace_future_args: () }
 }
 
-#[macro_export]
-macro_rules! flow_begin {
-    ($category:expr, $name:expr, $flow_id:expr $(, $key:expr => $val:expr)*) => {
-        $crate::__ignore_unused_variables!($flow_id);
-        $crate::__ignore_unused_variables!($($val),*);
-    }
+pub struct Arg<'a>(PhantomData<&'a ()>);
+
+pub trait ArgValue {
+    fn of<'a>(key: &'a str, value: Self) -> Arg<'a>
+    where
+        Self: 'a;
 }
 
-#[macro_export]
-macro_rules! flow_step {
-    ($category:expr, $name:expr, $flow_id:expr $(, $key:expr => $val:expr)*) => {
-        $crate::__ignore_unused_variables!($flow_id);
-        $crate::__ignore_unused_variables!($($val),*);
-    }
-}
-
-#[macro_export]
-macro_rules! flow_end {
-    ($category:expr, $name:expr, $flow_id:expr $(, $key:expr => $val:expr)*) => {
-        $crate::__ignore_unused_variables!($flow_id);
-        $crate::__ignore_unused_variables!($($val),*);
-    }
-}
-
-#[macro_export]
-macro_rules! trace_future_args {
-    ($category:expr, $name:expr $(, $key:expr => $val:expr)*) => {
-        {
-            $crate::__ignore_unused_variables!($($val),*);
-            $crate::TraceFutureArgs {
-                _use_trace_future_args: ()
-            }
-        }
-    };
-}
-
-#[doc(hidden)]
-#[macro_export]
-macro_rules! __ignore_unused_variables {
-    ($($val:expr),*) => {
+macro_rules! impl_arg_value {
+    ($($type:ty),*) => {
         $(
-            { let _ = &$val; }
+            impl ArgValue for $type {
+                #[inline]
+                fn of<'a>(_key: &'a str, _value: Self) -> Arg<'a>
+                where
+                    Self: 'a,
+                {
+                    Arg(PhantomData)
+                }
+            }
         )*
     };
 }
+
+impl_arg_value!((), bool, i32, u32, i64, u64, isize, usize, f64);
+
+macro_rules! impl_generic_arg_value {
+    ($(($type:ty, $generics:tt)),*) => {
+        $(
+        impl<$generics> ArgValue for $type {
+            #[inline]
+            fn of<'a>(_key: &'a str, _value: Self) -> Arg<'a>
+            where
+                Self: 'a,
+            {
+                Arg(PhantomData)
+            }
+        }
+    )*
+    };
+}
+
+impl_generic_arg_value!((*const T, T), (*mut T, T), (&'b str, 'b));
