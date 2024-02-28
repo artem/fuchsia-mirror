d0a2c9d53fc19ad6c80aa920b81541fca927674d
Jiaming Li <lijiaming@google.com>
2024-02-28 01:00:52 +0000

    Revert "[board-driver][sdk] Initial change for example board driver test"
    This reverts commit aa7ba9d998556f7124fe8e2edc6604c9c1592d01.
    
    Reason for revert: This breaks the coverage builder https://luci-milo.appspot.com/ui/p/fuchsia/builders/ci/fuchsia-coverage/b8754976595177294673/overview
    
    Original change's description:
    > [board-driver][sdk] Initial change for example board driver test
    >
    > Change-Id: I491bd8a745a949427985ab2b10767f4cc366e888
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/991643
    > Reviewed-by: Puneetha Ramachandra <puneetha@google.com>
    > Reviewed-by: Chase Latta <chaselatta@google.com>
    > Reviewed-by: Braden Kell <bradenkell@google.com>
    > Commit-Queue: Jiaming Li <lijiaming@google.com>
    > Reviewed-by: Jay Zhuang <jayzhuang@google.com>
    
    Change-Id: Iff865a9e38eb3d1abfbdc619702ed26a619eafb8
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/998661
    Fuchsia-Auto-Submit: Jiaming Li <lijiaming@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Jay Zhuang <jayzhuang@google.com>

--- sdk/lib/driver/devicetree/testing/board-test-helper.cc
+++ sdk/lib/driver/devicetree/testing/board-test-helper.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "lib/driver/devicetree/testing/board-test-helper.h"
+#include "sdk/lib/driver/devicetree/testing/board-test-helper.h"
 
 #include <fcntl.h>
 #include <fidl/fuchsia.boot/cpp/wire.h>
