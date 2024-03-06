b4481aa0fb83d075220e47a9f0aab3bfb16d21a1
Marco Vanotti <mvanotti@google.com>
2024-03-06 22:57:12 +0000

    [starnix/tests] Fix leaking fd in ScopedTempDir.
    There was an fd leakage in ScopedTempDir's `RecursiveUnmountAndRemove`
    function. When opening a directory with `opendir`, `closedir` needs to
    be called afterwards.
    
    Change-Id: If1821e8fe0c727fc9d8589225054047c9be2e0f4
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1002073
    Reviewed-by: James Robinson <jamesr@google.com>
    Commit-Queue: Marco Vanotti <mvanotti@google.com>

--- src/starnix/tests/syscalls/cpp/test_helper_test.cc
+++ src/starnix/tests/syscalls/cpp/test_helper_test.cc
@@ -4,6 +4,14 @@
 
 #include "src/starnix/tests/syscalls/cpp/test_helper.h"
 
+#include <errno.h>
+#include <fcntl.h>
+#include <lib/fit/defer.h>
+#include <sys/resource.h>
+#include <unistd.h>
+
+#include <cstring>
+
 #include <gtest/gtest.h>
 
 namespace {
@@ -14,6 +22,29 @@ TEST(TestHelperTest, DetectFailingChildren) {
   helper.RunInForkedProcess([] { FAIL() << "Expected failure"; });
 
   EXPECT_FALSE(helper.WaitForChildren());
+}
+
+TEST(ScopedTestDirTest, DoesntLeakFileDescriptors) {
+  test_helper::ForkHelper helper;
+
+  helper.RunInForkedProcess([] {
+    struct rlimit limit;
+    ASSERT_EQ(getrlimit(RLIMIT_NOFILE, &limit), 0) << "getrlimit: " << std::strerror(errno);
+    auto cleanup = fit::defer([limit]() {
+      EXPECT_EQ(setrlimit(RLIMIT_NOFILE, &limit), 0) << "setrlimit: " << std::strerror(errno);
+    });
+
+    struct rlimit new_limit = {.rlim_cur = 100, .rlim_max = limit.rlim_max};
+    ASSERT_EQ(setrlimit(RLIMIT_NOFILE, &new_limit), 0) << "setrlimit: " << std::strerror(errno);
+
+    for (size_t i = 0; i <= new_limit.rlim_cur; i++) {
+      test_helper::ScopedTempDir scoped_dir;
+      test_helper::ScopedFD mem_fd(test_helper::MemFdCreate("try_create", O_WRONLY));
+      EXPECT_TRUE(mem_fd.is_valid()) << "memfd_create: " << std::strerror(errno);
+    }
+  });
+
+  EXPECT_TRUE(helper.WaitForChildren());
 }
 
 }  // namespace
