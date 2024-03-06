606f994c89c9fe2b28602b94f832d29c20c892ed
Gary Bressler <geb@google.com>
2024-03-06 19:56:56 +0000

    [trf] Migrate intl test to Dictionary (phase 2/2)
    Phase 2: Delete old CreateRealm (now unused)
    
    Fixed: 319912911
    Change-Id: I479dba156c9921cd61aa215b97c4554357f05f12
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1000415
    Reviewed-by: Kendal Harland <kjharland@google.com>
    Commit-Queue: Gary Bressler <geb@google.com>

--- sdk/ctf/tests/fidl/fuchsia.intl/testing/realm-factory/src/main.rs
+++ sdk/ctf/tests/fidl/fuchsia.intl/testing/realm-factory/src/main.rs
@@ -10,7 +10,6 @@ use {
     fidl_fuchsia_io as fio,
     fidl_fuchsia_settings::IntlMarker,
     fidl_fuchsia_stash::StoreMarker,
-    fuchsia_async as fasync,
     fuchsia_component::server::ServiceFs,
     fuchsia_component_test::{
         Capability, ChildOptions, LocalComponentHandles, RealmBuilder, RealmInstance, Ref, Route,
@@ -36,18 +35,9 @@ async fn serve_realm_factory(stream: RealmFactoryRequestStream) {
 }
 
 async fn handle_request_stream(mut stream: RealmFactoryRequestStream) -> Result<()> {
-    let mut task_group = fasync::TaskGroup::new();
     let mut realms = vec![];
     while let Ok(Some(request)) = stream.try_next().await {
         match request {
-            RealmFactoryRequest::CreateRealm { options, realm_server, responder } => {
-                let realm = create_realm(options).await?;
-                let request_stream = realm_server.into_stream()?;
-                task_group.spawn(async move {
-                    realm_proxy::service::serve(realm, request_stream).await.unwrap();
-                });
-                responder.send(Ok(()))?;
-            }
             RealmFactoryRequest::CreateRealm2 { options, dictionary, responder } => {
                 let realm = create_realm(options).await?;
                 realm.root.controller().get_exposed_dictionary(dictionary).await?.unwrap();
@@ -58,8 +48,6 @@ async fn handle_request_stream(mut stream: RealmFactoryRequestStream) -> Result<
             RealmFactoryRequest::_UnknownMethod { .. } => unreachable!(),
         }
     }
-
-    task_group.join().await;
     Ok(())
 }
 
