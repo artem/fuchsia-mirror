122052252243ae9846785942cb12c7843fedcefb
Ruby Zhuang <rdzhuang@google.com>
2024-03-07 23:33:40 +0000

    [aml-usb-crg-phy-v2] Delete aml-usb-crg-phy-v2
    Bug: b/310228406
    
    Change-Id: Ica430bef6bc2b487ea8d6bceb8422fd1e5a5bb71
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/999528
    API-Review: Christopher Anderson <cja@google.com>
    Fuchsia-Auto-Submit: Ruby Zhuang <rdzhuang@google.com>
    Reviewed-by: Sarah Chan <spqchan@google.com>
    Reviewed-by: Sam Hansen <hansens@google.com>
    Commit-Queue: Ruby Zhuang <rdzhuang@google.com>

--- zircon/system/ulib/ddk-platform-defs/include/lib/ddk/platform-defs.h
+++ zircon/system/ulib/ddk-platform-defs/include/lib/ddk/platform-defs.h
@@ -168,7 +168,7 @@
 #define PDEV_DID_AMLOGIC_A5_CLK      0x2C
 // PDEV_DID_VIM3_USB_PHY was 0x2D
 #define PDEV_DID_AMLOGIC_RTC         0x2E
-#define PDEV_DID_AML_USB_CRG_PHY_V2  0x2F
+// PDEV_DID_AML_USB_CRG_PHY_V2 was 0x2F
 #define PDEV_DID_USB_CRG_UDC         0x30  // Corigine USB device controller
 #define PDEV_DID_AMLOGIC_DSP         0x31
 #define PDEV_DID_AMLOGIC_A1_CLK      0x32
