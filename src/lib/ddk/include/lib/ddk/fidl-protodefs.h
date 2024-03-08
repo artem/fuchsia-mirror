2faedbafc10d7f51d32d62beb696d848f9fcfa8d
Novin Changizi <novinc@google.com>
2024-03-08 00:27:19 +0000

    [audio][driver] Remove fidl-protodef for dsp
    Removes the fidl-protodef entry for the fuchsia.audio's DSP entry. This
    bind property (and the library it is in) aren't used anywhere.
    
    Change-Id: Ia12a4e366fa15d6186c650f98953e2e10d83ddd0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1003820
    Reviewed-by: Sarah Chan <spqchan@google.com>
    Fuchsia-Auto-Submit: Novin Changizi <novinc@google.com>
    Commit-Queue: Novin Changizi <novinc@google.com>

--- src/lib/ddk/include/lib/ddk/fidl-protodefs.h
+++ src/lib/ddk/include/lib/ddk/fidl-protodefs.h
@@ -29,7 +29,7 @@ DDK_FIDL_PROTOCOL_DEF(MAILBOX,         10, "fuchsia.hardware.mailbox.Service")
 DDK_FIDL_PROTOCOL_DEF(PLATFORM_BUS,    11, "fuchsia.hardware.platform.bus.PlatformBus")
 DDK_FIDL_PROTOCOL_DEF(INTERRUPT,       12, "fuchsia.hardware.interrupt.Provider")
 DDK_FIDL_PROTOCOL_DEF(PLATFORM_DEVICE, 13, "fuchsia.hardware.platform.device.Service")
-DDK_FIDL_PROTOCOL_DEF(DSP,             14, "fuchsia.hardware.dsp.Service")
+// 14 was "fuchsia.hardware.dsp.Service"
 // 15 was "fuchsia.hardware.hdmi.Service"
 DDK_FIDL_PROTOCOL_DEF(POWER_SENSOR,    16, "fuchsia.hardware.power.sensor.Service")
 DDK_FIDL_PROTOCOL_DEF(VREG,            17, "fuchsia.hardware.vreg.Service")
