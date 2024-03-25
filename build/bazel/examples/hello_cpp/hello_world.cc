9c84c00d681076159131f1c1a9c7078bfb2e0e83
Jiaming Li <lijiaming@google.com>
2024-03-25 02:50:46 +0000

    [internal][sdk] Remove the debug-agent only artifacts from internal SDK
    Change-Id: I0d8953ccf2d4745ab69098396029a8fcf95c08f3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1013952
    Commit-Queue: Jiaming Li <lijiaming@google.com>
    Reviewed-by: Jay Zhuang <jayzhuang@google.com>

--- build/bazel/examples/hello_cpp/hello_world.cc
+++ build/bazel/examples/hello_cpp/hello_world.cc
@@ -4,12 +4,6 @@
 
 #include <iostream>
 
-#include <re2/re2.h>
-
-#include "src/lib/files/file.h"
-#include "src/lib/files/glob.h"
-#include "src/lib/fxl/command_line.h"
-
 #if __Fuchsia_API_level__ == 17
 #include "lib/hello_printer_17.h"
 #endif
@@ -19,12 +13,6 @@ int main() {
   hello_printer_17::HelloPrinter printer;
   printer.PrintHello();
 #endif
-
-  // Unused, only tests if re2 include is working.
-  const re2::RE2 kUnused(".*_unused_re");
   std::cout << "Hello, my dear in-tree Bazel world!\n";
-  fxl::CommandLine();
-  files::IsFile("/tmp/test");
-  files::Glob("mypath");
   return 0;
 }
