4df8c96b736346662fe727a6f4f5b0a44b1cd341
Clayton Wilkinson <wilkinsonclay@google.com>
2024-03-25 23:49:23 +0000

    [ffx][compat] Adding schema support to target remove
    This adds using the verified machine writer to the
    ffx target remove command. The output is an enum indicating
    error and an error message if present.
    
    Bug: b/329891020
    Change-Id: I8bd4c8f49a268550a3e249632e08ae46004356ae
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1010838
    Reviewed-by: Casey Dahlin <sadmac@google.com>
    Commit-Queue: Clayton Wilkinson <wilkinsonclay@google.com>

--- src/developer/ffx/plugins/target/remove/src/lib.rs
+++ src/developer/ffx/plugins/target/remove/src/lib.rs
@@ -2,11 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use anyhow::Result;
 use async_trait::async_trait;
 use ffx_target_remove_args::RemoveCommand;
-use fho::{daemon_protocol, FfxMain, FfxTool, SimpleWriter, ToolIO};
+use fho::{bug, daemon_protocol, FfxMain, FfxTool, Result, ToolIO, VerifiedMachineWriter};
 use fidl_fuchsia_developer_ffx::TargetCollectionProxy;
+use schemars::JsonSchema;
+use serde::Serialize;
+
+#[derive(Debug, Serialize, JsonSchema)]
+pub enum CommandStatus {
+    /// Successful execution with an optional informational string.
+    Ok { message: Option<String> },
+    /// Unexpected error with string.
+    UnexpectedError { message: String },
+    /// A known kind of error that can be reported usefully to the user
+    UserError { message: String },
+}
 
 #[derive(FfxTool)]
 pub struct RemoveTool {
@@ -20,25 +31,44 @@ fho::embedded_plugin!(RemoveTool);
 
 #[async_trait(?Send)]
 impl FfxMain for RemoveTool {
-    type Writer = SimpleWriter;
-    async fn main(self, writer: Self::Writer) -> fho::Result<()> {
-        remove_impl(self.target_collection_proxy, self.cmd, writer).await?;
-        Ok(())
+    type Writer = VerifiedMachineWriter<CommandStatus>;
+    async fn main(self, mut writer: Self::Writer) -> fho::Result<()> {
+        match remove_impl(self.target_collection_proxy, self.cmd).await {
+            Ok(message) => {
+                if writer.is_machine() {
+                    writer.machine(&CommandStatus::Ok { message: Some(message) })?;
+                } else {
+                    writeln!(writer.stderr(), "{message}")
+                        .map_err(|e| bug!("writing to stderr: {e}"))?;
+                }
+                Ok(())
+            }
+            Err(fho::Error::User(e)) => {
+                writer.machine(&CommandStatus::UserError { message: e.to_string() })?;
+                Err(fho::Error::User(e))
+            }
+            Err(e) => {
+                writer.machine(&CommandStatus::UnexpectedError { message: e.to_string() })?;
+                Err(e)
+            }
+        }
     }
 }
 
-async fn remove_impl<W: ToolIO>(
+async fn remove_impl(
     target_collection: TargetCollectionProxy,
     cmd: RemoveCommand,
-    mut writer: W,
-) -> Result<()> {
+) -> Result<String> {
     let RemoveCommand { name_or_addr, .. } = cmd;
-    if target_collection.remove_target(&name_or_addr).await? {
-        writeln!(writer.stderr(), "Removed.")?;
+    if target_collection
+        .remove_target(&name_or_addr)
+        .await
+        .map_err(|e| bug!("Cannot remove target: {e}"))?
+    {
+        Ok("Removed.".to_string())
     } else {
-        writeln!(writer.stderr(), "No matching target found.")?;
+        Ok("No matching target found.".to_string())
     }
-    Ok(())
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -47,7 +77,7 @@ async fn remove_impl<W: ToolIO>(
 #[cfg(test)]
 mod test {
     use super::*;
-    use ffx_writer::TestBuffers;
+    use fho::{Format, TestBuffers};
     use fidl_fuchsia_developer_ffx as ffx;
 
     fn setup_fake_target_collection_proxy<T: 'static + Fn(String) -> bool + Send>(
@@ -62,37 +92,50 @@ mod test {
         })
     }
 
-    #[fuchsia_async::run_singlethreaded(test)]
+    #[fuchsia::test]
     async fn test_remove_existing_target() {
         let server = setup_fake_target_collection_proxy(|id| {
             assert_eq!(id, "correct-horse-battery-staple".to_owned());
             true
         });
+        let tool = RemoveTool {
+            cmd: RemoveCommand { name_or_addr: "correct-horse-battery-staple".to_owned() },
+            target_collection_proxy: server,
+        };
         let test_buffers = TestBuffers::default();
-        let writer = SimpleWriter::new_test(&test_buffers);
-        remove_impl(
-            server,
-            RemoveCommand { name_or_addr: "correct-horse-battery-staple".to_owned() },
-            writer,
-        )
-        .await
-        .unwrap();
-
+        let writer = VerifiedMachineWriter::<CommandStatus>::new_test(None, &test_buffers);
+        tool.main(writer).await.expect("run main");
         assert_eq!(test_buffers.into_stderr_str(), "Removed.\n");
     }
 
-    #[fuchsia_async::run_singlethreaded(test)]
+    #[fuchsia::test]
     async fn test_remove_nonexisting_target() {
         let server = setup_fake_target_collection_proxy(|_| false);
+        let tool = RemoveTool {
+            cmd: RemoveCommand { name_or_addr: "incorrect-donkey-battery-jazz".to_owned() },
+            target_collection_proxy: server,
+        };
         let test_buffers = TestBuffers::default();
-        let writer = SimpleWriter::new_test(&test_buffers);
-        remove_impl(
-            server,
-            RemoveCommand { name_or_addr: "incorrect-donkey-battery-jazz".to_owned() },
-            writer,
-        )
-        .await
-        .unwrap();
+        let writer = VerifiedMachineWriter::<CommandStatus>::new_test(None, &test_buffers);
+
+        tool.main(writer).await.expect("run main");
         assert_eq!(test_buffers.into_stderr_str(), "No matching target found.\n");
+    }
+
+    #[fuchsia::test]
+    async fn test_remove_machine_nonexisting_target() {
+        let server = setup_fake_target_collection_proxy(|_| false);
+        let tool = RemoveTool {
+            cmd: RemoveCommand { name_or_addr: "incorrect-donkey-battery-jazz".to_owned() },
+            target_collection_proxy: server,
+        };
+        let test_buffers = TestBuffers::default();
+        let writer =
+            VerifiedMachineWriter::<CommandStatus>::new_test(Some(Format::Json), &test_buffers);
+
+        tool.main(writer).await.expect("run main");
+        let (actual_stdout, actual_stderr) = test_buffers.into_strings();
+        assert_eq!(actual_stderr, "");
+        assert_eq!(actual_stdout, "{\"Ok\":{\"message\":\"No matching target found.\"}}\n");
     }
 }
