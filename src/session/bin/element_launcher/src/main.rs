23277c1fe4b3b85cd9f40c9de3e2ebf50f2bf98f
David Gilhooley <dgilhooley@google.com>
2024-03-25 20:02:31 +0000

    [element_launcher] Add unit test
    Add a unit test that checks that element launcher forwards the
    URL from its config to the element manager.
    
    Fixed: b/329232994
    Change-Id: Ia41cd1f28266cc728f8299bb5d7f9fb37c9038e0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1014292
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Fuchsia-Auto-Submit: David Gilhooley <dgilhooley@google.com>
    Reviewed-by: Yifei Teng <yifeit@google.com>

--- src/session/bin/element_launcher/src/main.rs
+++ src/session/bin/element_launcher/src/main.rs
@@ -8,17 +8,10 @@ use {
     tracing::{info, warn},
 };
 
-#[fuchsia::main(logging = true)]
-async fn main() {
-    info!("element_launcher is starting.");
-    let config = element_launcher_config::Config::take_from_startup_handle();
-    if config.main_element_url.is_empty() {
-        warn!("element_launcher was started without a main_element_url; quitting.");
-        return;
-    }
-
-    let element_manager = connect_to_protocol::<element::ManagerMarker>()
-        .expect("failed to connect to fuchsia.element.Manager");
+async fn propose_element(
+    element_manager: element::ManagerProxy,
+    config: element_launcher_config::Config,
+) {
     element_manager
         .propose_element(
             element::Spec {
@@ -39,7 +32,45 @@ async fn main() {
         .expect("Failed to propose element.");
 }
 
+#[fuchsia::main(logging = true)]
+async fn main() {
+    info!("element_launcher is starting.");
+    let config = element_launcher_config::Config::take_from_startup_handle();
+    if config.main_element_url.is_empty() {
+        warn!("element_launcher was started without a main_element_url; quitting.");
+        return;
+    }
+
+    let element_manager = connect_to_protocol::<element::ManagerMarker>()
+        .expect("failed to connect to fuchsia.element.Manager");
+    propose_element(element_manager, config).await;
+}
+
 #[cfg(test)]
 mod tests {
-    // TODO(https://fxbug.dev/329232994): Add unit tests
+    use super::*;
+    use futures::StreamExt;
+
+    #[fuchsia::test]
+    async fn test_propose_element() {
+        let test_url = "test";
+        let config = element_launcher_config::Config { main_element_url: test_url.to_string() };
+        let (element_manager, mut stream) =
+            fidl::endpoints::create_proxy_and_stream::<element::ManagerMarker>().unwrap();
+
+        let stream_fut = async move {
+            let request = stream.next().await.unwrap().unwrap();
+            match request {
+                element::ManagerRequest::ProposeElement { spec, responder, .. } => {
+                    assert_eq!(spec.component_url, Some(test_url.to_string()));
+                    responder.send(Ok(())).unwrap();
+                }
+                element::ManagerRequest::RemoveElement { .. } => {
+                    panic!("RemoveElement was called");
+                }
+            };
+        };
+        let propose_fut = propose_element(element_manager, config);
+        futures::join!(propose_fut, stream_fut);
+    }
 }
