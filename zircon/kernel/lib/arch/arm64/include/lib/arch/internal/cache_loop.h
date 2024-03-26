d53d12ca0e8b148e17b45d4b8820628296fa2652
Travis Geiselbrecht <travisg@google.com>
2024-03-26 22:50:27 +0000

    [kernel][arm64] clean up the cache loop a bit in prep for refactoring
    Validated via build of qemu to print the values passed to the DC CSW
    instruction and running 'ut arm64_cache' on the kernel command line.
    Observe that it properly interprets a legacy CCSIDR.
    
    Bug: 330385318
    
    Change-Id: I7bbd74e02110d6c88214507b57d4bf002eb04e1d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1004836
    Reviewed-by: Anirudh Mathukumilli <rudymathu@google.com>
    Fuchsia-Auto-Submit: Travis Geiselbrecht <travisg@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Adrian Danis <adanis@google.com>

--- zircon/kernel/lib/arch/arm64/include/lib/arch/internal/cache_loop.h
+++ zircon/kernel/lib/arch/arm64/include/lib/arch/internal/cache_loop.h
@@ -16,46 +16,64 @@
 //
 // Adapted from example code in the ARM Architecture Reference Manual ARMv8.
 .macro data_cache_way_set_op_impl, op name
-    mrs     x0, clidr_el1
-    and     w3, w0, #0x07000000     // get 2x level of coherence
-    lsr     w3, w3, #23
-    cbz     w3, .Lfinished_\name
-    mov     w10, #0                 // w10 = 2x cache level
+    mrs     x0, clidr_el1           // x0 = clidr_el1
+
+    and     w3, w0, #0x07000000     // w3 = level of coherence from clidr
+    lsr     w3, w3, #23             // multiply by 2
+
+    cbz     w3, .Lfinished_\name    // LoC == 0, exit now
+
+    mov     w10, #0                 // w10 = 2x cache level, start with 0
     mov     w8, #1                  // w8 = constant 1
+
+    // loop per level of cache
 .Lloop1_\name:
-    add     w2, w10, w10, lsr #1    // calculate 3x cache level
-    lsr     w1, w0, w2              // extract 3 bit cache type for this level
+    add     w2, w10, w10, lsr #1    // w2 = 3x cache level
+    lsr     w1, w0, w2              // w1 = 3 bit cache type for this level, extracted from clidr
     and     w1, w1, #0x7
+
     cmp     w1, #2
     b.lt    .Lskip_\name            // no data or unified cache at this level
-    msr     csselr_el1, x10         // select this cache level
+
+    msr     csselr_el1, x10         // select this cache level (w10 already holds cache level << 1)
     isb                             // synchronize change to csselr
+
+    // TODO(https://fxbug.dev/330385318): update this logic to work with extended CCSIDR
     mrs     x1, ccsidr_el1          // w1 = ccsidr
     and     w2, w1, #7              // w2 = log2(line len) - 4
     add     w2, w2, #4              // w2 = log2(line len)
+
     ubfx    w4, w1, #3, #10         // w4 = max way number, right aligned
     clz     w5, w4                  // w5 = 32 - log2(ways), bit position of way in DC operand
     lsl     w9, w4, w5              // w9 = max way number, aligned to position in DC operand
     lsl     w12, w8, w5             // w12 = amount to decrement way number per iteration
 
-.Lloop2_\name:
-    ubfx    w7, w1, #13, #15        // w7 = max set number, right aligned
-    lsl     w7, w7, w2              // w7 = max set number, aligned to position in DC operand
+    ubfx    w6, w1, #13, #15        // w6 = max set number, right aligned
+    lsl     w6, w6, w2              // w6 = max set number, aligned to position in DC operand
     lsl     w13, w8, w2             // w13 = amount to decrement set number per iteration
+
+    // loop by way
+.Lloop2_\name:
+    mov     w7, w6                  // w7 = reload max set number from w6
+
+    //  loop by set
 .Lloop3_\name:
     orr     w11, w10, w9            // w11 = combine way number and cache number
     orr     w11, w11, w7            //       and set number for DC operand
     dc      \op, x11                // data cache op
     subs    w7, w7, w13             // decrement set number
-    b.ge    .Lloop3_\name
+    b.ge    .Lloop3_\name           // repeat until set < 0
 
     subs    x9, x9, x12             // decrement way number
-    b.ge    .Lloop2_\name
+    b.ge    .Lloop2_\name           // repeat until way < 0
+
+    dsb     sy                      // ensure completion of previous cache maintenance instructions
+
 .Lskip_\name:
     add     w10, w10, #2            // increment 2x cache level
-    cmp     w3, w10
-    dsb     sy                      // ensure completetion of previous cache maintainance instructions
+    cmp     w3, w10                 // loop while we haven't reached level of coherence
     b.gt    .Lloop1_\name
+
 .Lfinished_\name:
 .endm
 
