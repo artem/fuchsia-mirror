728fde7882c6d4d1f30726cc8032e3d57792d236
Gary Bressler <geb@google.com>
2024-03-27 17:36:06 +0000

    [components][dictionaries] Add integration test
    Fixed: 329728835
    Change-Id: I47e0a4512bb02894cc9ef9cc71de7d04b9ec128c
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1014459
    Reviewed-by: Claire Gonyeo <cgonyeo@google.com>
    Commit-Queue: Gary Bressler <geb@google.com>

---
+++ src/sys/component_manager/tests/dictionaries/src/lib.rs
@@ -0,0 +1,34 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use {fidl_fidl_test_components as ftest, fuchsia_component::client};
+
+#[fuchsia::test]
+async fn protocols() {
+    fn path(letter: &str) -> String {
+        format!("/svc/fidl.test.components.Trigger-{letter}")
+    }
+
+    // See the test's cml to understand how this exercises dictionary routing.
+    {
+        let trigger =
+            client::connect_to_protocol_at_path::<ftest::TriggerMarker>(&path("a")).unwrap();
+        let out = trigger.run().await.unwrap();
+        assert_eq!(&out, "Triggered a");
+    }
+    {
+        let trigger =
+            client::connect_to_protocol_at_path::<ftest::TriggerMarker>(&path("b")).unwrap();
+        let out = trigger.run().await.unwrap();
+        assert_eq!(&out, "Triggered b");
+    }
+    {
+        let trigger =
+            client::connect_to_protocol_at_path::<ftest::TriggerMarker>(&path("c")).unwrap();
+        let out = trigger.run().await.unwrap();
+        assert_eq!(&out, "Triggered c");
+    }
+}
+
+// TODO(https://fxbug.dev/301674053): Add tests for more capability types.
