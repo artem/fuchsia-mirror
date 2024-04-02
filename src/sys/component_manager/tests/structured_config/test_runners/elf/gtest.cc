b358e22a90924489013aa33ce8595899a7fc313c
David Gilhooley <dgilhooley@google.com>
2024-04-02 17:53:04 +0000

    [structured_config] Enable gtest_runner support
    This support was added back in fxr/939333
    Add the runner name to the list of supported runners.
    
    Change-Id: I6817a846569eac22e40a124ff3c9f9fa3bf52df3
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1015337
    Reviewed-by: John Rosasco <rosasco@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Yifei Teng <yifeit@google.com>
    Reviewed-by: Josh Gargus <jjosh@google.com>
    Fuchsia-Auto-Submit: David Gilhooley <dgilhooley@google.com>

---
+++ src/sys/component_manager/tests/structured_config/test_runners/elf/gtest.cc
@@ -0,0 +1,21 @@
+// Copyright 2024 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gtest/gtest.h>
+
+#include "src/sys/component_manager/tests/structured_config/test_runners/elf/receiver_config.h"
+
+namespace {
+TEST(ScTest, CheckValues) {
+  receiver_config::Config c = receiver_config::Config::TakeFromStartupHandle();
+  ASSERT_EQ(c.my_flag(), true);
+  ASSERT_EQ(c.my_uint8(), 255);
+}
+
+TEST(ScTest, CreateOwnConfig) {
+  receiver_config::Config c;
+  c.my_uint8() = 5;
+  ASSERT_EQ(c.my_uint8(), 5);
+}
+}  // namespace
