ac583bc16e5a50a0f3f4355f735c4a4e8db174ca
Christopher Anderson <cja@google.com>
2024-04-04 20:20:09 +0000

    [sdk] Flesh out reviewer shell script
    - Add -f to filter users out
    - Add -c for reviewer count
    - Pass shellcheck tests
    
    Change-Id: I37233104a43cb9b60cb9eaa68d9641613e06ec31
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1020272
    Reviewed-by: Hunter Freyer <hjfreyer@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Fuchsia-Auto-Submit: Christopher Anderson <cja@google.com>

--- sdk/fidl/pick_random_reviewers.sh
+++ sdk/fidl/pick_random_reviewers.sh
@@ -4,15 +4,31 @@
 # found in the LICENSE file.
 
 # This script returns a random set of API reviewers from the owners file.
+usage() { echo "Usage: $0 [-c <reviewer count] [-f <username to filter out>]" 1>&2; exit 1; }
 
-if [ -z "$1" ]; then
-  COUNT="3"
-else
-  COUNT=$1
-fi
+while getopts "c:f:" ARG; do
+  case "${ARG}" in
+    c)
+      COUNT=${OPTARG}
+      ;;
+    f)
+      # Omit a specified user.
+      FILTER=('-e' "${OPTARG}")
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
 
+COUNT="${COUNT:-3}"
+# Omit comment lines, omit empty lines.
+FILTER+=('-e' '#' '-e' '^$' '-v')
 SCRIPT_SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-# Omit comment lines, omit empty lines, choose $COUNT out of the set of
-# unique API reviewers.
-cat ${SCRIPT_SRC_DIR}/OWNERS | grep -v '#' | grep -v '^$' | sort | uniq | shuf | head -n "${COUNT}"
+# Choose COUNT reviewers out of the set of unique API reviewers.
+grep "${FILTER[@]}" < "${SCRIPT_SRC_DIR}/OWNERS" |
+  sort |
+  uniq |
+  shuf |
+  head -n "${COUNT}"
