15cb1a5a98ab26a7c7d644a225098e53db68dc60
Aaron Drew <ripper@google.com>
2024-04-04 01:39:36 +0000

    Reland "[fxfs] Switch BestFit to use BTreeMap."
    This is a reland of commit 036acdfa1a8459dae85063de5f38e57827a1abfe
    
    A secondary structure has been added to try to speed up free() calls.
    
    Original change's description:
    > [fxfs] Switch BestFit to use BTreeMap.
    >
    > I'm planning on capping the number of free extents kept per size bucket.
    > This change is in preparation for that but it also brings potential
    > minor memory savings for now as a side effect.
    >
    > We (will) have a finite number of size buckets for allocations
    > based on number of blocks. Rather than store two 64-bit integers for
    > start/end of each range, this change allows us to store a set of start
    > offsets for each size bucket. Even with the overheads of BTreeSet, this
    > should lead to memory savings at scale.
    >
    > Change-Id: Iac17cdfd721923f0d859f02154921545e056cc22
    > Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1014892
    > Fuchsia-Auto-Submit: Aaron Drew <ripper@google.com>
    > Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    > Reviewed-by: Rui Qi Sim <rqsim@google.com>
    
    Change-Id: I4b3c49986d9003d93ab357f54269ab521d2f3fd0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1015912
    Commit-Queue: Aaron Drew <ripper@google.com>
    Reviewed-by: Chris Suter <csuter@google.com>

--- src/storage/fxfs/src/object_store/allocator/strategy.rs
+++ src/storage/fxfs/src/object_store/allocator/strategy.rs
@@ -16,7 +16,11 @@
 //!
 //! Strategies should be concerned only with selecting which free regions of disk to hand out.
 
-use std::{collections::BTreeSet, fmt::Debug, ops::Range};
+use std::{
+    collections::{btree_map, BTreeMap, BTreeSet},
+    fmt::Debug,
+    ops::Range,
+};
 
 /// An allocation strategy that returns the smallest extent that is large enough to hold the
 /// requested allocation, or the next best if no extent is big enough.
@@ -29,9 +33,17 @@ use std::{collections::BTreeSet, fmt::Debug, ops::Range};
 /// space remaining.
 #[derive(Debug, Default)]
 pub struct BestFit {
-    /// Holds free storage ranges for the filesystem ordered by length, then offset.
-    ranges: BTreeSet<(u64, u64)>,
+    /// Holds free storage ranges for the filesystem ordered by length, then start.
+    ranges: BTreeMap<u64, BTreeSet<u64>>,
+    /// A secondary index used to speed up coalescing of 'free' calls. Keyed by end -> start
+    by_end: BTreeMap<u64, u64>,
 }
+
+/// Tests (in allocator.rs) may set max extent size to low values for other reasons.
+/// We want to make sure that we never go below a value that is big enough for superblocks
+/// so the value we use here is chosen to be larger than the value allocator.rs calculates and
+/// a convenient power of two.
+const DEFAULT_MAX_EXTENT_SIZE: u64 = 2 << 20;
 
 impl BestFit {
     /// Tries to assign a set of contiguous `bytes` and returns the range, removing it
@@ -43,66 +55,86 @@ impl BestFit {
     /// There are no special requirements on alignment of `bytes` but the caller is generally
     /// encouraged to align to device block size.
     pub fn allocate(&mut self, bytes: u64) -> Option<Range<u64>> {
-        if let Some(&(size, offset)) = self.ranges.range((bytes, 0)..).next() {
-            assert!(size >= bytes);
-            self.ranges.remove(&(size, offset));
+        let mut result = self.ranges.range_mut(bytes..).next();
+        if result.is_none() {
+            // Insufficient space. Return the biggest range we have.
+            result = self.ranges.iter_mut().rev().next();
+        }
+        if let Some((&size, offsets)) = result {
+            debug_assert!(!offsets.is_empty());
+            let offset = offsets.pop_first().unwrap();
+            if offsets.is_empty() {
+                self.ranges.remove(&size);
+            }
+            self.by_end.remove(&(offset + size));
             if size > bytes {
                 self.free(offset + bytes..offset + size);
+                Some(offset..offset + bytes)
+            } else {
+                Some(offset..offset + size)
             }
-            return Some(offset..offset + bytes);
-        }
-        // Insufficient space. Return the biggest space we have.
-        if let Some(&(size, _)) = self.ranges.iter().rev().next() {
-            // Nb: We reversed the iterator to get the biggest size, but we
-            // generally want to serve the smallest offset so we do a second
-            // lookup here to find the smallest offset with the largest size.
-            let &(size, offset) = self.ranges.range((size, 0)..).next().unwrap();
-            self.ranges.remove(&(size, offset));
-            Some(offset..offset + size)
         } else {
             None
         }
     }
 
+    /// Allocates a specific range of the device.
+    ///
+    /// Used only in allocating space for the first extent of each Fxfs superblock.
+    /// (This is the only data only in the filesystem that is stored at a fixed device offset.)
     pub fn allocate_fixed_offset(&mut self, start: u64, bytes: u64) -> Option<Range<u64>> {
-        let end = start + bytes;
-        for &(size, offset) in self.ranges.range((bytes, 0)..) {
-            assert!(size >= bytes);
-            let range = offset..offset + size;
-            if range.start > start || range.end < end {
-                // Not a sub-range.
-                continue;
+        let mut to_remove = vec![];
+        let mut cur_end = start;
+        let mut first_offset = None;
+        let mut prev_end = None;
+        for (&end, &offset) in self.by_end.range(start + 1..) {
+            if first_offset.is_none() {
+                // Check that range covers start.
+                if offset > start {
+                    return None;
+                }
+                first_offset = Some(offset);
             }
-            self.ranges.remove(&(size, offset));
-            if range.start < start {
-                self.free(range.start..start);
+            if offset > start + bytes {
+                break;
             }
-            if range.end > end {
-                self.free(end..range.end);
+            if let Some(prev) = prev_end {
+                if prev != offset {
+                    // Non-contiguous ranges. Can't allocate.
+                    return None;
+                }
             }
-            return Some(start..end);
+            prev_end = Some(end);
+            cur_end = end;
+            to_remove.push((end, offset));
         }
-        None
+        if cur_end < start + bytes {
+            return None;
+        }
+
+        for (end, offset) in to_remove.into_iter() {
+            self.remove_range(offset..end);
+        }
+
+        if let Some(first_offset) = first_offset {
+            if first_offset < start {
+                self.free(first_offset..start);
+            }
+            if cur_end > start + bytes {
+                self.free(start + bytes..cur_end);
+            }
+            Some(start..start + bytes)
+        } else {
+            None
+        }
     }
 
     pub fn allocate_next_available(&mut self, start: u64, bytes: u64) -> Option<Range<u64>> {
-        let mut best = None;
-        for &(size, offset) in &self.ranges {
-            if offset + size <= start {
-                continue;
-            }
-            if let Some((best_size, best_offset)) = &mut best {
-                if offset < *best_offset {
-                    *best_size = size;
-                    *best_offset = offset;
-                }
-            } else {
-                best = Some((size, offset));
-            }
-        }
-        if let Some((size, offset)) = best {
-            self.ranges.remove(&(size, offset));
-            let mut range = offset..offset + size;
+        // by_offset is keyed by end, so this will give the first available range that ends after
+        // 'start', which is exactly what we need. ;)
+        if let Some((&end, &offset)) = self.by_end.range(start + 1..).next() {
+            self.remove_range(offset..end);
+            let mut range = offset..end;
             if range.start < start {
                 self.free(range.start..start);
                 range.start = start;
@@ -117,31 +149,57 @@ impl BestFit {
         }
     }
 
-    /// Adds a range of bytes to the pool of available ranges.
+    /// Internal helper function. Assumes range exists.
+    fn remove_range(&mut self, range: Range<u64>) {
+        let btree_map::Entry::Occupied(mut offsets) = self.ranges.entry(range.end - range.start)
+        else {
+            unreachable!()
+        };
+        offsets.get_mut().remove(&range.start);
+        if offsets.get().is_empty() {
+            offsets.remove_entry();
+        }
+        self.by_end.remove(&range.end);
+    }
+
+    /// Adds an arbitrary range of bytes to the pool of available ranges.
     ///
-    /// There are no special requirements on the alignment of `range` but
-    /// callers should should generally ensure ranges are block aligned.
+    /// Note that we keep these ranges in a map keyed by their length. To bound the size of this
+    /// map we only track ranges up to N blocks long (up to 2MB). Longer ranges
+    /// are broken up into ranges of this size.
     pub fn free(&mut self, mut range: Range<u64>) {
-        let mut prior_range = None;
-        let mut post_range = None;
-        // Look for ranges immediately before and after 'range' for coalescing.
-        for (size, offset) in &self.ranges {
-            if *offset + *size == range.start {
-                prior_range = Some((*size, *offset));
+        // If there is a free range immediately before this one, merge with it.
+        let mut iter = self.by_end.range(range.start..);
+        let mut next_item = iter.next();
+        if let Some((&end, &start)) = next_item {
+            if end == range.start {
+                self.remove_range(start..end);
+                range.start = start;
+                iter = self.by_end.range(range.start + 1..);
+                next_item = iter.next();
             }
-            if *offset == range.end {
-                post_range = Some((*size, *offset));
+        }
+        // If there is a free range immediately after this one, merge with it.
+        if let Some((&end, &start)) = next_item {
+            if start == range.end {
+                self.remove_range(start..end);
+                range.end = end;
             }
         }
-        if let Some(prior_range) = prior_range {
-            self.ranges.remove(&prior_range);
-            range.start = prior_range.1;
+
+        // We don't allow ranges longer than maximum extent size, but if we need to split such
+        // a range, we want the smaller fragment to come first (pushing small fragments together at
+        // the start of the device).
+        while (range.end - range.start) > DEFAULT_MAX_EXTENT_SIZE {
+            self.ranges
+                .entry(DEFAULT_MAX_EXTENT_SIZE)
+                .or_default()
+                .insert(range.end - DEFAULT_MAX_EXTENT_SIZE);
+            self.by_end.insert(range.end, range.end - DEFAULT_MAX_EXTENT_SIZE);
+            range.end -= DEFAULT_MAX_EXTENT_SIZE;
         }
-        if let Some(post_range) = post_range {
-            self.ranges.remove(&post_range);
-            range.end = post_range.0 + post_range.1;
-        }
-        self.ranges.insert((range.end - range.start, range.start));
+        self.ranges.entry(range.end - range.start).or_default().insert(range.start);
+        self.by_end.insert(range.end, range.start);
     }
 }
 
@@ -150,7 +208,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn hint_none() {
+    fn allocate() {
         let mut bestfit = BestFit::default();
         bestfit.free(0..0); // NOOP
         bestfit.free(0..100);
@@ -180,15 +238,34 @@ mod test {
     }
 
     #[test]
-    fn hint_require() {
+    fn fixed_offset() {
         let mut bestfit = BestFit::default();
         bestfit.free(0..100);
         assert_eq!(bestfit.allocate_fixed_offset(25, 50), Some(25..75));
         assert_eq!(bestfit.allocate_fixed_offset(25, 10), None);
+
+        // Test coalescing of adjacent available ranges if the request is large.
+        let mut bestfit = BestFit::default();
+        bestfit.free(0..DEFAULT_MAX_EXTENT_SIZE * 3 + 10000);
+        assert_eq!(
+            bestfit.allocate_fixed_offset(0, DEFAULT_MAX_EXTENT_SIZE * 2),
+            Some(0..DEFAULT_MAX_EXTENT_SIZE * 2)
+        );
+
+        // Try to allocate a range with a hole in it.
+        let mut bestfit = BestFit::default();
+        bestfit.free(0..100);
+        bestfit.free(200..400);
+        assert_eq!(bestfit.allocate_fixed_offset(50, 300), None);
+        //
+        // Try to allocate a range missing a tail.
+        let mut bestfit = BestFit::default();
+        bestfit.free(0..250);
+        assert_eq!(bestfit.allocate_fixed_offset(50, 300), None);
     }
 
     #[test]
-    fn hint_next_available() {
+    fn next_available() {
         let mut bestfit = BestFit::default();
         bestfit.free(0..96);
         assert_eq!(bestfit.allocate(1), Some(0..1));
@@ -213,5 +290,26 @@ mod test {
         // Confirm that we can allocate one block of 32 bytes. This will fail if coalescing
         // didn't occur.
         assert_eq!(bestfit.allocate_fixed_offset(0, 32), Some(0..32));
+    }
+
+    #[test]
+    fn max_range() {
+        let mut bestfit = BestFit::default();
+        bestfit.free(10..10 + 10 * DEFAULT_MAX_EXTENT_SIZE);
+
+        // We can't allocate bigger than DEFAULT_MAX_EXTENT_SIZE.
+        assert_eq!(
+            bestfit.allocate(2 * DEFAULT_MAX_EXTENT_SIZE),
+            Some(10..10 + DEFAULT_MAX_EXTENT_SIZE)
+        );
+
+        // Make sure that coalescing still works properly
+        bestfit.free(10..10 + DEFAULT_MAX_EXTENT_SIZE);
+        assert_eq!(bestfit.allocate(10), Some(10..20));
+        assert_eq!(bestfit.allocate(10), Some(20..30));
+        assert_eq!(
+            bestfit.allocate(DEFAULT_MAX_EXTENT_SIZE - 20),
+            Some(30..10 + DEFAULT_MAX_EXTENT_SIZE)
+        );
     }
 }
