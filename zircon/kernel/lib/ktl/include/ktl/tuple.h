b132d518b8e6fb6b651cac227f03325ce6cb14c2
Nick Maniscalco <maniscalco@google.com>
2024-04-11 18:04:56 +0000

    [ktl] Add std::ignore to ktl
    Add std::ignore to ktl for when [[maybe_unused]] cannot be used.
    
    Change-Id: I12f62a8eaf25dcf451d27fc352eaa61c2ddb0744
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1025654
    Reviewed-by: John Grossman <johngro@google.com>
    Fuchsia-Auto-Submit: Nick Maniscalco <maniscalco@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Roland McGrath <mcgrathr@google.com>

---
+++ zircon/kernel/lib/ktl/include/ktl/tuple.h
@@ -0,0 +1,18 @@
+// Copyright 2024 The Fuchsia Authors
+//
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT
+
+#ifndef ZIRCON_KERNEL_LIB_KTL_INCLUDE_KTL_TUPLE_H_
+#define ZIRCON_KERNEL_LIB_KTL_INCLUDE_KTL_TUPLE_H_
+
+#include <tuple>
+
+namespace ktl {
+
+using std::ignore;
+
+}  // namespace ktl
+
+#endif  // ZIRCON_KERNEL_LIB_KTL_INCLUDE_KTL_TUPLE_H_
