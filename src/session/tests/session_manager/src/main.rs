4173353080383e23479238880e82b07ff332eba8
Yifei Teng <yifeit@google.com>
2024-04-11 21:59:24 +0000

    [session] Reduce indentation in tests
    Change-Id: If4549944258049451101af156f9976d4102fc61e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1024988
    Commit-Queue: Yifei Teng <yifeit@google.com>
    Reviewed-by: Gary Bressler <geb@google.com>

--- src/session/tests/session_manager/src/main.rs
+++ src/session/tests/session_manager/src/main.rs
@@ -2,180 +2,176 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#[cfg(test)]
-mod tests {
-    use {
-        diagnostics_hierarchy::DiagnosticsHierarchy,
-        diagnostics_reader::{ArchiveReader, Inspect},
-        fidl::endpoints::create_proxy,
-        fidl_fuchsia_component as fcomponent, fidl_fuchsia_io as fio,
-        fuchsia_component::client::connect_to_protocol,
-        fuchsia_component_test::{
-            Capability, ChildOptions, ChildRef, DirectoryContents, RealmBuilder, Ref, Route,
-        },
-    };
+use {
+    diagnostics_hierarchy::DiagnosticsHierarchy,
+    diagnostics_reader::{ArchiveReader, Inspect},
+    fidl::endpoints::create_proxy,
+    fidl_fuchsia_component as fcomponent, fidl_fuchsia_io as fio,
+    fuchsia_component::client::connect_to_protocol,
+    fuchsia_component_test::{
+        Capability, ChildOptions, ChildRef, DirectoryContents, RealmBuilder, Ref, Route,
+    },
+};
 
-    const SESSION_URL: &'static str = "hello-world-session#meta/hello-world-session.cm";
+const SESSION_URL: &'static str = "hello-world-session#meta/hello-world-session.cm";
 
-    /// Passes if the root session launches successfully. This tells us:
-    ///     - session_manager is able to use the Realm service to launch a component.
-    ///     - the root session was started in the "session" collection.
-    #[fuchsia::test]
-    async fn launch_root_session() {
-        let realm =
-            connect_to_protocol::<fcomponent::RealmMarker>().expect("could not connect to Realm");
+/// Passes if the root session launches successfully. This tells us:
+///     - session_manager is able to use the Realm service to launch a component.
+///     - the root session was started in the "session" collection.
+#[fuchsia::test]
+async fn launch_root_session() {
+    let realm =
+        connect_to_protocol::<fcomponent::RealmMarker>().expect("could not connect to Realm");
 
-        let session_url = String::from(SESSION_URL);
-        println!("Session url: {}", &session_url);
+    let session_url = String::from(SESSION_URL);
+    println!("Session url: {}", &session_url);
 
-        // `launch_session()` requires an initial exposed-diretory request, so create, pass and
-        // immediately close a `Directory` channel.
-        let (_exposed_dir, exposed_dir_server_end) =
-            create_proxy::<fio::DirectoryMarker>().unwrap();
+    // `launch_session()` requires an initial exposed-diretory request, so create, pass and
+    // immediately close a `Directory` channel.
+    let (_exposed_dir, exposed_dir_server_end) = create_proxy::<fio::DirectoryMarker>().unwrap();
 
-        session_manager_lib::startup::launch_session(&session_url, exposed_dir_server_end, &realm)
-            .await
-            .expect("Failed to launch session");
-    }
+    session_manager_lib::startup::launch_session(&session_url, exposed_dir_server_end, &realm)
+        .await
+        .expect("Failed to launch session");
+}
 
-    // Add a `session_manager` component to the `RealmBuilder`, with the given
-    // config options set.
-    async fn add_session_manager(
-        builder: &RealmBuilder,
-        session_url: String,
-        autolaunch: bool,
-    ) -> anyhow::Result<ChildRef> {
-        let child_ref = builder
-            .add_child("session-manager", "#meta/session_manager.cm", ChildOptions::new().eager())
-            .await?;
-        builder
-            .add_capability(cm_rust::CapabilityDecl::Config(cm_rust::ConfigurationDecl {
-                name: "fuchsia.session.SessionUrl".to_string().parse()?,
-                value: session_url.into(),
-            }))
-            .await?;
-        builder
-            .add_capability(cm_rust::CapabilityDecl::Config(cm_rust::ConfigurationDecl {
-                name: "fuchsia.session.AutoLaunch".to_string().parse()?,
-                value: autolaunch.into(),
-            }))
-            .await?;
-        builder
-            .add_capability(cm_rust::CapabilityDecl::Config(cm_rust::ConfigurationDecl {
-                name: "fuchsia.power.SuspendEnabled".to_string().parse()?,
-                value: false.into(),
-            }))
-            .await?;
+// Add a `session_manager` component to the `RealmBuilder`, with the given
+// config options set.
+async fn add_session_manager(
+    builder: &RealmBuilder,
+    session_url: String,
+    autolaunch: bool,
+) -> anyhow::Result<ChildRef> {
+    let child_ref = builder
+        .add_child("session-manager", "#meta/session_manager.cm", ChildOptions::new().eager())
+        .await?;
+    builder
+        .add_capability(cm_rust::CapabilityDecl::Config(cm_rust::ConfigurationDecl {
+            name: "fuchsia.session.SessionUrl".to_string().parse()?,
+            value: session_url.into(),
+        }))
+        .await?;
+    builder
+        .add_capability(cm_rust::CapabilityDecl::Config(cm_rust::ConfigurationDecl {
+            name: "fuchsia.session.AutoLaunch".to_string().parse()?,
+            value: autolaunch.into(),
+        }))
+        .await?;
+    builder
+        .add_capability(cm_rust::CapabilityDecl::Config(cm_rust::ConfigurationDecl {
+            name: "fuchsia.power.SuspendEnabled".to_string().parse()?,
+            value: false.into(),
+        }))
+        .await?;
 
-        builder
-            .add_route(
-                Route::new()
-                    .capability(Capability::configuration("fuchsia.session.SessionUrl"))
-                    .capability(Capability::configuration("fuchsia.session.AutoLaunch"))
-                    .capability(Capability::configuration("fuchsia.power.SuspendEnabled"))
-                    .from(Ref::self_())
-                    .to(&child_ref),
-            )
-            .await?;
-        Ok(child_ref)
-    }
+    builder
+        .add_route(
+            Route::new()
+                .capability(Capability::configuration("fuchsia.session.SessionUrl"))
+                .capability(Capability::configuration("fuchsia.session.AutoLaunch"))
+                .capability(Capability::configuration("fuchsia.power.SuspendEnabled"))
+                .from(Ref::self_())
+                .to(&child_ref),
+        )
+        .await?;
+    Ok(child_ref)
+}
 
-    async fn get_session_manager_inspect(
-        realm: &fuchsia_component_test::RealmInstance,
-        selector: &str,
-    ) -> anyhow::Result<DiagnosticsHierarchy> {
-        ArchiveReader::new()
-            .add_selector(format!(
-                "realm_builder\\:{}/session-manager:{}",
-                realm.root.child_name(),
-                selector
-            ))
-            .snapshot::<Inspect>()
-            .await?
-            .pop()
-            .ok_or(anyhow::anyhow!("inspect data had no snapshot"))?
-            .payload
-            .ok_or(anyhow::anyhow!("inspect snapshot had no payload"))
-    }
+async fn get_session_manager_inspect(
+    realm: &fuchsia_component_test::RealmInstance,
+    selector: &str,
+) -> anyhow::Result<DiagnosticsHierarchy> {
+    ArchiveReader::new()
+        .add_selector(format!(
+            "realm_builder\\:{}/session-manager:{}",
+            realm.root.child_name(),
+            selector
+        ))
+        .snapshot::<Inspect>()
+        .await?
+        .pop()
+        .ok_or(anyhow::anyhow!("inspect data had no snapshot"))?
+        .payload
+        .ok_or(anyhow::anyhow!("inspect snapshot had no payload"))
+}
 
-    #[fuchsia::test]
-    async fn test_autolaunch_launches() -> anyhow::Result<()> {
-        let builder = RealmBuilder::new().await?;
+#[fuchsia::test]
+async fn test_autolaunch_launches() -> anyhow::Result<()> {
+    let builder = RealmBuilder::new().await?;
 
-        add_session_manager(
-            &builder,
-            "hello-world-session#meta/hello-world-session.cm".to_string(),
-            true,
+    add_session_manager(
+        &builder,
+        "hello-world-session#meta/hello-world-session.cm".to_string(),
+        true,
+    )
+    .await?;
+
+    let realm = builder.build().await?;
+
+    let inspect = get_session_manager_inspect(&realm, "root/session_started_at/0").await?;
+
+    // Assert the session has been launched once.
+    assert_eq!(
+        1,
+        inspect
+            .get_child("session_started_at")
+            .expect("session_started_at is not none")
+            .children
+            .len()
+    );
+
+    realm.destroy().await?;
+    Ok(())
+}
+
+#[fuchsia::test]
+async fn test_noautolaunch_does_not_launch() -> anyhow::Result<()> {
+    let builder = RealmBuilder::new().await?;
+
+    add_session_manager(
+        &builder,
+        "hello-world-session#meta/hello-world-session.cm".to_string(),
+        false,
+    )
+    .await?;
+
+    let realm = builder.build().await?;
+
+    let inspect = get_session_manager_inspect(&realm, "root/session_started_at").await?;
+
+    // No sessions should have launched.
+    assert_eq!(0, inspect.get_child("session_started_at").unwrap().children.len());
+
+    realm.destroy().await?;
+    Ok(())
+}
+
+#[fuchsia::test]
+async fn noautolaunch_file_overrides_structured_config() -> anyhow::Result<()> {
+    let builder = RealmBuilder::new().await?;
+
+    let session_manager = add_session_manager(
+        &builder,
+        "hello-world-session#meta/hello-world-session.cm".to_string(),
+        true,
+    )
+    .await?;
+
+    builder
+        .read_only_directory(
+            "root-data",
+            vec![&session_manager],
+            DirectoryContents::new().add_file("session-manager/noautolaunch", ""),
         )
         .await?;
 
-        let realm = builder.build().await?;
+    let realm = builder.build().await?;
 
-        let inspect = get_session_manager_inspect(&realm, "root/session_started_at/0").await?;
+    let inspect = get_session_manager_inspect(&realm, "root/session_started_at").await?;
 
-        // Assert the session has been launched once.
-        assert_eq!(
-            1,
-            inspect
-                .get_child("session_started_at")
-                .expect("session_started_at is not none")
-                .children
-                .len()
-        );
+    // No sessions should have launched.
+    assert_eq!(0, inspect.get_child("session_started_at").unwrap().children.len());
 
-        realm.destroy().await?;
-        Ok(())
-    }
-
-    #[fuchsia::test]
-    async fn test_noautolaunch_does_not_launch() -> anyhow::Result<()> {
-        let builder = RealmBuilder::new().await?;
-
-        add_session_manager(
-            &builder,
-            "hello-world-session#meta/hello-world-session.cm".to_string(),
-            false,
-        )
-        .await?;
-
-        let realm = builder.build().await?;
-
-        let inspect = get_session_manager_inspect(&realm, "root/session_started_at").await?;
-
-        // No sessions should have launched.
-        assert_eq!(0, inspect.get_child("session_started_at").unwrap().children.len());
-
-        realm.destroy().await?;
-        Ok(())
-    }
-
-    #[fuchsia::test]
-    async fn noautolaunch_file_overrides_structured_config() -> anyhow::Result<()> {
-        let builder = RealmBuilder::new().await?;
-
-        let session_manager = add_session_manager(
-            &builder,
-            "hello-world-session#meta/hello-world-session.cm".to_string(),
-            true,
-        )
-        .await?;
-
-        builder
-            .read_only_directory(
-                "root-data",
-                vec![&session_manager],
-                DirectoryContents::new().add_file("session-manager/noautolaunch", ""),
-            )
-            .await?;
-
-        let realm = builder.build().await?;
-
-        let inspect = get_session_manager_inspect(&realm, "root/session_started_at").await?;
-
-        // No sessions should have launched.
-        assert_eq!(0, inspect.get_child("session_started_at").unwrap().children.len());
-
-        realm.destroy().await?;
-        Ok(())
-    }
+    realm.destroy().await?;
+    Ok(())
 }
