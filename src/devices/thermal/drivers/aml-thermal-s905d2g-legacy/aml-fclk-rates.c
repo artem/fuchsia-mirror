594ee3be3bde9ddca8b0779574d004f3e4592cd9
Caslyn Tonelli <caslyn@google.com>
2024-04-16 18:29:43 +0000

    [drivers][aml-thermal] Replace countof with ARRAY_SIZE
    The countof macro in zircon/compiler.h will be removed.
    Replace use with local ARRAY_SIZE macro for this c file.
    
    Bug: 42165754
    Change-Id: Ie8bb16c35fe1d9d16c11ed2ec7dcb2818f31727d
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1028773
    Fuchsia-Auto-Submit: Caslyn Tonelli <caslyn@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>
    Reviewed-by: Puneetha Ramachandra <puneetha@google.com>

--- src/devices/thermal/drivers/aml-thermal-s905d2g-legacy/aml-fclk-rates.c
+++ src/devices/thermal/drivers/aml-thermal-s905d2g-legacy/aml-fclk-rates.c
@@ -6,6 +6,8 @@
 
 #include "aml-fclk.h"
 
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(*(x)))
+
 #define FCLK_PLL_RATE(_r, _premux, _postmux, _mux_div) \
   { .rate = (_r), .premux = (_premux), .postmux = (_postmux), .mux_div = (_mux_div), }
 
@@ -18,4 +20,4 @@ static const aml_fclk_rate_table_t fclk_pll_rate_table[] = {
 
 const aml_fclk_rate_table_t* s905d2_fclk_get_rate_table(void) { return fclk_pll_rate_table; }
 
-size_t s905d2_fclk_get_rate_table_count(void) { return countof(fclk_pll_rate_table); }
+size_t s905d2_fclk_get_rate_table_count(void) { return ARRAY_SIZE(fclk_pll_rate_table); }
