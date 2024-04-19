0d59df34dbd5e367260d42b38e92ff440d59d365
Ghanan Gowripalan <ghanan@google.com>
2024-04-19 16:03:28 +0000

    [starnix/tests] Use `unique_fd` in vmsplice tests
    ...and improve some logging on assertion failures.
    
    Bug: b/335701084
    Change-Id: I2410fcdfd9483c5fa95af08e35dfb72ce7d10ce0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1031279
    Reviewed-by: Adam Barth <abarth@google.com>
    Commit-Queue: Ghanan Gowripalan <ghanan@google.com>
    Fuchsia-Auto-Submit: Ghanan Gowripalan <ghanan@google.com>

--- src/starnix/tests/syscalls/cpp/vmsplice_test.cc
+++ src/starnix/tests/syscalls/cpp/vmsplice_test.cc
@@ -10,44 +10,55 @@
 #include <cstdlib>
 #include <string>
 
+#include <fbl/unique_fd.h>
 #include <gtest/gtest.h>
 
 namespace {
 
+constexpr size_t kNumPipeEndsCount = 2;
+
+void MakePipe(fbl::unique_fd (&fds)[kNumPipeEndsCount]) {
+  int raw_fds[kNumPipeEndsCount] = {};
+  ASSERT_EQ(pipe(raw_fds), 0) << strerror(errno);
+  for (size_t i = 0; i < kNumPipeEndsCount; ++i) {
+    fds[i] = fbl::unique_fd(raw_fds[i]);
+    ASSERT_TRUE(fds[i]);
+  }
+}
+
 TEST(VmspliceTest, VmspliceReadClosedPipe) {
-  int fds[2];
-  ASSERT_EQ(pipe(fds), 0);
-  ASSERT_EQ(close(fds[1]), 0);
+  fbl::unique_fd fds[kNumPipeEndsCount];
+  ASSERT_NO_FATAL_FAILURE(MakePipe(fds));
+  ASSERT_EQ(fds[1].reset(), 0) << strerror(errno);
   char buffer[4096] = {};
   iovec iov = {
       .iov_base = buffer,
       .iov_len = sizeof(buffer),
   };
-  ASSERT_EQ(vmsplice(fds[0], &iov, 1, SPLICE_F_NONBLOCK), 0);
-  ASSERT_EQ(vmsplice(fds[0], &iov, 1, 0), 0);
-  close(fds[0]);
+  ASSERT_EQ(vmsplice(fds[0].get(), &iov, 1, SPLICE_F_NONBLOCK), 0) << strerror(errno);
+  ASSERT_EQ(vmsplice(fds[0].get(), &iov, 1, 0), 0) << strerror(errno);
 }
 
 TEST(VmspliceTest, VmspliceWriteClosedPipe) {
+  constexpr int kRetCode = 42;
   EXPECT_EXIT(([]() {
                 signal(SIGPIPE, SIG_IGN);
 
-                int fds[2];
-                ASSERT_EQ(pipe(fds), 0);
-                ASSERT_EQ(close(fds[0]), 0);
+                fbl::unique_fd fds[kNumPipeEndsCount];
+                ASSERT_NO_FATAL_FAILURE(MakePipe(fds));
+                ASSERT_EQ(fds[0].reset(), 0) << strerror(errno);
                 char buffer[4096] = {};
                 iovec iov = {
                     .iov_base = buffer,
                     .iov_len = sizeof(buffer),
                 };
-                ASSERT_EQ(vmsplice(fds[1], &iov, 1, SPLICE_F_NONBLOCK), -1);
+                ASSERT_EQ(vmsplice(fds[1].get(), &iov, 1, SPLICE_F_NONBLOCK), -1);
                 ASSERT_EQ(errno, EPIPE);
-                ASSERT_EQ(vmsplice(fds[1], &iov, 1, 0), -1);
+                ASSERT_EQ(vmsplice(fds[1].get(), &iov, 1, 0), -1);
                 ASSERT_EQ(errno, EPIPE);
-                close(fds[1]);
-                exit(42);
+                exit(kRetCode);
               })(),
-              testing::ExitedWithCode(42), "");
+              testing::ExitedWithCode(kRetCode), "");
 }
 
 }  // namespace
