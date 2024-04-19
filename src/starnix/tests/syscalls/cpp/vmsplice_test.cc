0e8f3e0afaad68a4f782541d04b55ffb911b8b6e
Ghanan Gowripalan <ghanan@google.com>
2024-04-19 16:04:14 +0000

    [starnix/tests] Test changes to `vmsplice`d memory
    `vmsplice` should not copy bytes to a pipe's buffer; it should maintain
    a reference to the user buffer's backing memory without performing any
    copies. This lets userspace modify the bytes while its in the pipe, but
    before it is read. Unmapping the memory (after `vmsplice`-ing it) from
    userspace does not seem to affect the mapping held in the pipe.
    
    Bug: b/335701084
    Change-Id: I6ed338c96c961e14346666427ed64b3258c6710b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1031280
    Fuchsia-Auto-Submit: Ghanan Gowripalan <ghanan@google.com>
    Reviewed-by: Adam Barth <abarth@google.com>
    Commit-Queue: Ghanan Gowripalan <ghanan@google.com>

--- src/starnix/tests/syscalls/cpp/vmsplice_test.cc
+++ src/starnix/tests/syscalls/cpp/vmsplice_test.cc
@@ -3,7 +3,9 @@
 // found in the LICENSE file.
 
 #include <fcntl.h>
+#include <lib/fit/defer.h>
 #include <stdlib.h>
+#include <sys/mman.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
@@ -11,6 +13,7 @@
 #include <string>
 
 #include <fbl/unique_fd.h>
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 namespace {
@@ -59,6 +62,117 @@ TEST(VmspliceTest, VmspliceWriteClosedPipe) {
                 exit(kRetCode);
               })(),
               testing::ExitedWithCode(kRetCode), "");
+}
+
+TEST(VmspliceTest, ModifyBufferInPipe) {
+  constexpr char kInitialByte = 'A';
+  constexpr char kModifiedByte = 'B';
+
+  fbl::unique_fd fds[kNumPipeEndsCount];
+  ASSERT_NO_FATAL_FAILURE(MakePipe(fds));
+  // Volatile to let the compiler know not to optimize out the final write we
+  // perform to `write_buffer` which is never directly read here.
+  volatile char write_buffer[10];
+  std::fill_n(&write_buffer[0], std::size(write_buffer), kInitialByte);
+  {
+    iovec iov = {
+        .iov_base = const_cast<char*>(write_buffer),
+        .iov_len = sizeof(write_buffer),
+    };
+    ASSERT_EQ(vmsplice(fds[1].get(), &iov, 1, 0), static_cast<ssize_t>(sizeof(write_buffer)))
+        << strerror(errno);
+  }
+  std::fill_n(&write_buffer[0], std::size(write_buffer), kModifiedByte);
+
+  char read_buffer[sizeof(write_buffer)] = {};
+  ASSERT_EQ(read(fds[0].get(), read_buffer, sizeof(read_buffer)),
+            static_cast<ssize_t>(sizeof(read_buffer)))
+      << strerror(errno);
+  EXPECT_THAT(read_buffer, testing::Each(kModifiedByte));
+}
+
+TEST(VmspliceTest, UnmapBufferInPipe) {
+  constexpr size_t kMmapSize = 4096;
+  constexpr char kInitialByte = 'A';
+  constexpr char kModifiedByte = 'B';
+
+  fbl::unique_fd fds[kNumPipeEndsCount];
+  ASSERT_NO_FATAL_FAILURE(MakePipe(fds));
+  {
+    // Volatile to let the compiler know not to optimize out the final write we
+    // perform to `write_buffer` which is never directly read here.
+    volatile char* write_buffer = reinterpret_cast<volatile char*>(
+        mmap(NULL, kMmapSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+    ASSERT_NE(write_buffer, MAP_FAILED) << strerror(errno);
+    auto unmap_write_buffer = fit::defer([&]() {
+      EXPECT_EQ(munmap(const_cast<char*>(write_buffer), kMmapSize), 0) << strerror(errno);
+    });
+    std::fill_n(write_buffer, kMmapSize, kInitialByte);
+    {
+      iovec iov = {
+          .iov_base = const_cast<char*>(write_buffer),
+          .iov_len = kMmapSize,
+      };
+      ASSERT_EQ(vmsplice(fds[1].get(), &iov, 1, 0), static_cast<ssize_t>(kMmapSize))
+          << strerror(errno);
+    }
+    std::fill_n(&write_buffer[0], kMmapSize, kModifiedByte);
+  }
+
+  char read_buffer[kMmapSize] = {};
+  ASSERT_EQ(read(fds[0].get(), read_buffer, sizeof(read_buffer)),
+            static_cast<ssize_t>(sizeof(read_buffer)))
+      << strerror(errno);
+  EXPECT_THAT(read_buffer, testing::Each(kModifiedByte));
+}
+
+TEST(VmspliceTest, UnmapBufferInPipeThenMapInPlace) {
+  constexpr size_t kMmapSize = 4096;
+  constexpr char kInitialByte = 'A';
+  constexpr char kModifiedByte = 'B';
+
+  fbl::unique_fd fds[kNumPipeEndsCount];
+  ASSERT_NO_FATAL_FAILURE(MakePipe(fds));
+  // Volatile to let the compiler know not to optimize out the final write we
+  // perform to `write_buffer` which is never directly read here.
+  volatile char* write_buffer = reinterpret_cast<volatile char*>(
+      mmap(NULL, kMmapSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+  {
+    ASSERT_NE(write_buffer, MAP_FAILED) << strerror(errno);
+    auto unmap_write_buffer = fit::defer([&]() {
+      EXPECT_EQ(munmap(const_cast<char*>(write_buffer), kMmapSize), 0) << strerror(errno);
+    });
+    std::fill_n(write_buffer, kMmapSize, kInitialByte);
+    {
+      iovec iov = {
+          .iov_base = const_cast<char*>(write_buffer),
+          .iov_len = kMmapSize,
+      };
+      ASSERT_EQ(vmsplice(fds[1].get(), &iov, 1, 0), static_cast<ssize_t>(kMmapSize))
+          << strerror(errno);
+    }
+    std::fill_n(&write_buffer[0], kMmapSize, kModifiedByte);
+  }
+
+  {
+    // Volatile to let the compiler know not to optimize out the final write we
+    // perform to `write_buffer_new` which is never read here.
+    volatile char* write_buffer_new = reinterpret_cast<volatile char*>(
+        mmap(const_cast<char*>(write_buffer), kMmapSize, PROT_READ | PROT_WRITE,
+             MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+    ASSERT_NE(write_buffer_new, MAP_FAILED) << strerror(errno);
+    ASSERT_EQ(write_buffer, write_buffer_new);
+    auto unmap_write_buffer = fit::defer([&]() {
+      EXPECT_EQ(munmap(const_cast<char*>(write_buffer_new), kMmapSize), 0) << strerror(errno);
+    });
+    std::fill_n(&write_buffer[0], kMmapSize, kInitialByte);
+  }
+
+  char read_buffer[kMmapSize] = {};
+  ASSERT_EQ(read(fds[0].get(), read_buffer, sizeof(read_buffer)),
+            static_cast<ssize_t>(sizeof(read_buffer)))
+      << strerror(errno);
+  EXPECT_THAT(read_buffer, testing::Each(kModifiedByte));
 }
 
 }  // namespace
