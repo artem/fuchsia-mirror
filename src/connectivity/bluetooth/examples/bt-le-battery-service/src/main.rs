c39456f6872348d741e891677303f9c160042229
Ani Ramakrishnan <aniramakri@google.com>
2024-04-22 18:33:51 +0000

    [bt][examples] Add basic tests to the LE battery service
    Add some tests to verify the common paths for the LE battery service.
    This also means that LSC and API changes will be caught in CI/CQ for
    this component since it is not included in any configurations.
    
    Bug: b/326423915
    Test: fx test bt-le-battery-service-tests
    Change-Id: I8d278df48fd5df4ff9660ed8512a5d45eda7e753
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1033923
    Reviewed-by: Ben Lawson <benlawson@google.com>
    Commit-Queue: Ani Ramakrishnan <aniramakri@google.com>

--- src/connectivity/bluetooth/examples/bt-le-battery-service/src/main.rs
+++ src/connectivity/bluetooth/examples/bt-le-battery-service/src/main.rs
@@ -18,8 +18,8 @@ use tracing::{info, warn};
 
 /// Arbitrary Handle assigned to the Battery Service.
 const BATTERY_SERVICE_HANDLE: gatt::ServiceHandle = gatt::ServiceHandle { value: 10 };
-/// Fixed Handle assigned to the Battery characteristic.
-const BATTERY_CHARACTERISTIC_HANDLE: gatt::Handle = gatt::Handle { value: 1 };
+/// Fixed Handle assigned to the Battery Level characteristic.
+const BATTERY_LEVEL_CHARACTERISTIC_HANDLE: gatt::Handle = gatt::Handle { value: 1 };
 const BATTERY_SERVICE_UUID: &str = "0000180f-0000-1000-8000-00805f9b34fb";
 const BATTERY_LEVEL_UUID: &str = "00002A19-0000-1000-8000-00805f9b34fb";
 
@@ -54,11 +54,12 @@ impl BatteryState {
     /// of the change.
     pub fn set_level(&self, level: u8) -> Result<(), Error> {
         let mut inner = self.inner.lock();
-        if inner.level != level {
-            info!("Battery percentage changed ({}%)", level);
+        let current_level = inner.level;
+        if current_level != level {
+            info!("Battery level changed: (current = {current_level}%, new = {level}%)");
             let params = gatt::ValueChangedParameters {
                 peer_ids: Some(inner.peers.iter().cloned().map(Into::into).collect()),
-                handle: Some(BATTERY_CHARACTERISTIC_HANDLE),
+                handle: Some(BATTERY_LEVEL_CHARACTERISTIC_HANDLE),
                 value: Some(vec![level]),
                 ..Default::default()
             };
@@ -77,7 +78,8 @@ struct BatteryStateInner {
     /// The control handle used to send GATT notifications.
     service: gatt::LocalServiceControlHandle,
 
-    /// Set of remote peers that have subscribed to GATT battery notifications.
+    /// Set of remote peers that have subscribed to GATT notifications for the
+    /// Battery Level characteristic.
     peers: HashSet<PeerId>,
 }
 
@@ -102,7 +104,7 @@ async fn gatt_service_delegate(
                 peer_id, notify, responder, ..
             } => {
                 let peer_id = peer_id.into();
-                info!("Peer {} configured characteristic (notify: {})", peer_id, notify);
+                info!(%peer_id, "Configured characteristic (notify: {notify})");
                 if notify {
                     state.add_peer(peer_id);
                 } else {
@@ -117,7 +119,7 @@ async fn gatt_service_delegate(
             }
         }
     }
-    warn!("GATT Battery service was closed");
+    warn!("Battery GATT Server was closed");
     Ok(())
 }
 
@@ -132,7 +134,7 @@ async fn battery_manager_watcher(
         }
     }
 
-    warn!("BatteryClient was closed; battery level no longer available.");
+    warn!("BatteryClient was closed; battery level updates are no longer available.");
     Ok(())
 }
 
@@ -163,9 +165,9 @@ async fn main() -> Result<(), Error> {
     let battery_client = BatteryClient::create()?;
     let state = BatteryState::new(service_notification_handle);
 
-    // Build a GATT Battery service.
-    let characteristic = gatt::Characteristic {
-        handle: Some(BATTERY_CHARACTERISTIC_HANDLE),
+    // Build a GATT Battery service with the mandatory Battery Level characteristic.
+    let battery_level_chrc = gatt::Characteristic {
+        handle: Some(BATTERY_LEVEL_CHARACTERISTIC_HANDLE),
         type_: Uuid::from_str(BATTERY_LEVEL_UUID).ok().map(Into::into),
         properties: Some(
             gatt::CharacteristicPropertyBits::READ | gatt::CharacteristicPropertyBits::NOTIFY,
@@ -181,7 +183,7 @@ async fn main() -> Result<(), Error> {
         handle: Some(BATTERY_SERVICE_HANDLE),
         kind: Some(gatt::ServiceKind::Primary),
         type_: Uuid::from_str(BATTERY_SERVICE_UUID).ok().map(Into::into),
-        characteristics: Some(vec![characteristic]),
+        characteristics: Some(vec![battery_level_chrc]),
         ..Default::default()
     };
 
@@ -196,4 +198,105 @@ async fn main() -> Result<(), Error> {
     let service_delegate = gatt_service_delegate(&state, service_stream);
     let battery_watcher = battery_manager_watcher(&state, battery_client);
     try_join!(service_delegate, battery_watcher).map(|((), ())| ())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use assert_matches::assert_matches;
+    use async_test_helpers::expect_stream_item;
+    use async_utils::PollExt;
+    use fuchsia_async as fasync;
+    use std::pin::pin;
+
+    #[fuchsia::test]
+    fn read_battery_level() {
+        let mut exec = fasync::TestExecutor::new();
+        let (client, server) =
+            fidl::endpoints::create_proxy_and_stream::<gatt::LocalServiceMarker>().unwrap();
+        let service_notification_handle = server.control_handle();
+        let state = BatteryState::new(service_notification_handle);
+        let mut gatt_server_fut = pin!(gatt_service_delegate(&state, server));
+        exec.run_until_stalled(&mut gatt_server_fut).expect_pending("server active");
+
+        let mut read_fut =
+            pin!(client.read_value(&PeerId(123).into(), &BATTERY_LEVEL_CHARACTERISTIC_HANDLE, 0));
+        exec.run_until_stalled(&mut read_fut).expect_pending("GATT read waiting for response");
+        exec.run_until_stalled(&mut gatt_server_fut).expect_pending("server active");
+        let result = exec
+            .run_until_stalled(&mut read_fut)
+            .expect("GATT read response")
+            .expect("successful FIDL request");
+        // The initial battery level is 0.
+        assert_eq!(result, Ok(vec![0]));
+    }
+
+    #[fuchsia::test]
+    fn battery_level_change_notifications() {
+        let mut exec = fasync::TestExecutor::new();
+        let (client, server) =
+            fidl::endpoints::create_proxy_and_stream::<gatt::LocalServiceMarker>().unwrap();
+        let service_notification_handle = server.control_handle();
+        let state = BatteryState::new(service_notification_handle);
+        let mut gatt_server_fut = pin!(gatt_service_delegate(&state, server));
+        exec.run_until_stalled(&mut gatt_server_fut).expect_pending("server active");
+
+        // Peer subscribes to notifications on the Battery Level characteristic.
+        let id = PeerId(123).into();
+        let mut notify_fut = pin!(client.characteristic_configuration(
+            &id,
+            &BATTERY_LEVEL_CHARACTERISTIC_HANDLE,
+            true,
+            false
+        ));
+        exec.run_until_stalled(&mut notify_fut)
+            .expect_pending("GATT notify request waiting for response");
+        exec.run_until_stalled(&mut gatt_server_fut).expect_pending("server active");
+        let result = exec.run_until_stalled(&mut notify_fut).expect("GATT notify response");
+        assert_matches!(result, Ok(()));
+
+        // Simulate battery level change.
+        state.set_level(50).expect("successfully notified");
+
+        // Should receive battery level as a notification on the FIDL client event stream.
+        let mut notifications = client.take_event_stream();
+        let event = expect_stream_item(&mut exec, &mut notifications);
+        match event {
+            Ok(gatt::LocalServiceEvent::OnNotifyValue {
+                payload: gatt::ValueChangedParameters { handle, peer_ids, value, .. },
+            }) => {
+                assert_eq!(handle, Some(BATTERY_LEVEL_CHARACTERISTIC_HANDLE));
+                assert_eq!(peer_ids, Some(vec![id]));
+                assert_eq!(value, Some(vec![50]));
+            }
+            x => panic!("Expected notification, got: {x:?}"),
+        }
+    }
+
+    #[fuchsia::test]
+    fn write_is_error() {
+        let mut exec = fasync::TestExecutor::new();
+        let (client, server) =
+            fidl::endpoints::create_proxy_and_stream::<gatt::LocalServiceMarker>().unwrap();
+        let service_notification_handle = server.control_handle();
+        let state = BatteryState::new(service_notification_handle);
+        let mut gatt_server_fut = pin!(gatt_service_delegate(&state, server));
+
+        exec.run_until_stalled(&mut gatt_server_fut).expect_pending("server active");
+
+        // The Battery Level characteristic doesn't support GATT writes.
+        let request = gatt::LocalServiceWriteValueRequest {
+            peer_id: Some(PeerId(123).into()),
+            handle: Some(BATTERY_LEVEL_CHARACTERISTIC_HANDLE),
+            offset: Some(0),
+            value: Some(vec![1]),
+            ..Default::default()
+        };
+        let mut write_fut = pin!(client.write_value(&request));
+        exec.run_until_stalled(&mut write_fut).expect_pending("GATT write waiting for response");
+        exec.run_until_stalled(&mut gatt_server_fut).expect_pending("server active");
+        let result = exec.run_until_stalled(&mut write_fut).expect("GATT write response");
+        assert_matches!(result, Ok(Err(gatt::Error::WriteRequestRejected)));
+    }
 }
