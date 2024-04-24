074205ee4f7e61cbeb17191494bd23829d4e2ee0
Ghanan Gowripalan <ghanan@google.com>
2024-04-24 23:35:04 +0000

    [starnix/tests] Test `mremap`/`vmsplice` together
    Looks like:
    1) remapping a new private mapping on top of a `vmsplice`-ed private
       mapping does not modify the payload held in a pipe. Seems like
       the pipe payload continues to point to the old (POV of userspace)
       mapping.
    2) writes to memory through a mapping that aliases a shared `vmsplice`-ed
       mapping modifies the payload held in a pipe. This makes sense since all
       mappings (memory pointed to by userspace and the pipe payload) point to
       the same backing memory.
    
    Note that `VmspliceTest.RemapNewMemoryOverBufferInPipe` trivially passes
    on Starnix because Starnix does not share the mapping with the buffer
    provided to `vmsplice`.
    
    Bug: b/335701084
    Change-Id: Ia501c89bdf784e873f8e690df9171b4c01e09e30
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1032000
    Reviewed-by: James Robinson <jamesr@google.com>
    Commit-Queue: Ghanan Gowripalan <ghanan@google.com>
    Fuchsia-Auto-Submit: Ghanan Gowripalan <ghanan@google.com>

--- src/starnix/tests/syscalls/cpp/vmsplice_test.cc
+++ src/starnix/tests/syscalls/cpp/vmsplice_test.cc
@@ -116,7 +116,7 @@ TEST(VmspliceTest, UnmapBufferInPipe) {
       ASSERT_EQ(vmsplice(fds[1].get(), &iov, 1, 0), static_cast<ssize_t>(kMmapSize))
           << strerror(errno);
     }
-    std::fill_n(&write_buffer[0], kMmapSize, kModifiedByte);
+    std::fill_n(write_buffer, kMmapSize, kModifiedByte);
   }
 
   char read_buffer[kMmapSize] = {};
@@ -151,7 +151,7 @@ TEST(VmspliceTest, UnmapBufferInPipeThenMapInPlace) {
       ASSERT_EQ(vmsplice(fds[1].get(), &iov, 1, 0), static_cast<ssize_t>(kMmapSize))
           << strerror(errno);
     }
-    std::fill_n(&write_buffer[0], kMmapSize, kModifiedByte);
+    std::fill_n(write_buffer, kMmapSize, kModifiedByte);
   }
 
   {
@@ -159,13 +159,13 @@ TEST(VmspliceTest, UnmapBufferInPipeThenMapInPlace) {
     // perform to `write_buffer_new` which is never read here.
     volatile char* write_buffer_new = reinterpret_cast<volatile char*>(
         mmap(const_cast<char*>(write_buffer), kMmapSize, PROT_READ | PROT_WRITE,
-             MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+             MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0));
     ASSERT_NE(write_buffer_new, MAP_FAILED) << strerror(errno);
     ASSERT_EQ(write_buffer, write_buffer_new);
-    auto unmap_write_buffer = fit::defer([&]() {
+    auto unmap_write_buffer_new = fit::defer([&]() {
       EXPECT_EQ(munmap(const_cast<char*>(write_buffer_new), kMmapSize), 0) << strerror(errno);
     });
-    std::fill_n(&write_buffer[0], kMmapSize, kInitialByte);
+    std::fill_n(write_buffer_new, kMmapSize, kInitialByte);
   }
 
   char read_buffer[kMmapSize] = {};
@@ -174,5 +174,202 @@ TEST(VmspliceTest, UnmapBufferInPipeThenMapInPlace) {
       << strerror(errno);
   EXPECT_THAT(read_buffer, testing::Each(kModifiedByte));
 }
+
+class VmspliceRemapNewMemoryOverBufferInPipeTest : public testing::TestWithParam<bool> {};
+
+TEST_P(VmspliceRemapNewMemoryOverBufferInPipeTest, RemapNewMemoryOverBufferInPipe) {
+  constexpr char kInitialByte = 'A';
+  constexpr char kModifiedByte = 'B';
+  constexpr size_t kMmapSize = 4096;
+
+  fbl::unique_fd fds[kNumPipeEndsCount];
+  ASSERT_NO_FATAL_FAILURE(MakePipe(fds));
+  // Volatile to let the compiler know not to optimize out the final write we
+  // perform to `write_buffer` which is never read here.
+  volatile char* write_buffer = reinterpret_cast<volatile char*>(
+      mmap(NULL, kMmapSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+  ASSERT_NE(write_buffer, MAP_FAILED) << strerror(errno);
+  auto unmap_write_buffer = fit::defer([&]() {
+    EXPECT_EQ(munmap(const_cast<char*>(write_buffer), kMmapSize), 0) << strerror(errno);
+  });
+  std::fill_n(write_buffer, kMmapSize, kInitialByte);
+  {
+    iovec iov = {
+        .iov_base = const_cast<char*>(write_buffer),
+        .iov_len = kMmapSize,
+    };
+    ASSERT_EQ(vmsplice(fds[1].get(), &iov, 1, 0), static_cast<ssize_t>(kMmapSize))
+        << strerror(errno);
+  }
+
+  {
+    // Volatile to let the compiler know not to optimize out the final write we
+    // perform to `write_buffer_new` which is never read here.
+    volatile char* write_buffer_new = reinterpret_cast<volatile char*>(
+        mmap(NULL, kMmapSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+    ASSERT_NE(write_buffer_new, MAP_FAILED) << strerror(errno);
+    ASSERT_NE(write_buffer_new, write_buffer);
+    auto unmap_write_buffer_new = fit::defer([&]() {
+      EXPECT_EQ(munmap(const_cast<char*>(write_buffer_new), kMmapSize), 0) << strerror(errno);
+    });
+    std::fill_n(write_buffer_new, kMmapSize, kModifiedByte);
+
+    void* remapped_addr = mremap(const_cast<char*>(write_buffer_new), kMmapSize, kMmapSize,
+                                 MREMAP_MAYMOVE | MREMAP_FIXED, const_cast<char*>(write_buffer));
+    ASSERT_NE(remapped_addr, MAP_FAILED) << strerror(errno);
+    ASSERT_EQ(remapped_addr, write_buffer);
+    // The unmapping will be handled by `unmap_write_buffer`.
+    unmap_write_buffer_new.cancel();
+  }
+
+  if (GetParam()) {
+    // This write is performed on the _new_ mapping, not the old one which is
+    // "shared" with the paylaod sitting in the pipe buffer. Even though we no
+    // longer hold a reference to the private anon mapping, the kernel continues
+    // to keep it alive until it is read from the pipe.
+    std::fill_n(write_buffer, kMmapSize, kModifiedByte);
+  }
+
+  char read_buffer[kMmapSize] = {};
+  ASSERT_EQ(read(fds[0].get(), read_buffer, sizeof(read_buffer)),
+            static_cast<ssize_t>(sizeof(read_buffer)))
+      << strerror(errno);
+  EXPECT_THAT(read_buffer, testing::Each(kInitialByte));
+}
+
+void AliasSharedMapping(volatile char* addr, size_t size, volatile char** alias) {
+  // First allocate a mapping that will accommodate our alias mapping. This is
+  // where the original mapping will alias from.
+  void* remap_target = mmap(NULL, size, 0, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  ASSERT_NE(remap_target, MAP_FAILED) << strerror(errno);
+  // The aliased mapping obviously can't be the same as our original mapping.
+  ASSERT_NE(remap_target, addr);
+  auto unmap_remap_target =
+      fit::defer([&]() { EXPECT_EQ(munmap(remap_target, size), 0) << strerror(errno); });
+
+  void* remapped_addr =
+      mremap(const_cast<char*>(addr), 0, size, MREMAP_MAYMOVE | MREMAP_FIXED, remap_target);
+  ASSERT_NE(remapped_addr, MAP_FAILED) << strerror(errno);
+  ASSERT_EQ(remapped_addr, remap_target);
+
+  unmap_remap_target.cancel();
+  *alias = reinterpret_cast<volatile char*>(remapped_addr);
+}
+
+INSTANTIATE_TEST_SUITE_P(VmspliceTest, VmspliceRemapNewMemoryOverBufferInPipeTest,
+                         testing::Values(true, false));
+
+TEST(VmspliceTest, RemapSharedBufferInPipeThenModify) {
+  constexpr char kInitialByte = 'A';
+  constexpr char kModifiedByte = 'B';
+  constexpr size_t kMmapSize = 4096;
+
+  fbl::unique_fd fds[kNumPipeEndsCount];
+  ASSERT_NO_FATAL_FAILURE(MakePipe(fds));
+  // Volatile to let the compiler know not to optimize out the final write we
+  // perform to `write_buffer` which is never read here.
+  volatile char* write_buffer = reinterpret_cast<volatile char*>(
+      mmap(NULL, kMmapSize, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+  ASSERT_NE(write_buffer, MAP_FAILED) << strerror(errno);
+  auto unmap_write_buffer = fit::defer([&]() {
+    EXPECT_EQ(munmap(const_cast<char*>(write_buffer), kMmapSize), 0) << strerror(errno);
+  });
+  std::fill_n(write_buffer, kMmapSize, kInitialByte);
+  {
+    iovec iov = {
+        .iov_base = const_cast<char*>(write_buffer),
+        .iov_len = kMmapSize,
+    };
+    ASSERT_EQ(vmsplice(fds[1].get(), &iov, 1, 0), static_cast<ssize_t>(kMmapSize))
+        << strerror(errno);
+  }
+
+  {
+    // Volatile to let the compiler know not to optimize out the final write we
+    // perform to `write_buffer_new` which is never read here.
+    volatile char* write_buffer_new;
+    ASSERT_NO_FATAL_FAILURE(AliasSharedMapping(write_buffer, kMmapSize, &write_buffer_new));
+    auto unmap_write_buffer_new = fit::defer([&]() {
+      EXPECT_EQ(munmap(const_cast<char*>(write_buffer_new), kMmapSize), 0) << strerror(errno);
+    });
+
+    // `write_buffer_new` and `write_buffer` should point to the same memory even
+    // though they are different virtual addresses.
+    ASSERT_NE(write_buffer_new, write_buffer);
+    std::fill_n(write_buffer_new, kMmapSize, kModifiedByte);
+    std::for_each_n(write_buffer, kMmapSize, [&](char b) { EXPECT_EQ(b, kModifiedByte); });
+  }
+
+  char read_buffer[kMmapSize] = {};
+  ASSERT_EQ(read(fds[0].get(), read_buffer, sizeof(read_buffer)),
+            static_cast<ssize_t>(sizeof(read_buffer)))
+      << strerror(errno);
+  EXPECT_THAT(read_buffer, testing::Each(kModifiedByte));
+}
+
+template <typename U>
+void VmspliceUnmapThenModify(fbl::unique_fd& fd, volatile char* addr, volatile char* other_addr,
+                             size_t size, char new_byte, U& unmap) {
+  iovec iov = {
+      .iov_base = const_cast<char*>(addr),
+      .iov_len = size,
+  };
+  ASSERT_EQ(vmsplice(fd.get(), &iov, 1, 0), static_cast<ssize_t>(size)) << strerror(errno);
+  unmap.call();
+  std::fill_n(other_addr, size, new_byte);
+}
+
+class VmspliceUnmapBufferAfterVmsplicingRemappedBufferTest : public testing::TestWithParam<bool> {};
+
+TEST_P(VmspliceUnmapBufferAfterVmsplicingRemappedBufferTest,
+       UnmapBufferAfterVmsplicingRemappedBuffer) {
+  constexpr char kInitialByte = 'A';
+  constexpr char kModifiedByte = 'B';
+  constexpr size_t kMmapSize = 4096;
+
+  fbl::unique_fd fds[kNumPipeEndsCount];
+  ASSERT_NO_FATAL_FAILURE(MakePipe(fds));
+  // Volatile to let the compiler know not to optimize out the final write we
+  // perform to `write_buffer` which is never read here.
+  volatile char* write_buffer = reinterpret_cast<volatile char*>(
+      mmap(NULL, kMmapSize, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+  ASSERT_NE(write_buffer, MAP_FAILED) << strerror(errno);
+  auto unmap_write_buffer = fit::defer([&]() {
+    EXPECT_EQ(munmap(const_cast<char*>(write_buffer), kMmapSize), 0) << strerror(errno);
+  });
+  std::fill_n(write_buffer, kMmapSize, kInitialByte);
+
+  // Volatile to let the compiler know not to optimize out the final write we
+  // perform to `write_buffer_new` which is never read here.
+  volatile char* write_buffer_new;
+  ASSERT_NO_FATAL_FAILURE(AliasSharedMapping(write_buffer, kMmapSize, &write_buffer_new));
+  auto unmap_write_buffer_new = fit::defer([&]() {
+    EXPECT_EQ(munmap(const_cast<char*>(write_buffer_new), kMmapSize), 0) << strerror(errno);
+  });
+
+  // `write_buffer_new` and `write_buffer` should point to the same memory even
+  // though they are different virtual addresses.
+  ASSERT_NE(write_buffer_new, write_buffer);
+  std::for_each_n(write_buffer_new, kMmapSize, [&](char b) { EXPECT_EQ(b, kInitialByte); });
+
+  // Vmsplice one of the mappings, unmap the vmspliced region then modify the memory
+  // through the other region.
+  if (GetParam()) {
+    ASSERT_NO_FATAL_FAILURE(VmspliceUnmapThenModify(fds[1], write_buffer, write_buffer_new,
+                                                    kMmapSize, kModifiedByte, unmap_write_buffer));
+  } else {
+    ASSERT_NO_FATAL_FAILURE(VmspliceUnmapThenModify(
+        fds[1], write_buffer_new, write_buffer, kMmapSize, kModifiedByte, unmap_write_buffer_new));
+  }
+
+  char read_buffer[kMmapSize] = {};
+  ASSERT_EQ(read(fds[0].get(), read_buffer, sizeof(read_buffer)),
+            static_cast<ssize_t>(sizeof(read_buffer)))
+      << strerror(errno);
+  EXPECT_THAT(read_buffer, testing::Each(kModifiedByte));
+}
+
+INSTANTIATE_TEST_SUITE_P(VmspliceTest, VmspliceUnmapBufferAfterVmsplicingRemappedBufferTest,
+                         testing::Values(true, false));
 
 }  // namespace
