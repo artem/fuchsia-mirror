5579eb7e5171b06f65d5d208d118802f0d0f83ab
Pavan Kumar Juturu <jpavankumar@google.com>
2024-04-25 20:37:34 +0000

    [Lacewing] Changes to TestCaseRevive
    1. TestCaseRevice now supports test cases that starts with "_test_*" in addition to "test_*". This will be useful if you want to run the test only in revised fashion (without running the test standalone)
    
    2. Introduced a way to specify how often to run the test case method in revised sequence in the context of device operation that will be performed: PRE_ONLY, POST_ONLY, PRE_AND_POST
    
    3. User can now specify fuchsia_device_operation and test_method_execution_frequency options either as user params (in yaml or in build.gn) or in "tag_test()" decorator. This will give flexibiltiy to the user. If the test class contain multiple test cases and if user wants same settings for all the test cases then they can choose user params approach but if they like different settings for each test then they can use decorator approach
    
    Change-Id: If6fc8744a2b449eaf386d3e6a151fb8476138488
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1031274
    Reviewed-by: Stephen Demos <sdemos@google.com>
    Commit-Queue: Pavan Kumar Juturu <jpavankumar@google.com>

--- src/testing/end_to_end/examples/test_case_revive_example/test_case_revive_example.py
+++ src/testing/end_to_end/examples/test_case_revive_example/test_case_revive_example.py
@@ -14,15 +14,33 @@ _LOGGER: logging.Logger = logging.getLogger(__name__)
 class ExampleTestCaseRevive(test_case_revive.TestCaseRevive):
     """Example usage of test_case_revive.TestCaseRevive."""
 
-    # TODO(https://fxbug.dev/332381215): mypy complains about this decorator not
-    # being typed, but it is. Ignoring this for now, until we fix it.
     @test_case_revive.tag_test(tag_name="revive_test_case")
     def test_firmware_version(self) -> None:
+        """This test will be run both as a normal test and also as a revived."""
         for fuchsia_device in self.fuchsia_devices:
             _LOGGER.info(
                 "%s is running on %s firmware",
                 fuchsia_device.device_name,
                 fuchsia_device.firmware_version,
+            )
+
+    @test_case_revive.tag_test(
+        tag_name="revive_test_case",
+        fuchsia_device_operation=test_case_revive.FuchsiaDeviceOperation.SOFT_REBOOT,
+        test_method_execution_frequency=test_case_revive.TestMethodExecutionFrequency.POST_ONLY,
+    )
+    def _test_run_only_with_revive_option(self) -> None:
+        """This will be run only as a revived test case and runs the following
+        sequence:
+
+            1. Perform soft reboot operation
+            2. Run this test method
+        """
+        for fuchsia_device in self.fuchsia_devices:
+            _LOGGER.info(
+                "%s is a %s board",
+                fuchsia_device.device_name,
+                fuchsia_device.board,
             )
 
 
--- src/testing/end_to_end/mobly_base_tests/test_case_revive.py
+++ src/testing/end_to_end/mobly_base_tests/test_case_revive.py
@@ -4,6 +4,7 @@
 """Lacewing Test Case Reviver."""
 
 import importlib
+import dataclasses
 
 import enum
 import logging
@@ -21,6 +22,9 @@ _DMC_MODULE: str = "honeydew.auxiliary_devices.power_switch_dmc"
 _DMC_CLASS: str = "PowerSwitchDmc"
 
 
+# pylint: disable=protected-access
+
+
 class FuchsiaDeviceOperation(enum.StrEnum):
     """Operation that need to be performed on Fuchsia Device."""
 
@@ -35,12 +39,50 @@ class FuchsiaDeviceOperation(enum.StrEnum):
     IDLE_SUSPEND_AUTO_RESUME = "Idle-Suspend-Auto-Resume"
 
 
-def tag_test(tag_name: str = "revive_test_case") -> Callable[..., Any]:
+class TestMethodExecutionFrequency(enum.StrEnum):
+    """How often the test case method need to be executed in the revived test case."""
+
+    # This will result in: Run_Test -> Run_Device_Operation -> Run_Test
+    PRE_AND_POST = "Pre-and-Post"
+
+    # This will result in: Run_Test -> Run_Device_Operation
+    PRE_ONLY = "Pre-Only"
+
+    # This will result in: Run_Device_Operation -> Run_Test
+    POST_ONLY = "Post-Only"
+
+
+@dataclasses.dataclass(frozen=True)
+class _TestArgTuple:
+    test_case_method: str
+    fuchsia_device_operation: FuchsiaDeviceOperation
+    test_method_execution_frequency: TestMethodExecutionFrequency
+
+    def __str__(self) -> str:
+        return (
+            f"TestCaseMethod:{self.test_case_method}, "
+            f"FuchsiaDeviceOperation:{self.fuchsia_device_operation}, "
+            f"TestMethodExecutionFrequency:{self.test_method_execution_frequency}"
+        )
+
+
+def tag_test(
+    tag_name: str = "revive_test_case",
+    fuchsia_device_operation: FuchsiaDeviceOperation | None = None,
+    test_method_execution_frequency: TestMethodExecutionFrequency | None = None,
+) -> Callable[..., Any]:
     """Decorator that can be used to tag a test with a label"""
 
     def tags_decorator(func: Callable[..., Any]) -> Callable[..., Any]:
-        # pylint: disable=protected-access
         func._tag = tag_name  # type: ignore[attr-defined]
+        if fuchsia_device_operation is not None:
+            func._fuchsia_device_operation = FuchsiaDeviceOperation(  # type: ignore[attr-defined]
+                fuchsia_device_operation
+            )
+        if test_method_execution_frequency is not None:
+            func._test_method_execution_frequency = (  # type: ignore[attr-defined]
+                TestMethodExecutionFrequency(test_method_execution_frequency)
+            )
         return func
 
     return tags_decorator
@@ -50,9 +92,9 @@ class TestCaseRevive(fuchsia_base_test.FuchsiaBaseTest):
     """Test case revive is a lacewing test class that takes any Lacewing test
     case and modifies it to run in below sequence:
 
-    1. Run the test case
+    1. Run the test case (optional, if specified by user)
     2. Perform an operation requested by user (from list of supported operations)
-    3. Rerun the test case
+    3. Rerun the test case (optional, if specified by user)
     """
 
     def pre_run(self) -> None:
@@ -61,6 +103,7 @@ class TestCaseRevive(fuchsia_base_test.FuchsiaBaseTest):
         self._test_case_revive: bool = self.user_params.get(
             "test_case_revive", False
         )
+
         if self._test_case_revive is False:
             _LOGGER.info(
                 "[TestCaseRevive] - test_case_revive setting is not enabled "
@@ -68,74 +111,42 @@ class TestCaseRevive(fuchsia_base_test.FuchsiaBaseTest):
             )
             return
 
-        self._fuchsia_device_operation: str = self.user_params.get(
-            "fuchsia_device_operation", FuchsiaDeviceOperation.NONE
-        )
-        try:
-            self._fuchsia_device_operation_obj: FuchsiaDeviceOperation = (
-                FuchsiaDeviceOperation(self._fuchsia_device_operation)
-            )
-        except ValueError as err:
-            raise ValueError(
-                f"'{self._fuchsia_device_operation}' operation is not "
-                f"supported by 'TestCaseRevive'"
-            ) from err
-
         _LOGGER.info(
             "[TestCaseRevive] - test_case_revive setting is enabled in "
-            "user_params. So testing in revive mode with '%s' operation...",
-            self._fuchsia_device_operation,
+            "user_params. So testing in revive mode...",
         )
 
-        test_cases: list[str] = [
-            attribute
-            for attribute in dir(self)
-            if callable(getattr(self, attribute))
-            and attribute.startswith("test_") is True
-        ]
-        _LOGGER.info(
-            "[TestCaseRevive] - List of all the test cases in this test "
-            "class: %s",
-            test_cases,
-        )
+        self._read_and_validate_user_params()
 
-        # pylint: disable=protected-access
-        revived_test_cases: list[str] = [
-            test_case
-            for test_case in test_cases
-            if "_tag" in dir(getattr(self, test_case))
-            and getattr(self, test_case)._tag == "revive_test_case"
-        ]
-        _LOGGER.info(
-            "[TestCaseRevive] - List of all the test cases in this test class "
-            "that are configured to run with revived sequence: %s",
-            revived_test_cases,
-        )
+        revived_test_cases: list[str] = self._get_list_of_revived_test_cases()
 
-        test_arg_tuple_list: list[tuple[str]] = [
-            (revived_test_case,) for revived_test_case in revived_test_cases
-        ]
+        test_arg_tuple_list: list[
+            _TestArgTuple
+        ] = self._generate_test_args_tuple_list(revived_test_cases)
+
         self.generate_tests(
-            test_logic=self._test_case_revive_logic,
+            test_logic=self._logic_for_test_case_revive,
             name_func=self._revived_test_case_name_func,
-            arg_sets=test_arg_tuple_list,
+            arg_sets=[
+                dataclasses.astuple(test_arg_tuple)
+                for test_arg_tuple in test_arg_tuple_list
+            ],
         )
 
-    def _perform_op(self) -> None:
+    def _perform_op(
+        self, fuchsia_device_operation: FuchsiaDeviceOperation
+    ) -> None:
         """Perform user specified operation"""
 
         for fuchsia_device in self.fuchsia_devices:
             if (
-                self._fuchsia_device_operation_obj
+                fuchsia_device_operation
                 == FuchsiaDeviceOperation.IDLE_SUSPEND_AUTO_RESUME
             ):
                 fuchsia_device.system_power_state_controller.idle_suspend_auto_resume()
-            elif (
-                self._fuchsia_device_operation_obj
-                == FuchsiaDeviceOperation.SOFT_REBOOT
-            ):
+            elif fuchsia_device_operation == FuchsiaDeviceOperation.SOFT_REBOOT:
                 fuchsia_device.reboot()
-            elif self._fuchsia_device_operation_obj in [
+            elif fuchsia_device_operation in [
                 FuchsiaDeviceOperation.HARD_REBOOT,
                 FuchsiaDeviceOperation.POWER_CYCLE,
             ]:
@@ -161,29 +172,168 @@ class TestCaseRevive(fuchsia_base_test.FuchsiaBaseTest):
                     power_switch=self._power_switch, outlet=None
                 )
 
-    def _test_case_revive_logic(self, test_case: str) -> None:
+    def _logic_for_test_case_revive(
+        self,
+        test_case: str,
+        fuchsia_device_operation: FuchsiaDeviceOperation,
+        test_method_execution_frequency: TestMethodExecutionFrequency,
+    ) -> None:
         """TestCaseRevive logic"""
-        _LOGGER.info(
-            "[TestCaseRevive] - Running the %s before performing %s operation...",
-            test_case,
-            self._fuchsia_device_operation_obj,
-        )
-        getattr(self, test_case)()
+
+        sequence: str
+        if (
+            test_method_execution_frequency
+            == TestMethodExecutionFrequency.PRE_AND_POST
+        ):
+            sequence = (
+                f"{test_case} -> {fuchsia_device_operation} -> {test_case}"
+            )
+        elif (
+            test_method_execution_frequency
+            == TestMethodExecutionFrequency.PRE_ONLY
+        ):
+            sequence = f"{test_case} -> {fuchsia_device_operation}"
+        else:  # TestMethodExecutionFrequency.POST_ONLY
+            sequence = f"{fuchsia_device_operation} -> {test_case}"
+        _LOGGER.info("[TestCaseRevive] - Revived test logic: %s", sequence)
+
+        if test_method_execution_frequency in [
+            TestMethodExecutionFrequency.PRE_AND_POST,
+            TestMethodExecutionFrequency.PRE_ONLY,
+        ]:
+            _LOGGER.info(
+                "[TestCaseRevive] - Running the %s before performing %s operation...",
+                test_case,
+                fuchsia_device_operation,
+            )
+            getattr(self, test_case)()
 
         _LOGGER.info(
             "[TestCaseRevive] - Performing %s operation on all Fuchsia devices "
             "that are part of the testbed...",
-            self._fuchsia_device_operation_obj,
+            fuchsia_device_operation,
         )
-        self._perform_op()
+        self._perform_op(fuchsia_device_operation=fuchsia_device_operation)
 
-        _LOGGER.info(
-            "[TestCaseRevive] - Running the %s after performing %s operation...",
-            test_case,
-            self._fuchsia_device_operation_obj,
-        )
-        getattr(self, test_case)()
+        if test_method_execution_frequency in [
+            TestMethodExecutionFrequency.PRE_AND_POST,
+            TestMethodExecutionFrequency.POST_ONLY,
+        ]:
+            _LOGGER.info(
+                "[TestCaseRevive] - Running the %s after performing %s operation...",
+                test_case,
+                fuchsia_device_operation,
+            )
+            getattr(self, test_case)()
 
-    def _revived_test_case_name_func(self, test_case: str) -> str:
+    def _revived_test_case_name_func(
+        self,
+        test_case: str,
+        fuchsia_device_operation: FuchsiaDeviceOperation,
+        _: TestMethodExecutionFrequency,
+    ) -> str:
         """Revived test case name function"""
-        return f"{test_case}_revived_with_{self._fuchsia_device_operation_obj}"
+        test_case = test_case.lstrip("_")
+        return f"{test_case}_revived_with_{fuchsia_device_operation}"
+
+    def _read_and_validate_user_params(self) -> None:
+        """Read the user params associated with this test"""
+        self._fuchsia_device_operation: str = self.user_params.get(
+            "fuchsia_device_operation", FuchsiaDeviceOperation.NONE
+        )
+        try:
+            self._fuchsia_device_operation_obj: FuchsiaDeviceOperation = (
+                FuchsiaDeviceOperation(self._fuchsia_device_operation)
+            )
+        except ValueError as err:
+            raise ValueError(
+                f"'{self._fuchsia_device_operation}' operation is not "
+                f"supported by 'TestCaseRevive'"
+            ) from err
+
+        self._test_method_execution_frequency: str = self.user_params.get(
+            "test_method_execution_frequency",
+            TestMethodExecutionFrequency.PRE_AND_POST,
+        )
+        try:
+            self._test_method_execution_frequency_obj: (
+                TestMethodExecutionFrequency
+            ) = TestMethodExecutionFrequency(
+                self._test_method_execution_frequency
+            )
+        except ValueError as err:
+            raise ValueError(
+                f"'{self._test_method_execution_frequency}' is not valid"
+            ) from err
+
+    def _get_list_of_revived_test_cases(self) -> list[str]:
+        """Return the list of test cases that need to be revived."""
+        test_cases: list[str] = [
+            attribute
+            for attribute in dir(self)
+            if callable(getattr(self, attribute))
+            and attribute.startswith(("test_", "_test_")) is True
+        ]
+        _LOGGER.info(
+            "[TestCaseRevive] - List of all the test cases in this test "
+            "class: %s",
+            test_cases,
+        )
+
+        revived_test_cases: list[str] = [
+            test_case
+            for test_case in test_cases
+            if "_tag" in dir(getattr(self, test_case))
+            and getattr(self, test_case)._tag == "revive_test_case"
+        ]
+        _LOGGER.info(
+            "[TestCaseRevive] - List of all the test cases in this test class "
+            "that are configured to run with revived sequence: %s",
+            revived_test_cases,
+        )
+
+        return revived_test_cases
+
+    def _generate_test_args_tuple_list(
+        self, revived_test_cases: list[str]
+    ) -> list[_TestArgTuple]:
+        """Generate the list of duple data structure that need to be passed to
+        Mobly's generate_tests() method."""
+
+        test_arg_tuple_list: list[_TestArgTuple] = []
+        for revived_test_case in revived_test_cases:
+            fuchsia_device_operation: FuchsiaDeviceOperation = (
+                self._fuchsia_device_operation_obj
+            )
+            if "_fuchsia_device_operation" in dir(
+                getattr(self, revived_test_case)
+            ):
+                fuchsia_device_operation = getattr(
+                    self, revived_test_case
+                )._fuchsia_device_operation
+
+            test_method_execution_frequency: TestMethodExecutionFrequency = (
+                self._test_method_execution_frequency_obj
+            )
+            if "_test_method_execution_frequency" in dir(
+                getattr(self, revived_test_case)
+            ):
+                test_method_execution_frequency = getattr(
+                    self, revived_test_case
+                )._test_method_execution_frequency
+
+            test_arg_tuple: _TestArgTuple = _TestArgTuple(
+                test_case_method=revived_test_case,
+                fuchsia_device_operation=fuchsia_device_operation,
+                test_method_execution_frequency=test_method_execution_frequency,
+            )
+
+            test_arg_tuple_list.append(test_arg_tuple)
+
+        _LOGGER.debug(
+            "[TestCaseRevive] - Test arg list that will be passed to Mobly's "
+            "generate_test method to generate the revived test cases is: %s",
+            test_arg_tuple_list,
+        )
+
+        return test_arg_tuple_list
