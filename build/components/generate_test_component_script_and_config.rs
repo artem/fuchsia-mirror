58d808cfa85b3fff20754b9e5e06a8ea19695ac5
Ankur Mittal <anmittal@google.com>
2024-05-01 00:23:56 +0000

    [test_pilot] Generate partial test config for device tests
    This CL will generate partial config for all test components and pass it to the config generation tool.  The generator will make sure that all required files exist. We will implement logic to generate the config in next CL.
    
    This CL also aligns the arguments passed to the test pilot with design doc.
    
    BUG=327640651
    
    Change-Id: I315822f228b3514eb75b543a5c26ca8d99d77c4b
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1037035
    Commit-Queue: Ankur Mittal <anmittal@google.com>
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Reviewed-by: Aaron Wood <aaronwood@google.com>

--- build/components/generate_test_component_script_and_config.rs
+++ build/components/generate_test_component_script_and_config.rs
@@ -4,6 +4,7 @@
 
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -17,19 +18,45 @@ struct Args {
 
     #[structopt(long, help = "Generated script path.")]
     script_output_filename: String,
+
+    #[structopt(long, help = "Path to component manifest.")]
+    component_manifest_path: String,
+
+    #[structopt(long, help = "Path to partial test config.")]
+    partial_test_config: String,
+
+    #[structopt(long, help = "Path to test component specific config.")]
+    test_component_config: String,
+
+    #[structopt(long, help = "Generated test config path.")]
+    test_config_output_filename: String,
 }
 
 fn generate_bash_script(args: &Args) -> std::io::Result<()> {
     let mut file = File::create(&args.script_output_filename)?;
     file.write_all(b"#!/bin/bash\n")?;
     file.write_all(b"\n")?;
-    file.write_all(format!("FUCHSIA_BIN_PATH={} {}\n", args.bin_path, args.test_pilot).as_bytes())?;
+    file.write_all(
+        format!(
+            "{} --fuchsia-test-bin-path {} --fuchsia_test_configuration {}\n",
+            args.test_pilot, args.bin_path, args.test_config_output_filename
+        )
+        .as_bytes(),
+    )?;
     Ok(())
 }
 
-// TODO(https://fxbug.dev/327640651): Generate test config as well.
 fn main() -> std::io::Result<()> {
     let args = Args::from_args();
+
+    assert!(Path::new(&args.component_manifest_path).exists());
+    assert!(Path::new(&args.partial_test_config).exists());
+    assert!(Path::new(&args.test_component_config).exists());
+
     generate_bash_script(&args)?;
+
+    // Create a blank config.
+    // TODO(https://fxbug.dev/327640651): Generate full test config.
+    File::create(&args.test_config_output_filename)?;
     Ok(())
 }
