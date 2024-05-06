5157580177b93ce183270653e3fd6e171489bf02
Roland McGrath <mcgrathr@google.com>
2024-05-06 20:09:51 +0000

    [kernel][x86][phys] Start compiling linuxboot support code for 64-bit
    This starts the process of getting the linuxboot and legacy-boot
    code built for the 64-bit environment.  This is mostly just build
    wiring for a new "linuxboot64" flavor of "qemu" phys test to
    enable experimentation.  This causes new builds of various phys
    boot tests, but they are marked disabled so bots won't run them.
    
    The main code change here is to start handling the full 64-bit
    addresses for the command line and ramdisk (initrd) image, and
    for the ramdisk size.  In the load-bearing 32-bit build, these
    just now check for the high bits in the Linux boot_params that
    were ignored before.  Where previously an address or size above
    4GiB would be silently truncated to its lower 32 bits, now such
    values will not be used but instead very early logging will be
    attempted (and might have found a UART to log to) to complain
    about the too-large values.
    
    The other change only has any effect at all in the new 64-bit
    build, which is to do early address-space setup in InitMemory.
    The call was skipped before because it's a no-op for 32-bit.
    
    Bug: 335547883
    Change-Id: I91f8936001662d79c7ddab4c959aa20dc202ba26
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1037414
    Reviewed-by: Gianfranco Valentino <gevalentino@google.com>
    Fuchsia-Auto-Submit: Roland McGrath <mcgrathr@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/kernel/arch/x86/phys/linuxboot-init.cc
+++ zircon/kernel/arch/x86/phys/linuxboot-init.cc
@@ -4,6 +4,7 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT
 
+#include <lib/fit/result.h>
 #include <lib/zbi-format/memory.h>
 #include <lib/zircon-internal/e820.h>
 #include <stdio.h>
@@ -11,6 +12,7 @@
 #include <ktl/algorithm.h>
 #include <ktl/byte.h>
 #include <ktl/span.h>
+#include <phys/address-space.h>
 #include <phys/main.h>
 #include <phys/symbolize.h>
 
@@ -88,30 +90,57 @@ ktl::string_view GetBootloaderName(const linuxboot::boot_params& bp) {
   return {bootloader_name, sizeof(bootloader_name) - 1};
 }
 
+// This combines a 64-bit address stored in two separate 32-bit fields.
+// If the actual address doesn't fit in uintptr_t, it returns ktl::nullopt.
+// Note that a "successful" return might return nullptr.
+template <typename T>
+fit::result<uint64_t, T*> GetExtendedPtr(uint32_t low, uint32_t high) {
+  const uint64_t addr = (static_cast<uint64_t>(high) << 32) | low;
+  if (const uintptr_t ptr = static_cast<uintptr_t>(addr); ptr == addr) {
+    return fit::ok(reinterpret_cast<T*>(ptr));
+  }
+  return fit::error{addr};
+}
+
+fit::result<uint64_t, size_t> GetExtendedSize(uint32_t low, uint32_t high) {
+  const uint64_t value = (static_cast<uint64_t>(high) << 32) | low;
+  if (const size_t size = static_cast<size_t>(value); size == value) {
+    return fit::ok(size);
+  }
+  return fit::error{value};
+}
+
 }  // namespace
 
 LegacyBoot gLegacyBoot;
 
 // This populates the allocator and also collects other information.
 void InitMemory(void* bootloader_data, AddressSpace* aspace) {
-  // `aspace` is ignored as we'll set up the virtual address space as we switch
-  // to long mode.
+  if (aspace) {
+    // This is a no-op in 32-bit mode, which doesn't have paging turned on yet.
+    // There, paging will only be used as part of switching to long mode.  In
+    // the 64-bit build, this is necessary to ensure full identity mapping is
+    // in place, as the Linux/x86 64-bit boot protocol only requires the boot
+    // loader to set up mappings covering the kernel image itself and the
+    // pointers it passes in (boot_params, cmdline, ramdisk).
+    ArchSetUpAddressSpaceEarly(*aspace);
+  }
 
   auto& bp = *static_cast<const linuxboot::boot_params*>(bootloader_data);
 
   // Synthesize a boot loader name from the few bits we get.
   gLegacyBoot.bootloader = GetBootloaderName(bp);
 
-  if (bp.hdr.cmd_line_ptr != 0) {
+  auto cmdline_ptr = GetExtendedPtr<const char>(bp.hdr.cmd_line_ptr, bp.ext_cmd_line_ptr);
+  if (cmdline_ptr.is_ok() && *cmdline_ptr) {
     // The command line is NUL-terminated.
-    gLegacyBoot.cmdline = reinterpret_cast<const char*>(bp.hdr.cmd_line_ptr);
+    gLegacyBoot.cmdline = *cmdline_ptr;
   }
 
-  if (bp.hdr.ramdisk_image != 0) {
-    gLegacyBoot.ramdisk = {
-        reinterpret_cast<ktl::byte*>(bp.hdr.ramdisk_image),
-        bp.hdr.ramdisk_size,
-    };
+  auto ramdisk_image = GetExtendedPtr<ktl::byte>(bp.hdr.ramdisk_image, bp.ext_ramdisk_image);
+  auto ramdisk_size = GetExtendedSize(bp.hdr.ramdisk_size, bp.ext_ramdisk_size);
+  if (ramdisk_image.is_ok() && ramdisk_size.is_ok()) {
+    gLegacyBoot.ramdisk = {*ramdisk_image, *ramdisk_size};
   }
 
   gLegacyBoot.acpi_rsdp = bp.acpi_rsdp_addr;
@@ -121,6 +150,21 @@ void InitMemory(void* bootloader_data, AddressSpace* aspace) {
 
   // Now prime the allocator from that information.
   LegacyBootInitMemory();
+
+  // That may have set up the console, so these messages might be seen.
+  if (cmdline_ptr.is_error()) {
+    printf("%s: WARNING: Linux command-line address %#" PRIx64
+           " too high for 32-bit code, ignored.",
+           ProgramName(), cmdline_ptr.error_value());
+  }
+  if (ramdisk_image.is_error()) {
+    printf("%s: WARNING: Linux initrd address %#" PRIx64 " too high for 32-bit code, ignored.",
+           ProgramName(), ramdisk_image.error_value());
+  }
+  if (ramdisk_size.is_error()) {
+    printf("%s: WARNING: Linux initrd size %#" PRIx64 " too big for 32-bit code, ignored.",
+           ProgramName(), ramdisk_size.error_value());
+  }
 
   // Note this doesn't remove the memory covering the boot_params (zero page)
   // just examined.  We assume those have already been consumed as needed
