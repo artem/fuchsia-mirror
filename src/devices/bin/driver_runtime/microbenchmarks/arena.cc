96bfb77bbc99aade7a87857d411ed269647dec84
Megan Batty <mgnb@google.com>
2024-05-07 17:36:37 +0000

    [driver_runtime] Add benchmarks of varying allocs
    Adds benchmarks of allocating a large object into an arena and then lots
    of small ones, which currently causes the initial arena allocation to be
    left empty.
    
     Mean  Median (ns) Test case
     1403      83      Arena/AllocLargeThenSmall/4096bytes.one_large_block
     4803    2250      Arena/AllocLargeThenSmall/4096bytes.fifty_small_blocks
    10562    9000      Arena/AllocLargeThenSmall/8192bytes.one_large_block
     2261    2250      Arena/AllocLargeThenSmall/8192bytes.fifty_small_blocks
    14198   11167      Arena/AllocLargeThenSmall/16384bytes.one_large_block
    14524   11750      Arena/AllocLargeThenSmall/16384bytes.fifty_small_blocks
    15342   11646      Arena/AllocLargeThenSmall/32768bytes.one_large_block
    13627   11771      Arena/AllocLargeThenSmall/32768bytes.fifty_small_blocks
    14923   12312      Arena/AllocLargeThenSmall/65536bytes.one_large_block
    13067   11500      Arena/AllocLargeThenSmall/65536bytes.fifty_small_blocks
    
    Bug: b/336852954
    Change-Id: If2c48b3b961b3ccc4960cc159edd4779fd7ace4e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1041102
    Reviewed-by: Jocelyn Dang <jocelyndang@google.com>
    Reviewed-by: Fadi Meawad <fmeawad@google.com>
    Commit-Queue: Megan Batty <mgnb@google.com>

--- src/devices/bin/driver_runtime/microbenchmarks/arena.cc
+++ src/devices/bin/driver_runtime/microbenchmarks/arena.cc
@@ -32,6 +32,30 @@ bool ArenaAllocFreeTest(perftest::RepeatState* state, size_t buffer_size) {
   return true;
 }
 
+// Measure the time taken to allocate a large block followed by a bunch of small
+// ones.
+bool ArenaAllocLargeThenSmallTest(perftest::RepeatState* state, size_t large_size) {
+  constexpr uint32_t kTag = 'BNCH';
+  fdf::Arena arena(kTag);
+
+  state->DeclareStep("one_large_block");
+  state->DeclareStep("fifty_small_blocks");
+  while (state->KeepRunning()) {
+    void* block = arena.Allocate(large_size);
+    if (!block) {
+      return false;
+    }
+    state->NextStep();
+    for (size_t i = 0; i < 50; i++) {
+      void* block = arena.Allocate(32);
+      if (!block) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
 // Measure the time taken to check whether a block is contained in an arena
 // which holds |num_blocks|.
 bool ArenaContainsTest(perftest::RepeatState* state, uint32_t num_blocks) {
@@ -63,6 +87,16 @@ void RegisterTests() {
   for (auto block_size : kBlockSize) {
     auto alloc_free_name = fbl::StringPrintf("Arena/AllocFree/%ubytes", block_size);
     perftest::RegisterTest(alloc_free_name.c_str(), ArenaAllocFreeTest, block_size);
+  }
+
+  static const unsigned kLargeBlockSize[] = {
+      4 * 1024, 8 * 1024, 16 * 1024, 32 * 1024, 64 * 1024,
+  };
+  for (auto large_size : kLargeBlockSize) {
+    auto alloc_large_then_small_name =
+        fbl::StringPrintf("Arena/AllocLargeThenSmall/%ubytes", large_size);
+    perftest::RegisterTest(alloc_large_then_small_name.c_str(), ArenaAllocLargeThenSmallTest,
+                           large_size);
   }
 
   static const unsigned kNumBlocks[] = {
