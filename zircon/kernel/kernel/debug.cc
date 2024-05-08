eb0649ee93077e7a81ccc32f94b417e51fb243a6
Nick Maniscalco <maniscalco@google.com>
2024-05-08 17:56:21 +0000

    [kernel][debug] Disable threadload and threadq
    Disable the kernel's threadload and threadq diagnostic commands as
    they can assert failures or lockups.  See b/339440805 for details.
    
    Bug: 339440805
    Bug: 331847876
    Change-Id: I5a060969bbdf44bc6c265efe1a5497079668eaf7
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1044874
    Fuchsia-Auto-Submit: Nick Maniscalco <maniscalco@google.com>
    Reviewed-by: Tim Kilbourn <tkilbourn@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- zircon/kernel/kernel/debug.cc
+++ zircon/kernel/kernel/debug.cc
@@ -241,12 +241,16 @@ RecurringCallback g_threadq_callback([]() {
 
 }  // anonymous namespace
 static int cmd_threadload(int argc, const cmd_args* argv, uint32_t flags) {
-  g_threadload_callback.Toggle();
+  // TODO(https://fxbug.dev/339440805): Re-enable this command once we've resolved scheduler
+  // re-entrancy issues related to printing.
+  printf("threadload is disabled, see https://fxbug.dev/339440805.\n");
   return 0;
 }
 
 static int cmd_threadq(int argc, const cmd_args* argv, uint32_t flags) {
-  g_threadq_callback.Toggle();
+  // TODO(https://fxbug.dev/339440805): Re-enable this command once we've resolved scheduler
+  // re-entrancy issues related to printing.
+  printf("threadq is disabled, see https://fxbug.dev/339440805.\n");
   return 0;
 }
 
