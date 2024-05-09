bcc835f779508fa70c61ae4baf48a5905873aa55
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2024-05-09 18:19:21 +0000

    [snapshot] Enhance e2e test to also validate log files and others
    For PETS purposes we can likely reuse this test to ensure logging and
    inspect are working by validating the presence of data in a snapshot.
    
    We can likely reuse this test by just enhancing it to not only validate
    the inspect.json file, but also the log files as well as other metadata
    files present in the zip.
    
    Multiply: snapshot_test_fc
    Bug: 338416932
    Change-Id: Ife4b43b48c39c57c4a70eb35de88adc7187d63e2
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1040853
    Reviewed-by: Christopher Johnson <crjohns@google.com>
    Reviewed-by: Alex Pankhurst <pankhurst@google.com>
    Commit-Queue: Miguel Flores <miguelfrde@google.com>
    Reviewed-by: Kevin Cho <chok@google.com>

--- src/tests/end_to_end/snapshot/snapshot_test.py
+++ src/tests/end_to_end/snapshot/snapshot_test.py
@@ -16,8 +16,8 @@ import time
 import typing
 import zipfile
 
-from mobly import test_runner
-from mobly.asserts import assert_equal
+from typing import Dict, Any, List
+from mobly import asserts, test_runner
 from fuchsia_base_test import fuchsia_base_test
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
@@ -31,33 +31,61 @@ class SnapshotTest(fuchsia_base_test.FuchsiaBaseTest):
 
     def test_snapshot(self) -> None:
         # Get a device snapshot and extract the inspect.json file.
+        time_ns: list[int] = []
         for _ in range(self.user_params["repeat_count"]):
             with tempfile.TemporaryDirectory() as td:
                 file_name = "snapshot_test.zip"
-                start = time.time()
+                start = time.monotonic_ns()
                 self.fuchsia_dut.snapshot(td, file_name)
-                dur = time.time() - start
+                dur = time.monotonic_ns() - start
+                time_ns.append(dur)
+
                 final_path = os.path.join(td, file_name)
                 stat = os.stat(final_path)
-
                 _LOGGER.info(
-                    "Snapshot is %d bytes, took %.3f seconds", stat.st_size, dur
+                    "Snapshot is %d bytes, took %.3f seconds",
+                    stat.st_size,
+                    (dur / 1e9),
                 )
-                with zipfile.ZipFile(final_path) as zf:
-                    with zf.open("inspect.json") as inspect_file:
-                        inspect_data = json.load(inspect_file)
-                        self._check_archivist_data(inspect_data)
 
-    def _check_archivist_data(
-        self, inspect_data: typing.List[typing.Dict[str, typing.Any]]
+                with zipfile.ZipFile(final_path) as zf:
+                    # IMPORTANT: the exact contents of these files should not be verified. We just
+                    # want to assert that they exist and are not empty.
+                    self._validate_non_empty_files(
+                        zf,
+                        [
+                            "annotations.json",
+                            "build.snapshot.xml",
+                            "log.system.txt",
+                            "log.kernel.txt",
+                            "metadata.json",
+                        ],
+                    )
+                    self._validate_inspect(zf)
+        self._record_fuchsiaperf(time_ns)
+
+    def _validate_non_empty_files(
+        self, zf: zipfile.ZipFile, filenames: List[str]
     ) -> None:
+        for filename in filenames:
+            with zf.open(filename) as f:
+                contents = f.read()
+                asserts.assert_greater(len(contents), 0)
+
+    def _validate_inspect(self, zf: zipfile.ZipFile) -> None:
+        with zf.open("inspect.json") as inspect_file:
+            inspect_data = json.load(inspect_file)
+        asserts.assert_greater(len(inspect_data), 0)
+        self._check_archivist_data(inspect_data)
+
+    def _check_archivist_data(self, inspect_data: List[Dict[str, Any]]) -> None:
         # Find the Archivist's data, and assert that it's status is "OK"
-        archivist_only: typing.List[typing.Dict[str, typing.Any]] = [
+        archivist_only: List[Dict[str, Any]] = [
             data
             for data in inspect_data
             if data.get("moniker") == "bootstrap/archivist"
         ]
-        assert_equal(
+        asserts.assert_equal(
             len(archivist_only),
             1,
             "Expected to find one Archivist in the Inspect output.",
@@ -65,11 +93,24 @@ class SnapshotTest(fuchsia_base_test.FuchsiaBaseTest):
         archivist_data = archivist_only[0]
 
         health = archivist_data["payload"]["root"]["fuchsia.inspect.Health"]
-        assert_equal(
+        asserts.assert_equal(
             health["status"],
             "OK",
             "Archivist did not return OK status",
         )
+
+    def _record_fuchsiaperf(self, time_ns: list[int]) -> None:
+        fuchsiaperf_data = [
+            {
+                "test_suite": "fuchsia.test.snapshot",
+                "label": "SnapshotDuration",
+                "values": time_ns,
+                "unit": "ns",
+            },
+        ]
+        test_perf_file = os.path.join(self.log_path, "results.fuchsiaperf.json")
+        with open(test_perf_file, "w") as f:
+            json.dump(fuchsiaperf_data, f, indent=4)
 
 
 if __name__ == "__main__":
