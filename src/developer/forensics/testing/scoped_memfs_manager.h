5bbdd07c6fc5bf7325f6569df8eccf75b13110a1
Chris Drouillard <cdrllrd@google.com>
2024-05-10 22:20:48 +0000

    [forensics] Use /tmp in tests instead of memfs
    Use /tmp to back the directories created by ScopedMemFsManager instead
    of creating a new memfs for each directory.
    
    Bugs: b/293618742
    Change-Id: I026ec36ed4dc4afc8b43d5a8cc87467039db9dcd
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1037719
    Reviewed-by: Alex Pankhurst <pankhurst@google.com>
    Commit-Queue: Chris Drouillard <cdrllrd@google.com>

--- src/developer/forensics/testing/scoped_memfs_manager.h
+++ src/developer/forensics/testing/scoped_memfs_manager.h
@@ -5,68 +5,68 @@
 #ifndef SRC_DEVELOPER_FORENSICS_TESTING_SCOPED_MEMFS_MANAGER_H_
 #define SRC_DEVELOPER_FORENSICS_TESTING_SCOPED_MEMFS_MANAGER_H_
 
-#include <lib/async-loop/cpp/loop.h>
-#include <lib/async-loop/default.h>
-#include <lib/sync/completion.h>
+#include <fcntl.h>
+#include <lib/fdio/namespace.h>
 #include <lib/syslog/cpp/macros.h>
 
-#include <future>
+#include <memory>
 #include <string>
 #include <unordered_map>
-#include <vector>
 
-#include "src/storage/memfs/mounted_memfs.h"
+#include <fbl/unique_fd.h>
+
+#include "src/lib/files/scoped_temp_dir.h"
 
 namespace forensics::testing {
 
-// Manages creating and destroying MemFs backed directories in the calling processes namespace.
+// Manages creating and destroying directories in the calling processes namespace.
 class ScopedMemFsManager {
  public:
-  ScopedMemFsManager() : loop_(&kAsyncLoopConfigNoAttachToCurrentThread) {}
+  ScopedMemFsManager() {
+    tmp_storage_.reset(open("/tmp", O_RDONLY | O_DIRECTORY));
+    FX_CHECK(tmp_storage_.is_valid());
+    FX_CHECK(fdio_ns_get_installed(&ns_) == ZX_OK);
+  }
 
   ~ScopedMemFsManager() {
-    std::vector<std::promise<zx_status_t>> promises;
-    promises.reserve(filesystems_.size());
-
-    for (const auto& [_, memfs] : filesystems_) {
-      std::promise<zx_status_t>& promise = promises.emplace_back();
-      memfs->Shutdown([&promise](zx_status_t status) { promise.set_value(status); });
+    for (auto& [path, binding] : bindings_) {
+      FX_CHECK(fdio_ns_unbind(ns_, path.c_str()) == ZX_OK);
+      binding.reset();
+      if (path == "/tmp") {
+        // Rebind the real /tmp if it was unbound.
+        FX_CHECK(fdio_ns_bind_fd(ns_, "/tmp", tmp_storage_.get()) == ZX_OK);
+      }
     }
-
-    for (auto& promise : promises) {
-      FX_CHECK(promise.get_future().get() == ZX_OK);
-    }
-
-    loop_.Shutdown();
   }
 
-  bool Contains(const std::string& path) const { return filesystems_.count(path) != 0; }
+  bool Contains(const std::string& path) const { return bindings_.count(path) != 0; }
 
-  // Create a memfs backed directory at |path| in the component's namespace.
+  // Create a directory at |path| in the component's namespace.
   void Create(const std::string& path) {
     FX_CHECK(!Contains(path));
-    if (!started_) {
-      FX_CHECK(loop_.StartThread("forensics-scoped-memfs-manager") == ZX_OK);
-      started_ = true;
-    }
-    zx::result memfs = MountedMemfs::Create(loop_.dispatcher(), path.c_str());
-    FX_CHECK(memfs.is_ok()) << memfs.status_string();
-    filesystems_.emplace(path, std::move(memfs.value()));
-  }
 
-  // Destroy the memfs backed directory at |path| in the component's namespace.
-  void Destroy(const std::string& path) {
-    auto nh = filesystems_.extract(path);
-    FX_CHECK(!nh.empty());
-    std::promise<zx_status_t> promise;
-    nh.mapped()->Shutdown([&promise](zx_status_t status) { promise.set_value(status); });
-    FX_CHECK(promise.get_future().get() == ZX_OK);
+    if (path == "/tmp") {
+      // /tmp already exists in the namespace and backs all of the temporary directories. In order
+      // to provide a scoped /tmp, the main /tmp needs to be unbound from the namespace.
+      // `tmp_storage_` preserves a handle to the main /tmp and allows it to be bound again in the
+      // destructor.
+      FX_CHECK(fdio_ns_unbind(ns_, "/tmp") == ZX_OK);
+    }
+
+    auto temp_dir = std::make_unique<files::ScopedTempDirAt>(tmp_storage_.get());
+    fbl::unique_fd temp_dir_fd(
+        openat(temp_dir->root_fd(), temp_dir->path().c_str(), O_RDONLY | O_DIRECTORY));
+    FX_CHECK(temp_dir_fd.is_valid());
+
+    FX_CHECK(fdio_ns_bind_fd(ns_, path.c_str(), temp_dir_fd.get()) == ZX_OK);
+
+    bindings_.emplace(path, std::move(temp_dir));
   }
 
  private:
-  std::unordered_map<std::string, MountedMemfs> filesystems_;
-  async::Loop loop_;
-  bool started_{false};
+  fbl::unique_fd tmp_storage_;
+  fdio_ns_t* ns_;
+  std::unordered_map<std::string, std::unique_ptr<files::ScopedTempDirAt>> bindings_;
 };
 
 }  // namespace forensics::testing
