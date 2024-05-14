a4badf20ca44e71cd09bdee7c4ceadf8cf3018ae
Erick Tryzelaar <etryzelaar@google.com>
2024-05-14 07:05:06 +0000

    [pm] Deprecate `pm init`
    In preparation of the removal of `pm`, deprecate the usage of the
    `init` subcommand.
    
    Fixed: b/332712938
    Change-Id: I78e294ab1dae7c03acbf103d813e855fc1e04bc1
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/854505
    Fuchsia-Auto-Submit: Thomas Zander <thomasalpinus@google.com>
    Commit-Queue: Thomas Zander <thomasalpinus@google.com>
    Reviewed-by: Sen Jiang <senj@google.com>

--- src/sys/pkg/bin/pm/cmd/pm/pm.go
+++ src/sys/pkg/bin/pm/cmd/pm/pm.go
@@ -15,7 +15,6 @@ import (
 	"go.fuchsia.dev/fuchsia/src/sys/pkg/bin/pm/build"
 	"go.fuchsia.dev/fuchsia/src/sys/pkg/bin/pm/cmd/pm/archive"
 	buildcmd "go.fuchsia.dev/fuchsia/src/sys/pkg/bin/pm/cmd/pm/build"
-	initcmd "go.fuchsia.dev/fuchsia/src/sys/pkg/bin/pm/cmd/pm/init"
 	"go.fuchsia.dev/fuchsia/src/sys/pkg/bin/pm/cmd/pm/publish"
 	"go.fuchsia.dev/fuchsia/src/sys/pkg/bin/pm/cmd/pm/seal"
 	"go.fuchsia.dev/fuchsia/src/sys/pkg/bin/pm/cmd/pm/serve"
@@ -30,7 +29,6 @@ IMPORTANT: Please note that pm is being sunset and will be removed.
            through ffx. Please adapt workflows accordingly.
 
 Package Commands:
-    init     - initialize a package meta directory in the standard form
     build    - perform update and seal in order
     update   - update the merkle roots in meta/contents
     seal     - seal package metadata into a meta.far
@@ -98,7 +96,9 @@ func doMain() int {
 		err = nil
 
 	case "init":
-		err = initcmd.Run(cfg, flag.Args()[1:])
+		url := "https://fuchsia.dev/fuchsia-src/development/idk/documentation/packages"
+		fmt.Fprintf(os.Stderr, "please create the meta directory and the meta package file according to %v", url)
+		err = nil
 
 	case "publish":
 		err = publish.Run(cfg, flag.Args()[1:])
