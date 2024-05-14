f538047a444c7718f3995d229e6d5152c9389d09
Mary Xia <maryxia@google.com>
2024-05-14 00:08:46 +0000

    [trace_processing] Allow .fxt as input and format JSON
    Bug: b/338640046
    
    Change-Id: I7d66023c1229d999981bc13e931fe5b403860348
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1044882
    Commit-Queue: Mary Xia <maryxia@google.com>
    Reviewed-by: Gwen Mittertreiner <gmtr@google.com>

--- src/performance/lib/trace_processing/run_cpu_breakdown.py
+++ src/performance/lib/trace_processing/run_cpu_breakdown.py
@@ -8,7 +8,8 @@ Returns CPU breakdown in a JSON file.
 """
 
 import argparse
-import json
+import pathlib
+import pprint
 from trace_processing import trace_importing, trace_metrics, trace_model
 from trace_processing.metrics import cpu_breakdown
 import sys
@@ -25,22 +26,43 @@ def main() -> None:
     a free-form JSON with breakdown metrics to `output_path`.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("path_to_trace_json", type=str)
-    parser.add_argument("output_path", type=str)
     parser.add_argument(
-        "--percent_cutoff", type=float, default=DEFAULT_PERCENT_CUTOFF
+        "path_to_trace",
+        type=str,
+        help="path to trace file, either .fxt or .json",
+    )
+    parser.add_argument(
+        "output_path", type=str, help="output path for CPU breakdown, as .json"
+    )
+    parser.add_argument(
+        "--percent_cutoff",
+        type=float,
+        default=DEFAULT_PERCENT_CUTOFF,
+        help="percent cutoff (CPU usage less than this percent will not be logged)",
     )
     args = parser.parse_args()
 
+    trace_path_json: str = args.path_to_trace
+
+    if args.path_to_trace.endswith(".fxt"):
+        trace2json_path = pathlib.Path(sys.argv[0]).parent / "trace2json"
+        trace_path_json = trace_importing.convert_trace_file_to_json(
+            trace_path=args.path_to_trace,
+            trace2json_path=trace2json_path,
+        )
+    elif not args.path_to_trace.endswith(".json"):
+        parser.print_help()
+        return
+
     model: trace_model.Model = trace_importing.create_model_from_file_path(
-        args.path_to_trace_json
+        trace_path_json
     )
     breakdown = cpu_breakdown.CpuBreakdownMetricsProcessor(
         model, args.percent_cutoff
     ).process_metrics()
 
     with open(args.output_path, "w") as json_file:
-        json.dump(breakdown, json_file)
+        pprint.pprint(breakdown, json_file)
 
 
 if __name__ == "__main__":
