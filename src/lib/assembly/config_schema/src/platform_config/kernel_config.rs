4e0d8716f24b5cbbda699402b713a4936e558e98
Pierre Labatut <plabatut@google.com>
2024-05-15 09:20:10 +0000

    [assembly] Add contiguous pool setting to platform
    Both contiguous and protectect contiguous pools
    are configurable.
    
    Also document `continuous_eviction`.
    
    Test:
    fx set core.x64 --with-host src/lib/assembly:host_tests
    fx test run host_x64/assembly_platform_configuration_lib_tests
    
    Bug: 330838127
    
    Change-Id: I670694e7cab1b2e1cc2d76ad4dfcd8b4d4a7caad
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1043112
    Fuchsia-Auto-Submit: Pierre Labatut <plabatut@google.com>
    Commit-Queue: Pierre Labatut <plabatut@google.com>
    Reviewed-by: John Wittrock <wittrock@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/lib/assembly/config_schema/src/platform_config/kernel_config.rs
+++ src/lib/assembly/config_schema/src/platform_config/kernel_config.rs
@@ -5,6 +5,17 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+/// Amount of memory.
+#[derive(Debug, Deserialize, Serialize, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum MemorySize {
+    /// Number of bytes.
+    Fixed(u64),
+    /// Percentage of the system's physical memory.
+    /// the value must be stritly positive, and less than 100.
+    Relative(u8),
+}
+
 /// Platform configuration options for the kernel area.
 #[derive(Debug, Default, Deserialize, Serialize, PartialEq, JsonSchema)]
 #[serde(deny_unknown_fields)]
@@ -13,6 +24,10 @@ pub struct PlatformKernelConfig {
     pub memory_compression: bool,
     #[serde(default)]
     pub lru_memory_compression: bool,
+    /// Configures kernel eviction to run continually in the background to try
+    /// and keep the system out of memory pressure, as opposed to triggering
+    /// one-shot eviction only at memory pressure level transitions.
+    /// Enables the `kernel_evict_continuous` assembly input bundle.
     #[serde(default)]
     pub continuous_eviction: bool,
     /// For address spaces that use ASLR this controls the number of bits of
@@ -20,4 +35,10 @@ pub struct PlatformKernelConfig {
     /// address space and uses more memory for page tables. Valid values range
     /// from 0-36. Default value is 30.
     pub aslr_entropy_bits: Option<u8>,
+    /// Overrides the board-driver-specified size for sysmem's contiguous memory
+    /// pool. Default value is 0.
+    pub sysmem_contiguous_memory_size: Option<MemorySize>,
+    /// Overrides the board-driver-specified size for sysmem's default protected
+    /// memory pool. Default value is 5%.
+    pub sysmem_protected_memory_size: Option<MemorySize>,
 }
--- src/lib/assembly/platform_configuration/src/subsystems/kernel.rs
+++ src/lib/assembly/platform_configuration/src/subsystems/kernel.rs
@@ -3,8 +3,8 @@
 // found in the LICENSE file.
 
 use crate::subsystems::prelude::*;
-use assembly_config_schema::platform_config::kernel_config::PlatformKernelConfig;
-
+use anyhow::{ensure, Context};
+use assembly_config_schema::platform_config::kernel_config::{MemorySize, PlatformKernelConfig};
 pub(crate) struct KernelSubsystem;
 
 impl DefineSubsystemConfiguration<PlatformKernelConfig> for KernelSubsystem {
@@ -41,6 +41,39 @@ impl DefineSubsystemConfiguration<PlatformKernelConfig> for KernelSubsystem {
         if let Some(aslr_entropy_bits) = kernel_config.aslr_entropy_bits {
             let kernek_arg = format!("aslr.entropy_bits={}", aslr_entropy_bits);
             builder.kernel_arg(kernek_arg);
+        }
+
+        // A positive value specifies a fixed number of bytes, while a negative
+        // value indicates a relative amount, expressed as a percentage of the
+        // system's physical memory.
+        // Refer to src/devices/sysmem/drivers/sysmem/device.cc for percentage the value range.
+        let encode_to_integer = |mem_size: &MemorySize| -> anyhow::Result<i64> {
+            match mem_size {
+                MemorySize::Fixed(size) => Ok(i64::try_from(*size)?),
+                MemorySize::Relative(percentage) => {
+                    ensure!(
+                        *percentage >= 1 && *percentage <= 99,
+                        "Percentage value ({}) must be larger then 0 and less that 100",
+                        *percentage
+                    );
+                    Ok(-i64::from(*percentage))
+                }
+            }
+        };
+
+        if let Some(contiguous_memory_size) = &kernel_config.sysmem_contiguous_memory_size {
+            builder.kernel_arg(format!(
+                "driver.sysmem.contiguous_memory_size={}",
+                encode_to_integer(contiguous_memory_size)
+                    .context("Failed to convert `sysmem_contiguous_memory_size`")?
+            ));
+        }
+        if let Some(protected_memory_size) = &kernel_config.sysmem_protected_memory_size {
+            builder.kernel_arg(format!(
+                "driver.sysmem.protected_memory_size={}",
+                encode_to_integer(protected_memory_size)
+                    .context("Failed to convert `sysmem_protected_memory_size`")?
+            ));
         }
 
         Ok(())
@@ -99,11 +132,157 @@ mod test {
             gendir: Default::default(),
             resource_dir: Default::default(),
         };
-        let platform_kernel_config = Default::default();
+        let mut builder: ConfigurationBuilderImpl = Default::default();
+        let result =
+            KernelSubsystem::define_configuration(&context, &Default::default(), &mut builder);
+        assert!(result.is_ok());
+        assert!(builder.build().kernel_args.is_empty());
+    }
+
+    #[test]
+    fn test_contiguous_memory_size() {
+        let context = ConfigurationContext {
+            feature_set_level: &FeatureSupportLevel::Standard,
+            build_type: &BuildType::Eng,
+            board_info: &Default::default(),
+            ramdisk_image: false,
+            gendir: Default::default(),
+            resource_dir: Default::default(),
+        };
+        let platform_kernel_config = PlatformKernelConfig {
+            sysmem_contiguous_memory_size: Some(MemorySize::Fixed(123)),
+            sysmem_protected_memory_size: Some(MemorySize::Fixed(456)),
+            ..Default::default()
+        };
         let mut builder: ConfigurationBuilderImpl = Default::default();
         let result =
             KernelSubsystem::define_configuration(&context, &platform_kernel_config, &mut builder);
         assert!(result.is_ok());
-        assert!(!builder.build().kernel_args.iter().any(|s| s.starts_with("aslr.entropy_bits=12")));
+        let config = builder.build();
+        assert!(config.kernel_args.contains("driver.sysmem.contiguous_memory_size=123"));
+        assert!(config.kernel_args.contains("driver.sysmem.protected_memory_size=456"));
+    }
+
+    #[test]
+    fn test_contiguous_memory_size_negative() {
+        let context = ConfigurationContext {
+            feature_set_level: &FeatureSupportLevel::Standard,
+            build_type: &BuildType::Eng,
+            board_info: &Default::default(),
+            ramdisk_image: false,
+            gendir: Default::default(),
+            resource_dir: Default::default(),
+        };
+        let platform_kernel_config = PlatformKernelConfig {
+            sysmem_contiguous_memory_size: Some(MemorySize::Relative(5)),
+            sysmem_protected_memory_size: Some(MemorySize::Relative(12)),
+            ..Default::default()
+        };
+        let mut builder: ConfigurationBuilderImpl = Default::default();
+        let result =
+            KernelSubsystem::define_configuration(&context, &platform_kernel_config, &mut builder);
+        assert!(result.is_ok());
+        let config = builder.build();
+        assert!(config.kernel_args.contains("driver.sysmem.contiguous_memory_size=-5"));
+        assert!(config.kernel_args.contains("driver.sysmem.protected_memory_size=-12"));
+    }
+
+    #[test]
+    fn test_sysmem_contiguous_memory_size_percentage_too_high() {
+        let context = ConfigurationContext {
+            feature_set_level: &FeatureSupportLevel::Standard,
+            build_type: &BuildType::Eng,
+            board_info: &Default::default(),
+            ramdisk_image: false,
+            gendir: Default::default(),
+            resource_dir: Default::default(),
+        };
+        let platform_kernel_config = PlatformKernelConfig {
+            sysmem_contiguous_memory_size: Some(MemorySize::Relative(100)),
+            ..Default::default()
+        };
+        let mut builder: ConfigurationBuilderImpl = Default::default();
+        let result =
+            KernelSubsystem::define_configuration(&context, &platform_kernel_config, &mut builder);
+
+        let error_message = format!("{:#}", result.unwrap_err());
+        assert!(
+            error_message.contains("sysmem_contiguous_memory_size"),
+            "Faulty message `{}`",
+            error_message
+        );
+        assert!(error_message.contains("value (100)"));
+    }
+
+    #[test]
+    fn test_sysmem_protected_memory_size_percentage_too_high() {
+        let context = ConfigurationContext {
+            feature_set_level: &FeatureSupportLevel::Standard,
+            build_type: &BuildType::Eng,
+            board_info: &Default::default(),
+            ramdisk_image: false,
+            gendir: Default::default(),
+            resource_dir: Default::default(),
+        };
+        let platform_kernel_config = PlatformKernelConfig {
+            sysmem_protected_memory_size: Some(MemorySize::Relative(100)),
+            ..Default::default()
+        };
+        let mut builder: ConfigurationBuilderImpl = Default::default();
+        let result =
+            KernelSubsystem::define_configuration(&context, &platform_kernel_config, &mut builder);
+
+        let error_message = format!("{:#}", result.unwrap_err());
+        assert!(error_message.contains("sysmem_protected_memory_size"));
+        assert!(error_message.contains("value (100)"));
+    }
+    #[test]
+    fn test_sysmem_contiguous_memory_size_percentage_too_low() {
+        let context = ConfigurationContext {
+            feature_set_level: &FeatureSupportLevel::Standard,
+            build_type: &BuildType::Eng,
+            board_info: &Default::default(),
+            ramdisk_image: false,
+            gendir: Default::default(),
+            resource_dir: Default::default(),
+        };
+        let platform_kernel_config = PlatformKernelConfig {
+            sysmem_contiguous_memory_size: Some(MemorySize::Relative(0)),
+            ..Default::default()
+        };
+        let mut builder: ConfigurationBuilderImpl = Default::default();
+        let result =
+            KernelSubsystem::define_configuration(&context, &platform_kernel_config, &mut builder);
+
+        let error_message = format!("{:#}", result.unwrap_err());
+        assert!(
+            error_message.contains("sysmem_contiguous_memory_size"),
+            "Faulty message `{}`",
+            error_message
+        );
+        assert!(error_message.contains("value (0)"));
+    }
+
+    #[test]
+    fn test_sysmem_protected_memory_size_percentage_too_low() {
+        let context = ConfigurationContext {
+            feature_set_level: &FeatureSupportLevel::Standard,
+            build_type: &BuildType::Eng,
+            board_info: &Default::default(),
+            ramdisk_image: false,
+            gendir: Default::default(),
+            resource_dir: Default::default(),
+        };
+        let platform_kernel_config = PlatformKernelConfig {
+            sysmem_protected_memory_size: Some(MemorySize::Relative(0)),
+            ..Default::default()
+        };
+        let mut builder: ConfigurationBuilderImpl = Default::default();
+        let result =
+            KernelSubsystem::define_configuration(&context, &platform_kernel_config, &mut builder);
+
+        let error_message = format!("{:#}", result.unwrap_err());
+        assert!(error_message.contains("sysmem_protected_memory_size"));
+        assert!(error_message.contains("value (0)"));
     }
 }
