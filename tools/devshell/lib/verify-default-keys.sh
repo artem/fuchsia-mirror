639c3622404d6c71be7a1c0755136d43eb81aa35
Clayton Wilkinson <wilkinsonclay@google.com>
2024-05-15 17:37:48 +0000

    [fx] Updating remote scripts to use ~/ssh keys
    This removes the consideration of the in-tree ssh key when checking
    ssh keys between the local and remote computers.
    
    This is being done to align in-tree and out of tree ssh keys by using
    `ffx config get ssh.(priv|pub)` as the source of truth for the location
    of the keys both in and out of tree.
    
    Bug: b/339034335
    Change-Id: I90a1e4c7bf688af9a6e26e1ca74baa379c433d03
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1045213
    Commit-Queue: Clayton Wilkinson <wilkinsonclay@google.com>
    Reviewed-by: Colin Nelson <colnnelson@google.com>

--- tools/devshell/lib/verify-default-keys.sh
+++ tools/devshell/lib/verify-default-keys.sh
@@ -8,43 +8,32 @@ set -e
 _SSH_IDENTITY='.ssh/fuchsia_ed25519'
 _SSH_AUTH_KEYS='.ssh/fuchsia_authorized_keys'
 
-readonly LOCAL_KEY_MISMATCH_ERROR=64
-readonly REMOTE_KEY_MISMATCH_ERROR=65
 readonly MIXED_KEY_MISMATCH_ERROR=66
 readonly KEY_GENERATION_ERROR=67
 
 function fail_with_mixed_mismatch {
-  local extra_path_local=""
-  local extra_path_remote=""
-  if is_local_a_tree; then
-    extra_path_local="
-    ${local_fuchsia_dir}/.ssh/pkey*
-    ${local_fuchsia_dir}/.fx-ssh-path"
-  fi
-  if is_remote_a_tree; then
-    extra_path_remote="
-    ${remote_fuchsia_dir}/.ssh/pkey*
-    ${remote_fuchsia_dir}/.fx-ssh-path"
-  fi
-
   fx-error "
 You have different SSH credentials for Fuchsia devices in your local machine
 and on the remote server.
 
-Local and remote ~/.ssh/fuchsia_ed25519 and ~/.ssh/fuchsia_ed25519.pub need to match.
+Local and remote ~/.ssh/fuchsia_ed25519 need to match.
 
 Before you continue, update your local and remote source code.
 This ensures that the host tools on both machines expect the SSH credentials
 in a consistent location.
 
-If you have been using local tools like 'fx pave-remote', the local
+If you have been using local tools like 'fx flash-remote', the local
 credentials are probably most recent and you can backup and delete
 the remote keys on ${host}:
-    ~/.ssh/fuchsia_ed25519*${extra_path_remote}
+
+    ssh ${host} 'mkdir -p ~/.ssh/save && mv ~/.ssh/fuchsia_* ~/.ssh/save'
+
 and then run this command again.
 
 If you want to keep the remote credentials, backup and delete the local keys:
-    ~/.ssh/fuchsia_ed25519*${extra_path_local}
+
+    mkdir -p ~/.ssh/save && mv ~/.ssh/fuchsia_* ~/.ssh/save
+
 and then run this command again.
 
 If you manage your SSH credentials manually, use the --no-check-ssh-keys flag to skip this check.
@@ -54,8 +43,9 @@ If you manage your SSH credentials manually, use the --no-check-ssh-keys flag to
 
 
 # Check if there is a Fuchsia SSH key in HOME (used for the GN SDK)
-function normalize_local_gn_key {
-  if [[ ! -f "$HOME/${_SSH_IDENTITY}" ]]; then
+function normalize_local_key {
+  fx-info "Checking for ""${HOME}"/${_SSH_IDENTITY}""
+  if [[ ! -f "${HOME}/${_SSH_IDENTITY}" ]]; then
     return 1
   fi
   return 0
@@ -63,82 +53,10 @@ function normalize_local_gn_key {
 
 # Check whether a Fuchsia SSH key is present on the remote host when
 # the remote is not a Fuchsia tree.
-function normalize_remote_gn_key {
+function normalize_remote_key {
   # shellcheck disable=SC2029
   ssh ${ssh_base_args[@]+"${ssh_base_args[@]}"} "${host}" \
     "[[ -f \"$_SSH_IDENTITY\" ]]" || return ${_ERROR_NO_KEY}
-}
-
-# If a key exists in either //.ssh or HOME/.ssh, copy to the other location
-# (normalize) and return 0.
-# If no key is found on either location, return 1.
-# If keys exist on both locations, return 0 if they are the same, or exit
-# with LOCAL_KEY_MISMATCH_ERROR if they are different.
-#
-function normalize_local_intree_key {
-  # run in a subshell to avoid exiting
-  (
-    "${local_fuchsia_dir}/tools/ssh-keys/gen-ssh-keys.sh" --no-new-key >/dev/null 2>&1
-  )
-  status=$?
-  if [[ $status -eq $_ERROR_NO_KEY ]] ; then
-    return 1
-  elif [[ $status -eq 0 ]]; then
-    return 0
-  elif [[ $status -eq $_ERROR_MISMATCHED_KEYS ]]; then
-    fx-error "ERROR: mismatched key in local machine."
-    fx-error "Run '//tools/ssh-keys/gen-ssh-keys.sh' to learn how to fix it."
-    exit $LOCAL_KEY_MISMATCH_ERROR
-  else
-    fx-error "ERROR: unexpected error code $status from local //tools/ssh-keys/gen-ssh-keys.sh"
-    fx-error "Update your local source tree and run '//tools/ssh-keys/gen-ssh-keys.sh --no-new-key' manually to check."
-    exit $LOCAL_KEY_MISMATCH_ERROR
-  fi
-}
-
-
-# If a key exists in either //.ssh or HOME/.ssh on the remote server, copy to
-# the other location (normalize) and return 0.
-# If no key is found on either location, return 1.
-# If keys exist on both locations, return 0 if they are the same, or exit
-# with LOCAL_KEY_MISMATCH_ERROR if they are different.
-function normalize_remote_intree_key {
-  # run in a subshell to avoid exiting
-  (
-    # normalize remote keys accross //.ssh and HOME/.ssh using the remote
-    # gen-ssh-keys.sh with --no-new-key argument, so it doesn't generate a new
-    # key if one doesn't exist.
-    # shellcheck disable=SC2029
-    ssh ${ssh_base_args[@]+"${ssh_base_args[@]}"} "${host}" \
-      "${remote_fuchsia_dir}/tools/ssh-keys/gen-ssh-keys.sh --no-new-key >/dev/null 2>&1"
-  )
-  status=$?
-  if [[ $status -eq $_ERROR_NO_KEY ]] ; then
-    return 1
-  elif [[ $status -eq 0 ]]; then
-    return 0
-  elif [[ $status -eq $_ERROR_MISMATCHED_KEYS ]]; then
-    fx-error "ERROR: mismatched SSH keys in the remote machine."
-    fx-error "Run '//tools/ssh-keys/gen-ssh-keys.sh' on ${host} to learn how to fix it."
-    exit $REMOTE_KEY_MISMATCH_ERROR
-  elif [[ $status -eq 1 ]]; then
-    # error code "1" is from an old gen-ssh-keys.sh source.
-    fx-error "ERROR: you have an old //tools/ssh-keys/gen-ssh-keys.sh in ${host}:${remote_fuchsia_dir}."
-    fx-error "Please update your remote source tree and run again, or use --no-check-ssh-keys to skip SSH key checking."
-    exit $REMOTE_KEY_MISMATCH_ERROR
-  else
-    fx-error "ERROR: unexpected error code $status from remote //tools/ssh-keys/gen-ssh-keys.sh"
-    fx-error "Please update your remote source tree in ${host} and run '//tools/ssh-keys/gen-ssh-keys.sh --no-new-key' manually to check."
-    exit $REMOTE_KEY_MISMATCH_ERROR
-  fi
-}
-
-# Arguments
-#   *: Public key comment.
-function sync_remote_keys_intree {
-  # shellcheck disable=SC2029
-  ssh ${ssh_base_args[@]+"${ssh_base_args[@]}"} "${host}" \
-    "${remote_fuchsia_dir}/tools/ssh-keys/gen-ssh-keys.sh --description \"$*\""
 }
 
 function compare_remote_and_local {
@@ -150,7 +68,7 @@ function compare_remote_and_local {
 }
 
 function copy_local_to_remote {
-  scp ${ssh_base_args[@]+"${ssh_base_args[@]}"} -q -p "${HOME}/${_SSH_IDENTITY}" "${HOME}/${_SSH_IDENTITY}.pub" "${host}:.ssh/"
+  scp ${ssh_base_args[@]+"${ssh_base_args[@]}"} -q -p "${HOME}/${_SSH_IDENTITY}" "${host}:.ssh/"
   # shellcheck disable=SC2029
   ssh ${ssh_base_args[@]+"${ssh_base_args[@]}"} "${host}" "cat >> ${_SSH_AUTH_KEYS}" < "${HOME}/${_SSH_AUTH_KEYS}"
 }
@@ -160,7 +78,7 @@ function copy_remote_to_local {
     # force subshell to limit scope of umask
     umask 077
     mkdir -p "$HOME/.ssh"
-    scp ${ssh_base_args[@]+"${ssh_base_args[@]}"} -q -p "${host}:${_SSH_IDENTITY}" "${host}:${_SSH_IDENTITY}.pub" "$HOME/.ssh"
+    scp ${ssh_base_args[@]+"${ssh_base_args[@]}"} -q -p "${host}:${_SSH_IDENTITY}" "$HOME/.ssh"
     umask 133
     # shellcheck disable=SC2029
     ssh ${ssh_base_args[@]+"${ssh_base_args[@]}"} "${host}" cat "${_SSH_AUTH_KEYS}" >> "${HOME}/${_SSH_AUTH_KEYS}"
@@ -216,47 +134,26 @@ function verify_default_keys {
   local has_local_key=false
   local has_remote_key=false
 
-  # keep in sync with //tools/ssh-keys/gen-ssh-keys.sh
   local -r _ERROR_NO_KEY=112
   local -r _ERROR_MISMATCHED_KEYS=113
-  # // keep in sync with //tools/ssh-keys/gen-ssh-keys.sh
 
-
-  if is_local_a_tree; then
-    if normalize_local_intree_key; then
-      has_local_key=true
-    fi
-  else
-    if normalize_local_gn_key; then
-      has_local_key=true
-    fi
+  if normalize_local_key; then
+    has_local_key=true
   fi
 
-  if is_remote_a_tree; then
-    if normalize_remote_intree_key; then
-      has_remote_key=true
-    fi
-  else
-    if normalize_remote_gn_key; then
-      has_remote_key=true
-    fi
+  if normalize_remote_key; then
+    has_remote_key=true
   fi
+
 
   if ! $has_local_key && ! $has_remote_key; then
-    if is_remote_a_tree; then
-      fx-warn "No SSH credentials found, generating on the remote server first."
-      sync_remote_keys_intree "triggered by $0 from $(hostname -f)" \
-        || exit $KEY_GENERATION_ERROR
-      has_remote_key=true
-    else
       # if remote is not a Fuchsia tree, it is easier to generate the
       # key locally and copy to the remote than the opposite.
       fx-warn "No SSH credentials found, generating one locally first."
-      # check-fuchsia-ssh-config is defined in fuchsia-common.sh,
+      # heck-ssh-config is defined in //tools/devshell/lib/vars.sh,
       # assume it is already loaded by the caller
-      check-fuchsia-ssh-config || exit $KEY_GENERATION_ERROR
+      check-ssh-config  || exit $KEY_GENERATION_ERROR
       has_local_key=true
-    fi
   fi
 
   if $has_local_key && $has_remote_key ; then
@@ -267,15 +164,9 @@ function verify_default_keys {
   elif $has_local_key && ! $has_remote_key; then
     fx-warn "Copying local SSH credentials to the remote server"
     copy_local_to_remote
-    if is_remote_a_tree; then
-      sync_remote_keys_intree
-    fi
 
   elif ! $has_local_key && $has_remote_key; then
     fx-warn "Copying remote SSH credentials to the local machine"
     copy_remote_to_local
-    if is_local_a_tree; then
-      "${local_fuchsia_dir}/tools/ssh-keys/gen-ssh-keys.sh"
-    fi
   fi
 }
--- tools/ssh-keys/gen-ssh-keys.sh
+++ tools/ssh-keys/gen-ssh-keys.sh
@@ -71,60 +71,22 @@ function normalize_local_keys {
   test -f "$intree" && have_in_tree=true
   test -f "$inhome" && have_in_home=true
 
-  if $have_in_tree && $have_in_home && ! cmp --silent "$intree" "$inhome"; then
-    mismatched_keys
-    exit $_ERROR_MISMATCHED_KEYS
-  elif $have_in_tree && ! $have_in_home; then
+  if $have_in_tree && ! $have_in_home; then
     echo "Copying existing SSH credentials from //.ssh/pkey to ~/.ssh/fuchsia_ed25519"
     copy_ssh_keys "$intree" "$inhome" \
       "${FUCHSIA_ROOT}/.ssh/authorized_keys" \
       "${HOME}/.ssh/fuchsia_authorized_keys"
-  elif ! $have_in_tree && $have_in_home; then
-    echo "Copying existing SSH credentials from ~/.ssh/fuchsia_ed25519 to //.ssh/pkey"
-    copy_ssh_keys "$inhome" "$intree" \
-      "${HOME}/.ssh/fuchsia_authorized_keys" \
-      "${FUCHSIA_ROOT}/.ssh/authorized_keys"
   fi
 }
 
-# $INFRA_RECIPES is a variable set by infra bots. This keeps the infra recipes
-# running inside its per-build directory without seeking keys in a user's home
-# directory. See fxr/415297.
+# $INFRA_RECIPES is a variable set by infra bots.
 if [[ "${INFRA_RECIPES}" == "1" ]]; then
-  readonly SSH_DIR="${FUCHSIA_ROOT}/.ssh"
-  readonly keyfile="${SSH_DIR}/pkey"
-  readonly authfile="${SSH_DIR}/authorized_keys"
-else
-  readonly SSH_DIR="${HOME}/.ssh"
-  readonly keyfile="${SSH_DIR}/fuchsia_ed25519"
-  readonly authfile="${SSH_DIR}/fuchsia_authorized_keys"
-  normalize_local_keys
+  fx-error "gen-ssh-keys.sh should not be run by infra bots"
+  exit 0
 fi
 
-if [[ ! -f "${keyfile}" ]]; then
-  if $no_new_key; then
-    {
-      echo "ERROR: SSH key doesn't exist: ${keyfile}."
-      echo "Run //tools/ssh-keys/gen-ssh-keys.sh to generate a new one."
-    } 1>&2
-    exit $_ERROR_NO_KEY
-  fi
-  # force subshell to limit scope of umask
-  ( umask 077 && mkdir -p "${SSH_DIR}"; )
-  ssh-keygen -N "" -t ed25519 -f "${keyfile}" -C "${description}"
-  ssh-keygen -y -f "${keyfile}" >>"${authfile}"
 
-  if [[ "${INFRA_RECIPES}" != "1" ]]; then
-    # normalize again, so that the key created is kept in sync
-    normalize_local_keys
-  fi
-fi
-
-if [[ -f "${FUCHSIA_ROOT}/.fx-ssh-path" ]]; then
-  { read old_keyfile && read old_authfile; } < "${FUCHSIA_ROOT}/.fx-ssh-path"
-  if [[ "${old_keyfile}" == "${keyfile}" && "${old_authfile}" == "${authfile}" ]]; then
-    exit 0
-  fi
-fi
-
-echo -e "${keyfile}\n${authfile}" >"${FUCHSIA_ROOT}/.fx-ssh-path"
+# This is a tranisitional script that copies the in-tree ssh key to
+# ~/.ssh if one does not already exist. If neither exist, no action is
+# taken at this point
+normalize_local_keys
