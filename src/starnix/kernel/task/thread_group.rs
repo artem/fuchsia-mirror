5174003dc47307f9479944b25f140faf6fbdb694
Brian Bosak <bbosak@google.com>
2024-05-20 19:03:23 +0000

    [starnix] Fix SIGCHLD handling
    We should be sending SIGCHLD to all threads in a thread group
    when a thread group exits, not just to one thread.
    
    Fixed: b/340937109
    Change-Id: I1f48cdaee6b95b29ed91129534dbf29e53bd66f9
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1050604
    Commit-Queue: Brian Bosak <bbosak@google.com>
    Reviewed-by: Guocheng Wei <guochengwei@google.com>

--- src/starnix/kernel/task/thread_group.rs
+++ src/starnix/kernel/task/thread_group.rs
@@ -615,10 +615,11 @@ impl ThreadGroup {
 
         // Send signals
         if let Some(exit_signal) = zombie.exit_info.exit_signal {
-            if let Some(signal_target) = state.get_signal_target(exit_signal.into()) {
+            // get targets and notify all
+            for target in state.tasks() {
                 let mut signal_info = zombie.to_wait_result().as_signal_info();
                 signal_info.signal = exit_signal;
-                send_signal(&signal_target, signal_info).unwrap_or_else(|e| {
+                send_signal(&target, signal_info).unwrap_or_else(|e| {
                     log_warn!("Failed to send exit signal: {}", e);
                 });
             }
