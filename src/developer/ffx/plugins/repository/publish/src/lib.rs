2d4056e931d844c9f301dd14ee5b75c2e8fa9e70
Clayton Wilkinson <wilkinsonclay@google.com>
2024-05-20 14:51:10 +0000

    [ffx][compat]Adding machine support to repo publish
    This adds support for the JSON encoded output for
    `ffx repository publish`.
    
    This output encoding is intended to be used by integration scripts and
    build tools so that it is more stable and structured vs. human friendly
    output.
    
    Bug: b/336807621
    Change-Id: I4d7c33bd1de3cdd8ef7f5c900494b1fe9ac98d57
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1050319
    Reviewed-by: Andrew Davies <awdavies@google.com>
    Commit-Queue: Clayton Wilkinson <wilkinsonclay@google.com>

--- src/developer/ffx/plugins/repository/publish/src/lib.rs
+++ src/developer/ffx/plugins/repository/publish/src/lib.rs
@@ -2,16 +2,172 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use anyhow::Result;
-use errors::ffx_error;
-use ffx_core::ffx_plugin;
-
+use async_trait::async_trait;
 pub use ffx_repository_publish_args::RepoPublishCommand;
+use fho::{Error, FfxMain, FfxTool, Result, VerifiedMachineWriter};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use std::marker::PhantomData;
 
-#[ffx_plugin()]
-pub async fn cmd_repo_publish(cmd: RepoPublishCommand) -> Result<()> {
-    package_tool::cmd_repo_publish(cmd)
-        .await
-        .map_err(|err| ffx_error!("Error: failed to publish packages to repository: {err:?}"))?;
-    Ok(())
+#[derive(Debug, Deserialize, Serialize, JsonSchema, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum CommandStatus {
+    /// Successfully executed the command.
+    Ok {},
+    /// Unexpected error with string denoting error message.
+    UnexpectedError { message: String },
+    /// A known error that can be reported to the user.
+    UserError { message: String },
+}
+
+///  trait is used to allow mocking of the method.
+
+#[async_trait(?Send)]
+pub trait PackageTools {
+    async fn cmd_repo_publish(mut cmd: RepoPublishCommand) -> Result<()>;
+}
+
+pub struct SizedPackageTools {}
+
+#[async_trait(?Send)]
+impl PackageTools for SizedPackageTools {
+    async fn cmd_repo_publish(cmd: RepoPublishCommand) -> Result<()> {
+        package_tool::cmd_repo_publish(cmd).await.map_err(Into::into)
+    }
+}
+
+#[derive(FfxTool)]
+pub struct PublishTool<T: PackageTools> {
+    #[command]
+    cmd: RepoPublishCommand,
+    _phantom: PhantomData<T>,
+}
+
+fho::embedded_plugin!(PublishTool<SizedPackageTools>);
+
+#[async_trait(?Send)]
+impl<T: PackageTools> FfxMain for PublishTool<T> {
+    type Writer = VerifiedMachineWriter<CommandStatus>;
+    async fn main(self, mut writer: Self::Writer) -> Result<()> {
+        match T::cmd_repo_publish(self.cmd).await {
+            Ok(()) => {
+                writer.machine(&CommandStatus::Ok {})?;
+                Ok(())
+            }
+            Err(e @ Error::User(_)) => {
+                writer.machine(&CommandStatus::UserError { message: e.to_string() })?;
+                Err(e)
+            }
+            Err(e) => {
+                writer.machine(&CommandStatus::UnexpectedError { message: e.to_string() })?;
+                Err(e)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use fho::{bug, Format, TestBuffers};
+
+    struct OkFakeTools {}
+    #[async_trait(?Send)]
+    impl PackageTools for OkFakeTools {
+        async fn cmd_repo_publish(_cmd: RepoPublishCommand) -> Result<()> {
+            Ok(())
+        }
+    }
+
+    struct ErrFakeTools {}
+    #[async_trait(?Send)]
+    impl PackageTools for ErrFakeTools {
+        async fn cmd_repo_publish(_cmd: RepoPublishCommand) -> Result<()> {
+            Err(bug!("general error"))
+        }
+    }
+    #[fuchsia::test]
+    async fn test_machine_ok() {
+        let tool = PublishTool::<OkFakeTools> {
+            cmd: RepoPublishCommand {
+                signing_keys: None,
+                trusted_keys: None,
+                trusted_root: None,
+                package_manifests: vec![],
+                package_list_manifests: vec![],
+                package_archives: vec![],
+                product_bundle: vec![],
+                time_versioning: false,
+                metadata_current_time: chrono::Utc::now(),
+                refresh_root: false,
+                clean: false,
+                depfile: None,
+                copy_mode: fuchsia_repo::repository::CopyMode::Copy,
+                delivery_blob_type: 1,
+                watch: false,
+                ignore_missing_packages: false,
+                blob_manifest: None,
+                blob_repo_dir: None,
+                repo_path: "/somewhere".into(),
+            },
+            _phantom: PhantomData,
+        };
+        let buffers = TestBuffers::default();
+        let writer =
+            <PublishTool<OkFakeTools> as FfxMain>::Writer::new_test(Some(Format::Json), &buffers);
+
+        let res = tool.main(writer).await;
+
+        let (stdout, stderr) = buffers.into_strings();
+        assert!(res.is_ok(), "expected ok: {stdout} {stderr}");
+        let err = format!("schema not valid {stdout}");
+        let json = serde_json::from_str(&stdout).expect(&err);
+        let err = format!("json must adhere to schema: {json}");
+        <PublishTool<OkFakeTools> as FfxMain>::Writer::verify_schema(&json).expect(&err);
+        assert_eq!(json, serde_json::json!({"ok":{}}));
+    }
+
+    #[fuchsia::test]
+    async fn test_machine_error() {
+        let tool = PublishTool::<ErrFakeTools> {
+            cmd: RepoPublishCommand {
+                signing_keys: None,
+                trusted_keys: None,
+                trusted_root: None,
+                package_manifests: vec![],
+                package_list_manifests: vec![],
+                package_archives: vec![],
+                product_bundle: vec![],
+                time_versioning: false,
+                metadata_current_time: chrono::Utc::now(),
+                refresh_root: false,
+                clean: false,
+                depfile: None,
+                copy_mode: fuchsia_repo::repository::CopyMode::Copy,
+                delivery_blob_type: 1,
+                watch: false,
+                ignore_missing_packages: false,
+                blob_manifest: None,
+                blob_repo_dir: None,
+                repo_path: "/somewhere".into(),
+            },
+            _phantom: PhantomData,
+        };
+        let buffers = TestBuffers::default();
+        let writer =
+            <PublishTool<ErrFakeTools> as FfxMain>::Writer::new_test(Some(Format::Json), &buffers);
+
+        let res = tool.main(writer).await;
+
+        let (stdout, stderr) = buffers.into_strings();
+        assert!(res.is_err(), "expected err: {stdout} {stderr}");
+        let err = format!("schema not valid {stdout}");
+        let json = serde_json::from_str(&stdout).expect(&err);
+        let err = format!("json must adhere to schema: {json}");
+        <PublishTool<ErrFakeTools> as FfxMain>::Writer::verify_schema(&json).expect(&err);
+        assert_eq!(
+            json,
+            serde_json::json!({"unexpected_error" :{"message": "BUG: An internal command error occurred.\nError: general error"}})
+        );
+    }
 }
