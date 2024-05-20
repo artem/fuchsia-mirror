22192510e055ec2ce3fdcd9e4b76883a9f80f451
Clayton Wilkinson <wilkinsonclay@google.com>
2024-05-20 14:55:39 +0000

    [ffx][compat] Adding --machine support to repo add-from-pm
    This adds support for the JSON encoded output for
    `ffx repository add-from-pm`.
    
    This output encoding is intended to be used by integration scripts and
    build tools so that it is more stable and structured vs. human friendly
    output.
    
    Bug: b/336825573
    Change-Id: Ie527961242290ec8574936c4c47322f7fb114a2e
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1049729
    Commit-Queue: Clayton Wilkinson <wilkinsonclay@google.com>
    Reviewed-by: Thomas Zander <thomasalpinus@google.com>

--- src/developer/ffx/plugins/repository/add-from-pm/src/lib.rs
+++ src/developer/ffx/plugins/repository/add-from-pm/src/lib.rs
@@ -1,97 +1,165 @@
 // Copyright 2021 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-use anyhow::{anyhow, Context, Result};
-use errors::{ffx_bail, ffx_error};
-use ffx_core::ffx_plugin;
+use async_trait::async_trait;
+use camino::FromPathBufError;
+use ffx::RepositoryIteratorMarker;
 use ffx_repository_add_from_pm_args::AddFromPmCommand;
-use fidl_fuchsia_developer_ffx::{RepositoryIteratorMarker, RepositoryRegistryProxy};
+use fho::{
+    bug, daemon_protocol, return_user_error, user_error, Error, FfxContext, FfxMain, FfxTool,
+    Result, VerifiedMachineWriter,
+};
+use fidl_fuchsia_developer_ffx as ffx;
 use fidl_fuchsia_developer_ffx_ext::{RepositoryError, RepositorySpec};
 use fuchsia_url::RepositoryUrl;
+use schemars::JsonSchema;
+use serde::Serialize;
+use std::collections::BTreeSet;
 
 enum RepoRegState {
     New,
     Duplicate,
 }
 
-#[ffx_plugin(RepositoryRegistryProxy = "daemon::protocol")]
-pub async fn add_from_pm(cmd: AddFromPmCommand, repos: RepositoryRegistryProxy) -> Result<()> {
-    // Validate that we can construct a valid repository url from the name.
-    let repo_url = RepositoryUrl::parse_host(cmd.repository.to_string())
-        .map_err(|err| ffx_error!("invalid repository name for {:?}: {}", cmd.repository, err))?;
-    let repo_name = repo_url.host();
+#[derive(Debug, Serialize, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum CommandStatus {
+    /// Successful execution with an optional informational string.
+    Ok { repo_name: String },
+    /// Unexpected error with string.
+    UnexpectedError { message: String },
+    /// A known kind of error that can be reported usefully to the user
+    UserError { message: String },
+}
 
-    let full_path = cmd
-        .pm_repo_path
-        .canonicalize()
-        .with_context(|| format!("failed to canonicalize {:?}", cmd.pm_repo_path))?;
+#[derive(FfxTool)]
+pub struct AddFromPmTool {
+    #[command]
+    cmd: AddFromPmCommand,
+    #[with(daemon_protocol())]
+    repos: ffx::RepositoryRegistryProxy,
+}
 
-    let repo_spec = RepositorySpec::Pm {
-        path: full_path.try_into()?,
-        aliases: cmd.aliases.into_iter().collect(),
-    };
+fho::embedded_plugin!(AddFromPmTool);
 
-    match repo_registered_state(&repos, repo_name, &repo_spec).await? {
-        RepoRegState::Duplicate => {
-            tracing::info!("attempt to re-add {repo_name} using the same parameters, ignoring");
-            Ok(())
-        }
-        RepoRegState::New => match repos.add_repository(repo_name, &repo_spec.into()).await? {
-            Ok(()) => {
-                println!("added repository {}", repo_name);
+#[async_trait(?Send)]
+impl FfxMain for AddFromPmTool {
+    type Writer = VerifiedMachineWriter<CommandStatus>;
+    async fn main(self, mut writer: Self::Writer) -> fho::Result<()> {
+        match self.add_from_pm().await.map_err(Into::into) {
+            Ok(repo_name) => {
+                writer.machine_or(
+                    &CommandStatus::Ok { repo_name: repo_name.clone() },
+                    format!("added repository {repo_name}"),
+                )?;
                 Ok(())
             }
-            Err(err) => {
-                let err = RepositoryError::from(err);
-                ffx_bail!("Adding repository {} failed: {}", repo_name, err);
+            Err(e @ Error::User(_)) => {
+                writer.machine(&CommandStatus::UserError { message: e.to_string() })?;
+                Err(e)
             }
-        },
+            Err(e) => {
+                writer.machine(&CommandStatus::UnexpectedError { message: e.to_string() })?;
+                Err(e)
+            }
+        }
     }
 }
 
-async fn repo_registered_state(
-    repos: &RepositoryRegistryProxy,
-    repo_name: &str,
-    repo_spec: &RepositorySpec,
-) -> Result<RepoRegState> {
-    let (client, server) =
-        ffx_core::macro_deps::fidl::endpoints::create_endpoints::<RepositoryIteratorMarker>();
-    repos.list_repositories(server).context("listing repositories")?;
-    let client = client.into_proxy().context("creating repository iterator proxy")?;
+impl AddFromPmTool {
+    pub async fn add_from_pm(&self) -> Result<String> {
+        // Validate that we can construct a valid repository url from the name.
+        let repo_url =
+            RepositoryUrl::parse_host(self.cmd.repository.to_string()).map_err(|err| {
+                user_error!("invalid repository name for {:?}: {}", self.cmd.repository, err)
+            })?;
+        let repo_name = repo_url.host();
+        let full_path =
+            self.cmd.pm_repo_path.canonicalize().with_bug_context(|| {
+                format!("failed to canonicalize {:?}", self.cmd.pm_repo_path)
+            })?;
 
-    loop {
-        let batch = client.next().await.context("fetching next batch of repositories")?;
-        if batch.is_empty() {
-            break;
-        }
+        let repo_spec = RepositorySpec::Pm {
+            path: full_path.try_into().map_err(|e: FromPathBufError| bug!(e))?,
+            aliases: self.cmd.aliases.clone().into_iter().collect(),
+        };
 
-        for repo in batch {
-            if repo.name == repo_name {
-                let spec = RepositorySpec::try_from(repo.spec)?;
-                // If the repo specs match, we can continue with a warning.
-                if spec == *repo_spec {
-                    return Ok(RepoRegState::Duplicate);
-                } else {
-                    return Err(anyhow!("repository {repo_name} is registered using different settings, it could be removed by running `ffx repository remove {repo_name}"));
-                }
+        match self.repo_registered_state(repo_name, &repo_spec).await? {
+            RepoRegState::Duplicate => {
+                tracing::info!("attempt to re-add {repo_name} using the same parameters, ignoring");
+                Ok(repo_name.to_string())
             }
+            RepoRegState::New => match self
+                .repos
+                .add_repository(repo_name, &repo_spec.into())
+                .await
+                .map_err(|e| bug!(e))?
+            {
+                Ok(()) => Ok(repo_name.to_string()),
+                Err(err) => {
+                    let err = RepositoryError::from(err);
+                    Err(user_error!("Adding repository {} failed: {}", repo_name, err))
+                }
+            },
         }
     }
 
-    Ok(RepoRegState::New)
+    async fn repo_registered_state(
+        &self,
+        repo_name: &str,
+        repo_spec: &RepositorySpec,
+    ) -> Result<RepoRegState> {
+        let (client, server) =
+            ffx_core::macro_deps::fidl::endpoints::create_endpoints::<RepositoryIteratorMarker>();
+        self.repos.list_repositories(server).bug_context("listing repositories")?;
+        let client = client.into_proxy().bug_context("creating repository iterator proxy")?;
+        let (repo_path, repo_aliases) = match repo_spec {
+            RepositorySpec::Pm { path, aliases } => (Some(path.to_string()), aliases),
+            _ => return_user_error!("only pm style repositories are supported"),
+        };
+
+        loop {
+            let batch = client.next().await.bug_context("fetching next batch of repositories")?;
+            if batch.is_empty() {
+                break;
+            }
+
+            for repo in batch {
+                if repo.name == repo_name {
+                    // If the same path and aliases are specified, we can continue with a warning.
+                    match repo.spec {
+                        fidl_fuchsia_developer_ffx::RepositorySpec::Pm(spec) => {
+                            let spec_aliases = if let Some(aliases) = spec.aliases {
+                                BTreeSet::from_iter(aliases.into_iter())
+                            } else {
+                                BTreeSet::new()
+                            };
+                            if repo_path == spec.path && *repo_aliases == spec_aliases {
+                                return Ok(RepoRegState::Duplicate);
+                            }
+                        }
+                        _ => {
+                            return_user_error!("repository {repo_name} is registered using different settings, it could be removed by running `ffx repository remove {repo_name}");
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(RepoRegState::New)
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use assert_matches::assert_matches;
+    use fho::Format;
     use fidl_fuchsia_developer_ffx::{
         PmRepositorySpec, RepositoryConfig, RepositoryIteratorRequest, RepositoryRegistryMarker,
         RepositoryRegistryRequest, RepositoryRegistryRequestStream, RepositorySpec,
     };
-    use fuchsia_async as fasync;
     use futures::{SinkExt, StreamExt, TryStreamExt};
+    use serde_json::json;
 
     struct FakeRepositoryRegistry;
 
@@ -137,7 +205,7 @@ mod test {
         }
     }
 
-    #[fasync::run_singlethreaded(test)]
+    #[fuchsia::test]
     async fn test_add_from_pm() {
         let tmp = tempfile::tempdir().unwrap();
 
@@ -150,16 +218,18 @@ mod test {
 
         let _ = FakeRepositoryRegistry::new(stream, sender);
 
-        add_from_pm(
-            AddFromPmCommand {
+        let tool = AddFromPmTool {
+            cmd: AddFromPmCommand {
                 repository: "my-repo".to_owned(),
                 pm_repo_path: tmp.path().to_path_buf(),
                 aliases: vec![],
             },
             repos,
-        )
-        .await
-        .unwrap();
+        };
+        let buffers = fho::TestBuffers::default();
+        let writer = <AddFromPmTool as FfxMain>::Writer::new_test(None, &buffers);
+
+        tool.main(writer).await.expect("main ok");
 
         assert_eq!(
             receiver.next().await.unwrap(),
@@ -171,32 +241,123 @@ mod test {
                 })
             )
         );
+        let got = buffers.into_stdout_str();
+        assert_eq!(got, "added repository my-repo\n");
     }
 
-    #[fasync::run_singlethreaded(test)]
+    #[fuchsia::test]
     async fn test_add_from_pm_rejects_invalid_names() {
         let tmp = tempfile::tempdir().unwrap();
 
-        let repos =
-            setup_fake_repos(move |req| panic!("should not receive any requests: {:?}", req));
+        let repos: ffx::RepositoryRegistryProxy = fho::testing::fake_proxy(move |req| {
+            panic!("should not receive any requests: {:?}", req)
+        });
 
-        for name in ["", "my_repo", "MyRepo", "😀"] {
-            assert_matches!(
-                add_from_pm(
-                    AddFromPmCommand {
-                        repository: name.to_owned(),
-                        pm_repo_path: tmp.path().to_path_buf(),
-                        aliases: vec![],
-                    },
-                    repos.clone(),
-                )
-                .await,
-                Err(_)
-            );
+        for (name, want_msg) in [
+            ("", "invalid repository name for \"\": url parse error"),
+            ("my_repo", "invalid repository name for \"my_repo\": invalid host"),
+            ("MyRepo", "invalid repository name for \"MyRepo\": invalid host"),
+            ("😀", "invalid repository name for \"😀\": invalid host"),
+        ] {
+            let tool = AddFromPmTool {
+                cmd: AddFromPmCommand {
+                    repository: name.to_owned(),
+                    pm_repo_path: tmp.path().to_path_buf(),
+                    aliases: vec![],
+                },
+                repos: repos.clone(),
+            };
+            let buffers = fho::TestBuffers::default();
+            let writer = <AddFromPmTool as FfxMain>::Writer::new_test(None, &buffers);
+            let got_msg = tool.main(writer).await.expect_err("expected error").to_string();
+            assert_eq!(got_msg, want_msg);
         }
     }
 
-    #[fasync::run_singlethreaded(test)]
+    #[fuchsia::test]
+    async fn test_add_from_pm_machine() {
+        let tmp = tempfile::tempdir().unwrap();
+
+        let (sender, mut receiver) = futures::channel::mpsc::channel::<_>(1);
+
+        let (repos, stream) = ffx_core::macro_deps::fidl::endpoints::create_proxy_and_stream::<
+            RepositoryRegistryMarker,
+        >()
+        .unwrap();
+
+        let _ = FakeRepositoryRegistry::new(stream, sender);
+
+        let tool = AddFromPmTool {
+            cmd: AddFromPmCommand {
+                repository: "my-new-repo".to_owned(),
+                pm_repo_path: tmp.path().to_path_buf(),
+                aliases: vec![],
+            },
+            repos,
+        };
+        let buffers = fho::TestBuffers::default();
+        let writer = <AddFromPmTool as FfxMain>::Writer::new_test(Some(Format::Json), &buffers);
+
+        let res = tool.main(writer).await;
+
+        let got = receiver.next().await.unwrap();
+        assert_eq!(
+            got,
+            (
+                "my-new-repo".to_owned(),
+                RepositorySpec::Pm(PmRepositorySpec {
+                    path: Some(tmp.path().canonicalize().unwrap().to_str().unwrap().to_string()),
+                    ..Default::default()
+                })
+            )
+        );
+        let (stdout, stderr) = buffers.into_strings();
+        assert!(res.is_ok(), "expected ok: {stdout} {stderr}");
+        let err = format!("schema not valid {stdout}");
+        let json = serde_json::from_str(&stdout).expect(&err);
+        let err = format!("json must adhere to schema: {json}");
+        <AddFromPmTool as FfxMain>::Writer::verify_schema(&json).expect(&err);
+        assert_eq!(json, json!({"ok": { "repo_name": "my-new-repo"}}));
+        assert_eq!(stderr, "");
+    }
+
+    #[fuchsia::test]
+    async fn test_add_from_pm_rejects_invalid_names_machine() {
+        let tmp = tempfile::tempdir().unwrap();
+
+        let repos: ffx::RepositoryRegistryProxy = fho::testing::fake_proxy(move |req| {
+            panic!("should not receive any requests: {:?}", req)
+        });
+
+        for (name, want_msg) in [
+            ("", "invalid repository name for \"\": url parse error"),
+            ("my_repo", "invalid repository name for \"my_repo\": invalid host"),
+            ("MyRepo", "invalid repository name for \"MyRepo\": invalid host"),
+            ("😀", "invalid repository name for \"😀\": invalid host"),
+        ] {
+            let tool = AddFromPmTool {
+                cmd: AddFromPmCommand {
+                    repository: name.to_owned(),
+                    pm_repo_path: tmp.path().to_path_buf(),
+                    aliases: vec![],
+                },
+                repos: repos.clone(),
+            };
+            let buffers = fho::TestBuffers::default();
+            let writer = <AddFromPmTool as FfxMain>::Writer::new_test(Some(Format::Json), &buffers);
+            let res = tool.main(writer).await;
+
+            let (stdout, stderr) = buffers.into_strings();
+            assert!(res.is_err(), "expected err: {stdout} {stderr}");
+            let err = format!("schema not valid {stdout}");
+            let json = serde_json::from_str(&stdout).expect(&err);
+            let err = format!("json must adhere to schema: {json}");
+            <AddFromPmTool as FfxMain>::Writer::verify_schema(&json).expect(&err);
+            assert_eq!(json, json!({"user_error": { "message": want_msg}}));
+        }
+    }
+
+    #[fuchsia::test]
     async fn test_add_from_pm_exits_when_existing_repo() {
         let tmp = tempfile::tempdir().unwrap();
 
@@ -209,16 +370,16 @@ mod test {
 
         let _ = FakeRepositoryRegistry::new(stream, sender);
 
-        add_from_pm(
-            AddFromPmCommand {
+        let tool = AddFromPmTool {
+            cmd: AddFromPmCommand {
                 repository: "my-repo".to_owned(),
                 pm_repo_path: tmp.path().to_path_buf(),
                 aliases: vec![],
             },
-            repos.clone(),
-        )
-        .await
-        .unwrap();
+            repos: repos.clone(),
+        };
+
+        tool.add_from_pm().await.unwrap();
 
         assert_eq!(
             receiver.next().await.unwrap(),
@@ -234,34 +395,29 @@ mod test {
         // Adding the same repo (same name, same config)
         // is expected to be successful, as add_from_pm
         // will ignore request in this case.
-        assert_eq!(
-            add_from_pm(
-                AddFromPmCommand {
-                    repository: "my-repo".to_owned(),
-                    pm_repo_path: tmp.path().to_path_buf(),
-                    aliases: vec![],
-                },
-                repos.clone(),
-            )
-            .await
-            .is_err(),
-            false
-        );
+
+        let tool = AddFromPmTool {
+            cmd: AddFromPmCommand {
+                repository: "my-repo".to_owned(),
+                pm_repo_path: tmp.path().to_path_buf(),
+                aliases: vec![],
+            },
+            repos: repos.clone(),
+        };
+
+        assert_eq!(tool.add_from_pm().await.is_err(), false);
 
         // Adding a repo under the same name with a different config
         // should fail, as it would overwrite the already added repo.
-        assert_eq!(
-            add_from_pm(
-                AddFromPmCommand {
-                    repository: "my-repo".to_owned(),
-                    pm_repo_path: "/foo".into(),
-                    aliases: vec![],
-                },
-                repos,
-            )
-            .await
-            .is_err(),
-            true
-        );
+        let tool = AddFromPmTool {
+            cmd: AddFromPmCommand {
+                repository: "my-repo".to_owned(),
+                pm_repo_path: "/foo".into(),
+                aliases: vec![],
+            },
+            repos,
+        };
+
+        assert_eq!(tool.add_from_pm().await.is_err(), true);
     }
 }
