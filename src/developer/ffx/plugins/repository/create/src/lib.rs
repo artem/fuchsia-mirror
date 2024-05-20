ee3952e6b9d96cc41c3c0c5b67d4f45f3bd016aa
Clayton Wilkinson <wilkinsonclay@google.com>
2024-05-20 03:33:13 +0000

    [ffx][compat]Adding machine support to repo create
    This adds support for the JSON encoded output for
    `ffx repository create`.
    
    This output encoding is intended to be used by integration scripts and
    build tools so that it is more stable and structured vs. human friendly
    output.
    
    Bug: b/336807621
    Change-Id: I4188891842ff37b44b40d8415c44baa125a72ed0
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1050297
    Reviewed-by: Andrew Davies <awdavies@google.com>
    Commit-Queue: Clayton Wilkinson <wilkinsonclay@google.com>

--- src/developer/ffx/plugins/repository/create/src/lib.rs
+++ src/developer/ffx/plugins/repository/create/src/lib.rs
@@ -2,16 +2,143 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use anyhow::Result;
-use errors::ffx_error;
-use ffx_core::ffx_plugin;
+use async_trait::async_trait;
+use ffx_repository_create_args::RepoCreateCommand;
+use fho::{bug, Error, FfxMain, FfxTool, Result, VerifiedMachineWriter};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use std::marker::PhantomData;
 
-pub use ffx_repository_create_args::RepoCreateCommand;
+#[derive(Debug, Deserialize, Serialize, JsonSchema, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum CommandStatus {
+    /// Successfully waited for the target (either to come up or shut down).
+    Ok {},
+    /// Unexpected error with string denoting error message.
+    UnexpectedError { message: String },
+    /// A known error that can be reported to the user.
+    UserError { message: String },
+}
 
-#[ffx_plugin()]
-pub async fn cmd_repo_create(cmd: RepoCreateCommand) -> Result<()> {
-    package_tool::cmd_repo_create(cmd)
-        .await
-        .map_err(|err| ffx_error!("Error: failed to create repository: {err:?}"))?;
-    Ok(())
+///  trait is used to allow mocking of
+/// the method.
+
+#[async_trait(?Send)]
+pub trait PackageTools {
+    async fn cmd_repo_create(cmd: RepoCreateCommand) -> Result<()>;
+}
+
+pub struct SizedPackageTools {}
+
+#[async_trait(?Send)]
+impl PackageTools for SizedPackageTools {
+    async fn cmd_repo_create(cmd: RepoCreateCommand) -> Result<()> {
+        package_tool::cmd_repo_create(cmd).await.map_err(Into::into)
+    }
+}
+
+#[derive(FfxTool)]
+pub struct CreateTool<T: PackageTools> {
+    #[command]
+    cmd: RepoCreateCommand,
+    _phantom: PhantomData<T>,
+}
+
+fho::embedded_plugin!(CreateTool<SizedPackageTools>);
+
+#[async_trait(?Send)]
+impl<T: PackageTools> FfxMain for CreateTool<T> {
+    type Writer = VerifiedMachineWriter<CommandStatus>;
+    async fn main(self, mut writer: Self::Writer) -> Result<()> {
+        match T::cmd_repo_create(self.cmd).await {
+            Ok(()) => {
+                writer.machine(&CommandStatus::Ok {})?;
+                Ok(())
+            }
+            Err(e @ Error::User(_)) => {
+                writer.machine(&CommandStatus::UserError { message: e.to_string() })?;
+                Err(e)
+            }
+            Err(e) => {
+                writer.machine(&CommandStatus::UnexpectedError { message: e.to_string() })?;
+                Err(e)
+            }
+        }
+        .map_err(|err| bug!("Error: failed to create repository: {err:?}"))?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use fho::{Format, TestBuffers};
+
+    struct OkFakeTools {}
+    #[async_trait(?Send)]
+    impl PackageTools for OkFakeTools {
+        async fn cmd_repo_create(_cmd: RepoCreateCommand) -> Result<()> {
+            Ok(())
+        }
+    }
+
+    struct ErrFakeTools {}
+    #[async_trait(?Send)]
+    impl PackageTools for ErrFakeTools {
+        async fn cmd_repo_create(_cmd: RepoCreateCommand) -> Result<()> {
+            Err(bug!("general error"))
+        }
+    }
+    #[fuchsia::test]
+    async fn test_machine_ok() {
+        let tool = CreateTool::<OkFakeTools> {
+            cmd: RepoCreateCommand {
+                time_versioning: false,
+                keys: None,
+                repo_path: "/somewhere".into(),
+            },
+            _phantom: PhantomData,
+        };
+        let buffers = TestBuffers::default();
+        let writer =
+            <CreateTool<OkFakeTools> as FfxMain>::Writer::new_test(Some(Format::Json), &buffers);
+
+        let res = tool.main(writer).await;
+
+        let (stdout, stderr) = buffers.into_strings();
+        assert!(res.is_ok(), "expected ok: {stdout} {stderr}");
+        let err = format!("schema not valid {stdout}");
+        let json = serde_json::from_str(&stdout).expect(&err);
+        let err = format!("json must adhere to schema: {json}");
+        <CreateTool<OkFakeTools> as FfxMain>::Writer::verify_schema(&json).expect(&err);
+        assert_eq!(json, serde_json::json!({"ok":{}}));
+    }
+
+    #[fuchsia::test]
+    async fn test_machine_error() {
+        let tool = CreateTool::<ErrFakeTools> {
+            cmd: RepoCreateCommand {
+                time_versioning: false,
+                keys: None,
+                repo_path: "/somewhere".into(),
+            },
+            _phantom: PhantomData,
+        };
+        let buffers = TestBuffers::default();
+        let writer =
+            <CreateTool<ErrFakeTools> as FfxMain>::Writer::new_test(Some(Format::Json), &buffers);
+
+        let res = tool.main(writer).await;
+
+        let (stdout, stderr) = buffers.into_strings();
+        assert!(res.is_err(), "expected err: {stdout} {stderr}");
+        let err = format!("schema not valid {stdout}");
+        let json = serde_json::from_str(&stdout).expect(&err);
+        let err = format!("json must adhere to schema: {json}");
+        <CreateTool<ErrFakeTools> as FfxMain>::Writer::verify_schema(&json).expect(&err);
+        assert_eq!(
+            json,
+            serde_json::json!({"unexpected_error" :{"message": "BUG: An internal command error occurred.\nError: general error"}})
+        );
+    }
 }
