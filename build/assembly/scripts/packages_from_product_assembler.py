f4f3c81c42957fc59e366eca3b509f87d6bf9aac
David 'Digit' Turner <digit@google.com>
2024-05-22 07:24:21 +0000

    [assembly] Properly handle @gn_targets path when exporting packages from Bazel to Ninja.
    When a package is assembled with Bazel and includes inputs from the Ninja
    build directory, its assembly manifest will contain paths that reference
    the @gn_targets repository, or symlinks that point to files inside the
    latter. The files in @gn_targets are themselves symlinks pointing to the
    Ninja output directory.
    
    Because the content of @gn_targets is regenerated every time a different
    GN bazel_action() is built, these paths can point to stale symlinks
    when they are used outside of the Bazel invocation that generated
    them.
    
    This can happen with the following sequence:
    
    1) A package is generated by GN, even if Bazel assembly is enabled.
       This happens for example for any fuchsia_shell_package() target
       such as `//third_party/sbase:basename_pkg.pkg`.
    
    2) The assembly_input_bundle() GN target is built, which reference
       the package in 1.
    
    On the Bazel side:
    
      `//build/bazel/assembly/assembly_input_bundle:platform_eng`,
      a `platform_aibs()` target, depends on:
    
      `//build/bazel/assembly/assembly_input_bundle:core_realm_eng`,
      a `fuchsia_platform_artifacts()` target, which depends on
    
      `@gn_targets//bundles/assembly:core_realm_eng` which exposes
      the
    
    3) //build/bazel/assembly/assembly_input_bundle:platform_eng is
       a Bazel platform_aibs() target, which references the
    
       The corresponding manifest file and content are stored in the
       Bazel output_base.
    
    Fortunately, their format is fixed intentionally to allow the extraction
    of the corresponding Ninja output artifact easily.
    
    Bug: 42082672
    Fixed: 340667657
    Change-Id: I637d153faa859e83d80a3d3ed427200c2edeaf36
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1051952
    Reviewed-by: David Fang <fangism@google.com>
    Commit-Queue: David Turner <digit@google.com>
    Reviewed-by: Aidan Wolter <awolter@google.com>

--- build/assembly/scripts/packages_from_product_assembler.py
+++ build/assembly/scripts/packages_from_product_assembler.py
@@ -14,6 +14,52 @@ import sys
 from pathlib import Path
 
 
+def _rebase_bazel_path(path: str, rebase_prefix: str) -> str:
+    """Rebase paths relative to the Bazel execroot if needed.
+
+    This also takes care of paths that belong to the @gn_targets
+    repository. Because the content of this repository is regenerated
+    on every bazel_action() command launched from Ninja, these paths
+    may be dangling symlinks. However, they are intentionally formatted
+    in a way that allows extracting the corresponding Ninja path.
+
+    These can appear in two ways:
+
+    - Directly as a |path| value that starts with `external/gn_targets/`, e.g.:
+
+        external/gn_targets/src/foo/_files/obj/src/foo/cmd
+
+    - As a Bazel path to a symlink that points to an absolute path within
+      the Bazel {OUTPUT_BASE}/external/gn_targets/ repository, e.g.:
+
+        bazel-out/k8-fastbuild/bin/src/foo/cmd
+         --symlink-->
+           /work/out/default/gen/build/bazel/output_base/external/gn_targets/src/foo/_files/obj/src/foo/cmd
+
+    In both cases, keeping the path components that follow the `/_files/` one gets the
+    correct result (relative to the Ninja build dir, i.e. the current directory).
+
+    Args:
+      path: Input package path, relative to rebase_prefix.
+      rebase_prefix: Either an empty string, or a path prefix to the output base
+         that must contain a trailing separator.
+
+    Returns:
+      new path, relative to the current directory.
+    """
+    rebased_path = rebase_prefix + path
+    if os.path.islink(rebased_path):
+        path = os.readlink(rebased_path)
+
+    _, external_gn_targets, suffix = path.partition("external/gn_targets/")
+    if external_gn_targets:
+        _, files, ninja_path = suffix.partition("/_files/")
+        assert files, f"Unexpected @gn_targets path: {path}"
+        return ninja_path
+
+    return rebased_path
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Parse assembly product output manifest to get packages for a particular package set."
@@ -30,9 +76,9 @@ def main():
         help="Package set to get the packages for",
     )
     parser.add_argument(
-        "--rebase",
+        "--bazel-execroot",
         required=False,
-        help="Optionally rebase the package manifest paths from the assembly manifest.",
+        help="Optionally rebase the package manifest paths relative to the Bazel execroot, from the assembly manifest.",
     )
     parser.add_argument(
         "--output",
@@ -41,16 +87,18 @@ def main():
     )
     args = parser.parse_args()
 
-    assert not args.rebase or os.path.isdir(
-        args.rebase
-    ), "--rebase needs to specify a valid directory path!"
+    assert not args.bazel_execroot or os.path.isdir(
+        args.bazel_execroot
+    ), "--bazel-execroot needs to specify a valid directory path!"
+    rebase_prefix = (
+        args.bazel_execroot.rstrip("/") + "/" if args.bazel_execroot else ""
+    )
+
     assembly_manifest = json.load(args.assembly_manifest)
     package_manifests = [
         (
             # Normalize the rebased path to remove any internal ../..'s.
-            os.path.normpath(args.rebase.rstrip("/") + "/" + package)
-            if args.rebase
-            else package
+            os.path.normpath(_rebase_bazel_path(package, rebase_prefix))
         )
         for package in assembly_manifest[args.package_set]
     ]
