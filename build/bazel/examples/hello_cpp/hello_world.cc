0848e1950134d6e5cead383fec42f21c6acb79ec
David Dorwin <ddorwin@google.com>
2024-05-29 15:38:40 +0000

    [versioning][bazel] Add comment about raw use of __Fuchsia_API_level__
    Make it clear that this part of the example is not something that
    readers should follow.
    
    Bug: 42084512
    Change-Id: Ic2fc0c0a32d4d42556d8191064528840e5fe3059
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1054582
    Commit-Queue: David Dorwin <ddorwin@google.com>
    Reviewed-by: David Turner <digit@google.com>

--- build/bazel/examples/hello_cpp/hello_world.cc
+++ build/bazel/examples/hello_cpp/hello_world.cc
@@ -4,6 +4,8 @@
 
 #include <iostream>
 
+// In real code, use the macros in <zircon/availability.h> for such checks.
+// This is an exceptional case where a check for equality is needed.
 #if __Fuchsia_API_level__ == 17
 #include "lib/hello_printer_17.h"
 #endif
--- build/bazel/examples/hello_cpp/lib/hello_printer_17.h
+++ build/bazel/examples/hello_cpp/lib/hello_printer_17.h
@@ -5,6 +5,8 @@
 #ifndef BUILD_BAZEL_EXAMPLES_HELLO_CPP_LIB_HELLO_PRINTER_17_H_
 #define BUILD_BAZEL_EXAMPLES_HELLO_CPP_LIB_HELLO_PRINTER_17_H_
 
+// In real code, use the macros in <zircon/availability.h> for such checks.
+// This is an exceptional case where a check for equality is needed.
 #if __Fuchsia_API_level__ == 17
 
 namespace hello_printer_17 {
@@ -19,6 +21,6 @@ class HelloPrinter {
 
 }  // namespace hello_printer_17
 
-#endif  // __Fuchsia_API_level__
+#endif  // __Fuchsia_API_level__ == 17
 
 #endif  // BUILD_BAZEL_EXAMPLES_HELLO_CPP_LIB_HELLO_PRINTER_17_H_
