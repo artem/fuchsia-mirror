cb173c4faccdc68021bd3a6db8ba2ee21ed07c98
Andrew Davies <awdavies@google.com>
2024-06-04 19:36:11 +0000

    [ffx] Update get-time docs.
    See https://fuchsia.dev/reference/syscalls/clock_get_monotonic
    
    Fixed: b/343379740
    Change-Id: Ied92a1d804d29d40c5d006f8cb3d1854abe54585
    Reviewed-on: https://fuchsia-review.googlesource.com/c/fuchsia/+/1060053
    Reviewed-by: Clayton Wilkinson <wilkinsonclay@google.com>
    Fuchsia-Auto-Submit: Andrew Davies <awdavies@google.com>
    Commit-Queue: Auto-Submit <auto-submit@fuchsia-infra.iam.gserviceaccount.com>

--- src/developer/ffx/plugins/target/get-time/src/args.rs
+++ src/developer/ffx/plugins/target/get-time/src/args.rs
@@ -10,7 +10,12 @@ use ffx_core::ffx_command;
 #[argh(
     subcommand,
     name = "get-time",
-    description = "Gets the monotonic time on the target device",
+    description = r#"Returns the current time in the system monotonic clock.
+This is the number of nanoseconds since the system was powered on.
+It does not always reset on reboot and does not adjust during sleep,
+and thus should not be used as a reliable source of uptime.
+
+See https://fuchsia.dev/reference/syscalls/clock_get_monotonic"#,
     example = "To get the target time:
 
     $ ffx target get-time
